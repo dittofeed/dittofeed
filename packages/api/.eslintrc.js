5a55731d5ca236b5efe181ee9722bec726210b46
Max Gurewitz <maxgurewitz@gmail.com>
2023-02-01 10:08:03 -0800

    book a demo link

--- packages/api/.eslintrc.js
+++ packages/api/.eslintrc.js
@@ -1,3 +1,26 @@
 module.exports = {
   extends: ["plugin:@typescript-eslint/recommended-requiring-type-checking"],
 };
+
+describe("When the bookmark button is clicked", () => {
+
+  beforeEach(async () => {
+	  df = await Dittofeed.setupTestEnv();
+  })
+
+	it("Emails the clicking user", async () => {
+		render(<BookmarkButton/>)
+		fireEvent.click(screen.getByText(/bookmark/i))
+
+		// Using simulated time.
+		await df.sleep("1 week");
+		const messages = await df.fetchMessages();
+
+		expect(messages).toEqual([
+			expect.objectContaining({
+				to: "test@email.com",
+				body: expect.stringContaining("https://app.com/user-1/bookmarks")
+			});
+		]);
+  });
+});
