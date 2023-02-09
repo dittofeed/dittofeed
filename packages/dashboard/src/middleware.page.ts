9725f9a9b39370e0f7125cee572b99f2360376d4
Max Gurewitz <max@dittofeed.com>
2023-02-09 04:38:44 -0800

    provide env var for dynamic redirects to dashboard base path (#36)

---
+++ packages/dashboard/src/middleware.page.ts
@@ -0,0 +1,25 @@
+import type { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
+
+export function middleware(request: NextRequest) {
+  if (
+    process.env.BASE_PATH &&
+    request.nextUrl.pathname.startsWith("/dashboard")
+  ) {
+    const pathname = `${process.env.DASHBOARD_BASE_PATH}/${request.nextUrl.pathname}`;
+    return NextResponse.redirect(new URL(pathname, request.url));
+  }
+}
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     */
+    "/((?!api|_next/static|_next/image|favicon.ico).*)",
+  ],
+};
