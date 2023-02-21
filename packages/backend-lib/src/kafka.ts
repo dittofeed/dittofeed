8aba9977fa5f1170488c15fcb12239f68a1d9741
Max Gurewitz <max@dittofeed.com>
2023-02-20 22:00:54 -0800

    add ssl option to kafka (#60)

--- packages/backend-lib/src/kafka.ts
+++ packages/backend-lib/src/kafka.ts
@@ -2,11 +2,12 @@ import { Kafka, Partitioners } from "kafkajs";
 
 import config from "./config";
 
-const { kafkaUsername, kafkaPassword, kafkaBrokers } = config();
+const { kafkaUsername, kafkaPassword, kafkaBrokers, kafkaSsl } = config();
 
 export const kafka = new Kafka({
   clientId: "dittofeed",
   brokers: kafkaBrokers,
+  ssl: kafkaSsl,
   sasl: {
     mechanism: "plain",
     username: kafkaUsername,
