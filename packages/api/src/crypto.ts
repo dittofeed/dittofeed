02c60e85126524b7498898c46af59a7fa950e45b
Max Gurewitz <maxgurewitz@gmail.com>
2023-02-28 16:40:20 -0800

    add missing crypto file

---
+++ packages/api/src/crypto.ts
@@ -0,0 +1,14 @@
+import * as crypto from "crypto";
+
+export function generateDigest({
+  sharedSecret,
+  rawBody,
+}: {
+  sharedSecret: string;
+  rawBody: string;
+}) {
+  return crypto
+    .createHmac("sha1", sharedSecret)
+    .update(Buffer.from(rawBody, "utf-8"))
+    .digest("hex");
+}
