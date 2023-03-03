b1e2d8d9244de55b6c08f95975fb7e41cb7f560b
Max Gurewitz <maxgurewitz@gmail.com>
2023-03-02 20:58:22 -0800

    user events test

--- packages/backend-lib/src/userEvents.test.ts
+++ packages/backend-lib/src/userEvents.test.ts
@@ -1,6 +1,28 @@
 import { Workspace } from "@prisma/client";
+import { segmentIdentifyEvent } from "../test/factories/segment";
+import config from "./config";
+import { insertUserEvents } from "./userEvents/clickhouse";
 
-describe("findAllUserTraits", () => {
+describe.skip("findAllUserTraits", () => {
   let workspace: Workspace;
-  beforeEach(async () => {});
+  beforeEach(async () => {
+    await insertUserEvents({
+      tableVersion: config().defaultUserEventsTableVersion,
+      workspaceId: workspace.id,
+      events: [
+        {
+          // One day earlier than current time
+          processingTime: "2021-12-31 00:15:30",
+          messageRaw: segmentIdentifyEvent({
+            // userId,
+            // anonymousId,
+            // timestamp: "2021-12-31 00:15:00",
+            // traits: {
+            //   status: "onboarding",
+            // },
+          }),
+        },
+      ],
+    });
+  });
 });
