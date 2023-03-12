6104423e0dfd05ecab5ae3ed14b556be42fe1661
Max Gurewitz <max@dittofeed.com>
2023-03-12 10:15:01 -0700

    use upsert for bootstrapped user properties (#75)

--- packages/backend-lib/src/config.ts
+++ packages/backend-lib/src/config.ts
@@ -100,14 +100,6 @@ export type Config = Overwrite<
   }
 > & {
   defaultWorkspaceId: string;
-  defaultIdUserPropertyId: string;
-  defaultAnonymousIdIdUserPropertyId: string;
-  defaultEmailUserPropertyId: string;
-  defaultPhoneUserPropertyId: string;
-  defaultFirstNameUserPropertyId: string;
-  defaultLastNameUserPropertyId: string;
-  defaultLanguageUserPropertyId: string;
-  defaultAccountManagerUserPropertyId: string;
   defaultUserEventsTableVersion: string;
 };
 
@@ -218,30 +210,6 @@ function parseRawConfig(rawConfig: RawConfig): Config {
     temporalNamespace: rawConfig.temporalNamespace ?? "default",
     defaultWorkspaceId:
       rawConfig.defaultWorkspaceId ?? "024f3d0a-8eee-11ed-a1eb-0242ac120002",
-    defaultIdUserPropertyId:
-      rawConfig.defaultIdUserPropertyId ??
-      "eaeafca1-a8ec-49df-8359-43fef17a9794",
-    defaultAnonymousIdIdUserPropertyId:
-      rawConfig.defaultAnonymousIdIdUserPropertyId ??
-      "ad4c926b-dcae-4da4-b779-6e23061e6e17",
-    defaultEmailUserPropertyId:
-      rawConfig.defaultEmailUserPropertyId ??
-      "7e45987c-4a2c-42ef-867e-0a1e1e8a3a56",
-    defaultPhoneUserPropertyId:
-      rawConfig.defaultPhoneUserPropertyId ??
-      "462f651a-9c51-4658-9b65-67234280cedd",
-    defaultFirstNameUserPropertyId:
-      rawConfig.defaultFirstNameUserPropertyId ??
-      "352889dc-696b-42eb-a54a-9baa5fc62b18",
-    defaultLastNameUserPropertyId:
-      rawConfig.defaultLastNameUserPropertyId ??
-      "6f390a01-52cc-4308-be9e-ecabd5474a93",
-    defaultLanguageUserPropertyId:
-      rawConfig.defaultLanguageUserPropertyId ??
-      "6e0c65eb-3792-414b-9d56-fd7828b71ebe",
-    defaultAccountManagerUserPropertyId:
-      rawConfig.defaultAccountManagerUserPropertyId ??
-      "6e0c65eb-3792-414b-9d56-fd7828b71ebe",
     // UUID with _ instead of - for clickhouse compatibility
     defaultUserEventsTableVersion:
       rawConfig.defaultUserEventsTableVersion ??
