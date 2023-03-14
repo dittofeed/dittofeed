96b76f072685a4e7c5cfbbde715d290e7cb07458
Max Gurewitz <maxgurewitz@gmail.com>
2023-03-14 10:38:12 -0700

    api request handler factory

---
+++ packages/dashboard/src/lib/apiRequestHandlerFactory.ts
@@ -0,0 +1,58 @@
+import axios, { AxiosRequestConfig, AxiosResponse } from "axios";
+import { TSchema, Static } from "@sinclair/typebox";
+import { schemaValidate } from "isomorphic-lib/src/resultHandling/schemaValidation";
+import {
+  CompletionStatus,
+  EphemeralRequestStatus,
+} from "isomorphic-lib/src/types";
+
+export default function apiRequestHandlerFactory<S extends TSchema, E>({
+  request,
+  requestConfig,
+  setRequest,
+  responseSchema,
+  setResponse,
+}: {
+  requestConfig: AxiosRequestConfig;
+  request: EphemeralRequestStatus<E>;
+  setResponse: (response: Static<S>) => void;
+  responseSchema: S;
+  setRequest: (request: EphemeralRequestStatus<Error>) => void;
+}) {
+  return async function apiRequestHandler() {
+    if (request.type === CompletionStatus.InProgress) {
+      return;
+    }
+
+    setRequest({
+      type: CompletionStatus.InProgress,
+    });
+    let response: AxiosResponse;
+    try {
+      response = await axios(requestConfig);
+    } catch (e) {
+      const error = e as Error;
+
+      setRequest({
+        type: CompletionStatus.Failed,
+        error,
+      });
+      return;
+    }
+    const parsedResponse = schemaValidate(response.data, responseSchema);
+    if (parsedResponse.isErr()) {
+      console.error("unable to parse response", parsedResponse.error);
+
+      setRequest({
+        type: CompletionStatus.Failed,
+        error: new Error(JSON.stringify(parsedResponse.error)),
+      });
+      return;
+    }
+
+    setResponse(parsedResponse.value);
+    setRequest({
+      type: CompletionStatus.NotStarted,
+    });
+  };
+}
