e1bc4cf801958beb53c9a2ebd1f246d75a82a25b
Max Gurewitz <maxgurewitz@gmail.com>
2023-03-13 18:43:46 -0700

    script to generate segment curl

---
+++ packages/api/scripts/generateSegmentRequest.ts
@@ -0,0 +1,37 @@
+// SHARED_SECRET=**** yarn workspace api ts-node ./scripts/generateSegmentRequest.ts
+
+import backendConfig from "backend-lib/src/config";
+import { segmentIdentifyEvent } from "backend-lib/test/factories/segment";
+
+import { generateDigest } from "../src/crypto";
+
+function generateSegmentRequest() {
+  const sharedSecret = process.env.SHARED_SECRET;
+  if (!sharedSecret) {
+    throw new Error("Missing env variable SHARED_SECRET");
+  }
+  const env = backendConfig().nodeEnv;
+
+  const host = process.env.HOST ?? "localhost:3001";
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+  const traits = process.env.TRAITS ? JSON.parse(process.env.TRAITS) : {};
+
+  const payload = segmentIdentifyEvent({
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    traits: {
+      env,
+      ...traits,
+    },
+  });
+  const body = JSON.stringify(payload);
+  const signature = generateDigest({
+    sharedSecret,
+    rawBody: body,
+  });
+  // eslint-disable-next-line no-console
+  console.log(
+    `curl -X POST -H "x-signature:${signature}" -H "Content-Type: application/json" -d '${body}' '${host}/api/webhooks/segment'`
+  );
+}
+
+generateSegmentRequest();
