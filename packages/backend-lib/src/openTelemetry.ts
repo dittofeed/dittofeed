c91305699cf68705f2d5fdefbaa7a5c555461677
Max Gurewitz <max@dittofeed.com>
2023-03-18 15:17:20 -0700

    add telemetry to worker (#92)
    * upgrade temporal dependencies

--- packages/backend-lib/src/openTelemetry.ts
+++ packages/backend-lib/src/openTelemetry.ts
@@ -9,26 +9,35 @@ import { SemanticResourceAttributes } from "@opentelemetry/semantic-conventions"
 
 import config from "./config";
 
+export interface OpenTelemetry {
+  sdk: NodeSDK;
+  resource: Resource;
+  traceExporter: OTLPTraceExporter;
+  start: () => Promise<void>;
+}
+
 export function initOpenTelemetry({
   serviceName,
   configOverrides,
 }: {
   serviceName: string;
   configOverrides?: InstrumentationConfigMap;
-}) {
+}): OpenTelemetry {
   const { otelCollector, startOtel } = config();
-
-  const sdk = new NodeSDK({
-    resource: new Resource({
-      [SemanticResourceAttributes.SERVICE_NAME]: serviceName,
-    }),
-    instrumentations: [getNodeAutoInstrumentations(configOverrides)],
-    traceExporter: new OTLPTraceExporter({
-      url: otelCollector,
-    }),
+  const resource = new Resource({
+    [SemanticResourceAttributes.SERVICE_NAME]: serviceName,
+  });
+  const traceExporter = new OTLPTraceExporter({
+    url: otelCollector,
   });
 
-  return async function startOpentelemetry() {
+  const sdk = new NodeSDK({
+    resource,
+    instrumentations: [getNodeAutoInstrumentations(configOverrides)],
+    traceExporter,
+  });
+
+  const start = async function start() {
     if (!startOtel) {
       return;
     }
@@ -54,5 +63,12 @@ export function initOpenTelemetry({
       console.error("Error initializing tracing", error);
       process.exit(1);
     }
+  };
+
+  return {
+    start,
+    sdk,
+    resource,
+    traceExporter,
   };
 }
--- packages/worker/scripts/startWorker.ts
+++ packages/worker/scripts/startWorker.ts
@@ -1,14 +1,26 @@
 import {
+  makeWorkflowExporter,
+  OpenTelemetryActivityInboundInterceptor,
+} from "@temporalio/interceptors-opentelemetry/lib/worker";
+import {
   appendDefaultInterceptors,
+  defaultSinks,
   NativeConnection,
   Worker,
 } from "@temporalio/worker";
 import backendConfig from "backend-lib/src/config";
+import { initOpenTelemetry } from "backend-lib/src/openTelemetry";
 import * as activities from "backend-lib/src/temporal/activities";
 import { CustomActivityInboundInterceptor } from "backend-lib/src/temporal/activityInboundInterceptor";
 import connectWorkflowCLient from "backend-lib/src/temporal/connectWorkflowClient";
 
+import config from "../src/config";
+
 async function run() {
+  const otel = initOpenTelemetry({
+    serviceName: config().workerServiceName,
+  });
+
   const [connection, workflowClient] = await Promise.all([
     NativeConnection.connect({
       address: backendConfig().temporalAddress,
@@ -22,6 +34,10 @@ async function run() {
     workflowsPath: require.resolve("backend-lib/src/temporal/workflows"),
     activities,
     taskQueue: "default",
+    sinks: {
+      ...defaultSinks,
+      exporter: makeWorkflowExporter(otel.traceExporter, otel.resource),
+    },
     interceptors: appendDefaultInterceptors(
       {
         activityInbound: [
@@ -29,11 +45,14 @@ async function run() {
             new CustomActivityInboundInterceptor(ctx, {
               workflowClient,
             }),
+          (ctx) => new OpenTelemetryActivityInboundInterceptor(ctx),
         ],
       },
       console
     ),
+    enableSDKTracing: true,
   });
+  await otel.start();
   await worker.run();
 }
 
