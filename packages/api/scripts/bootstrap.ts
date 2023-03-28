f820fc44b33a3a0878eadfeec41899a192349c4f
Max Gurewitz <maxgurewitz@gmail.com>
2023-03-28 11:52:53 -0700

    log config from bootstrap script

--- packages/api/scripts/bootstrap.ts
+++ packages/api/scripts/bootstrap.ts
@@ -1,6 +1,14 @@
 import bootstrap from "backend-lib/src/bootstrap";
+import backendConfig from "backend-lib/src/config";
+import logger from "backend-lib/src/logger";
 
-bootstrap().catch((e) => {
+function boostrapStart() {
+  if (backendConfig().logConfig) {
+    logger().info(backendConfig(), "Initialized with config");
+  }
+  return bootstrap();
+}
+boostrapStart().catch((e) => {
   console.error(e);
   process.exit(1);
 });
