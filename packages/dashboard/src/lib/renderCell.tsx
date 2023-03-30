465af891f490f779e58c0b70cb400649c13f2e38
Max Gurewitz <maxgurewitz@gmail.com>
2023-03-29 22:00:37 -0700

    ignore linting error

--- packages/dashboard/src/lib/renderCell.tsx
+++ packages/dashboard/src/lib/renderCell.tsx
@@ -1,6 +1,7 @@
 import { Box, Tooltip } from "@mui/material";
 import React from "react";
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export default function renderCell(params: any) {
   return (
     <Tooltip title={params.value} placement="right-start">
