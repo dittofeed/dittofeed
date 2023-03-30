51d613ef7c35dc4c2df12a351838d4afef39f075
Max Gurewitz <max@dittofeed.com>
2023-03-30 16:18:17 -0700

    update add_segment_assignment_relation migration
    - wrap in transaction
    - set NOT NULL condition *after* data is copied to temp column
    - add configEnv script to api so that can run prisma scripts more easily on boxes

--- packages/backend-lib/prisma/migrations/20230328173619_add_segment_assignment_relation/migration.sql
+++ packages/backend-lib/prisma/migrations/20230328173619_add_segment_assignment_relation/migration.sql
@@ -5,7 +5,9 @@
 
 */
 -- AlterTable
-ALTER TABLE "SegmentAssignment" ADD COLUMN "segmentId_temp" UUID NOT NULL;
+BEGIN;
+
+ALTER TABLE "SegmentAssignment" ADD COLUMN "segmentId_temp" UUID;
 
 UPDATE "SegmentAssignment"
 SET "segmentId_temp" = CAST("segmentId" AS UUID);
@@ -14,8 +16,12 @@ ALTER TABLE "SegmentAssignment" DROP COLUMN "segmentId";
 
 ALTER TABLE "SegmentAssignment" RENAME COLUMN "segmentId_temp" TO "segmentId";
 
+ALTER TABLE "SegmentAssignment" ALTER COLUMN "segmentId" SET NOT NULL;
+
 -- CreateIndex
 CREATE UNIQUE INDEX "SegmentAssignment_workspaceId_userId_segmentId_key" ON "SegmentAssignment"("workspaceId", "userId", "segmentId");
 
 -- AddForeignKey
 ALTER TABLE "SegmentAssignment" ADD CONSTRAINT "SegmentAssignment_segmentId_fkey" FOREIGN KEY ("segmentId") REFERENCES "Segment"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+COMMIT;
