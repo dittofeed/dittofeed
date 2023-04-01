e0c4466a0a454af6f46930a3683a72ffcedc9720
Max Gurewitz <maxgurewitz@gmail.com>
2023-04-01 14:54:11 -0700

    add separate addInitialStateToProps for dashboard

---
+++ packages/dashboard/src/lib/addInitialStateToProps.ts
@@ -0,0 +1,24 @@
+import backendConfig from "backend-lib/src/config";
+
+import { PropsWithInitialState } from "./appStore";
+import { AppState } from "./types";
+
+export function addInitialStateToProps<T>(
+  props: T,
+  serverInitialState: Partial<AppState>
+): T & PropsWithInitialState {
+  // eslint-disable-next-line global-require, @typescript-eslint/no-var-requires
+  const { sourceControlProvider, enableSourceControl } = backendConfig();
+  const stateWithEnvVars: Partial<AppState> = {
+    apiBase: process.env.DASHBOARD_API_BASE ?? "http://localhost:3001",
+    sourceControlProvider,
+    enableSourceControl,
+    ...serverInitialState,
+  };
+  return {
+    ...props,
+    // the "stringify and then parse again" piece is required as next.js
+    // isn't able to serialize it to JSON properly
+    serverInitialState: JSON.parse(JSON.stringify(stateWithEnvVars)),
+  };
+}
