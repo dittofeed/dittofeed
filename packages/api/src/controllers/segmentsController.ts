d0abfb052be5e226360c3fdf3e327a0e41f713f8
Max Gurewitz <maxgurewitz@gmail.com>
2023-04-13 21:56:54 -0700

    noop broadcast endpoint

--- packages/api/src/controllers/segmentsController.ts
+++ packages/api/src/controllers/segmentsController.ts
@@ -4,6 +4,7 @@ import { Segment } from "backend-lib/src/types";
 import { FastifyInstance } from "fastify";
 import { schemaValidate } from "isomorphic-lib/src/resultHandling/schemaValidation";
 import {
+  BroadcastResource,
   DeleteSegmentRequest,
   DeleteSegmentResponse,
   SegmentDefinition,
@@ -196,65 +197,30 @@ export default async function segmentsController(fastify: FastifyInstance) {
     {
       schema: {
         description: "Submit a broadcast for a segment.",
-        body: UpsertSegmentResource,
+        body: BroadcastResource,
         response: {
-          200: SegmentResource,
+          200: BroadcastResource,
         },
       },
     },
     async (request, reply) => {
-      let segment: Segment;
-      const { id, name, definition, workspaceId } = request.body;
+      const { id, name, workspaceId, segmentId } = request.body;
 
-      const canCreate = workspaceId && name && definition;
+      const broadcast = await prisma().broadcast.upsert({
+        where: {
+          id,
+        },
+        create: {
+          name,
+          segmentId,
+          workspaceId,
+          id,
+          triggeredAt: new Date(),
+        },
+        update: {},
+      });
 
-      if (canCreate && id) {
-        segment = await prisma().segment.upsert({
-          where: {
-            id,
-          },
-          create: {
-            id,
-            workspaceId,
-            name,
-            definition,
-          },
-          update: {
-            workspaceId,
-            name,
-            definition,
-          },
-        });
-      } else {
-        segment = await prisma().segment.update({
-          where: {
-            id,
-          },
-          data: {
-            workspaceId,
-            name,
-            definition,
-          },
-        });
-      }
-
-      const segmentDefinitionResult = schemaValidate(
-        segment.definition,
-        SegmentDefinition
-      );
-
-      if (segmentDefinitionResult.isErr()) {
-        // TODO add logging
-        return reply.status(500).send();
-      }
-      const resource: SegmentResource = {
-        id: segment.id,
-        name: segment.name,
-        workspaceId: segment.workspaceId,
-        definition: segmentDefinitionResult.value,
-      };
-
-      return reply.status(200).send(resource);
+      return reply.status(200).send(broadcast);
     }
   );
 }
