31e14091ed526ce49d372d4bf215a110d1989fcb
Max Gurewitz <maxgurewitz@gmail.com>
2023-04-17 12:14:10 -0700

    install backend deps, rough draft of frontend

--- packages/dashboard/src/pages/dashboard/subscription-groups/upload/[id].page.tsx
+++ packages/dashboard/src/pages/dashboard/subscription-groups/upload/[id].page.tsx
@@ -1,5 +1,9 @@
+import { Button } from "@mui/material";
+import axios from "axios";
+import { CompletionStatus } from "isomorphic-lib/src/types";
 import { GetServerSideProps } from "next";
 import { useRouter } from "next/router";
+import { ChangeEvent, useState } from "react";
 
 import { PropsWithInitialState, useAppStore } from "../../../../lib/appStore";
 import getSubscriptionGroupsSSP from "../getSubscriptionGroupsSSP";
@@ -10,21 +14,55 @@ import SubscriptionGroupLayout, {
 export const getServerSideProps: GetServerSideProps<PropsWithInitialState> =
   getSubscriptionGroupsSSP;
 
+const oneMb = 1048576;
+
 export default function SubscriptionGroupConfig() {
   const path = useRouter();
+  const [file, setFile] = useState<File | null>(null);
 
   const id = typeof path.query.id === "string" ? path.query.id : undefined;
 
   const workspace = useAppStore((store) => store.workspace);
+
+  const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const uploadedFile = e.target.files ? e.target.files[0] : null;
+    if (uploadedFile && uploadedFile.size <= oneMb) {
+      setFile(uploadedFile);
+    } else {
+      setFile(null);
+    }
+  };
+
+  const handleSubmit = () => {
+    (async () => {
+      if (file && workspace.type === CompletionStatus.Successful) {
+        const formData = new FormData();
+        formData.append("csv", file);
+        formData.append("workspaceId", workspace.value.id);
+        await axios.post("/api/subscription-groups/upload", formData);
+      }
+    })();
+  };
 
   if (!id) {
     return null;
   }
 
   return (
-    <SubscriptionGroupLayout
-      tab={SubscriptionGroupTabLabel.Upload}
-      id={id}
-    ></SubscriptionGroupLayout>
+    <SubscriptionGroupLayout tab={SubscriptionGroupTabLabel.Upload} id={id}>
+      <Button variant="contained" component="label">
+        Choose CSV file of users
+        <input accept=".csv" type="file" hidden onChange={handleFileChange} />
+      </Button>
+      {file ? file.name : null}
+      <Button
+        variant="contained"
+        color="primary"
+        onClick={handleSubmit}
+        disabled={!file}
+      >
+        Upload
+      </Button>
+    </SubscriptionGroupLayout>
   );
 }
