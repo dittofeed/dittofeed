009db242bdc3ee6ad6577d9a3fbedd3418782dc9
Max Gurewitz <maxgurewitz@gmail.com>
2023-04-19 12:54:38 -0700

    render markdown as html

--- packages/isomorphic-lib/src/liquid.ts
+++ packages/isomorphic-lib/src/liquid.ts
@@ -1,9 +1,17 @@
 import { Liquid } from "liquidjs";
+import MarkdownIt from "markdown-it";
+
+const md = new MarkdownIt({
+  html: true,
+  breaks: true,
+});
 
 export const liquidEngine = new Liquid({
   strictVariables: true,
   lenientIf: true,
 });
+
+liquidEngine.registerFilter("markdown", (value) => md.render(value));
 
 export function renderWithUserProperties({
   template,
