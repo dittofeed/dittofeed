0128abb91d839f9c3704a38bc0ce63f2466724b4
Max Gurewitz <maxgurewitz@gmail.com>
2023-04-19 18:01:56 -0700

    failing test

--- packages/isomorphic-lib/src/liquid.test.ts
+++ packages/isomorphic-lib/src/liquid.test.ts
@@ -38,6 +38,25 @@ const expectedBaseLayoutTemplate = `
 </html>
 `;
 
+const markdownEmailTemplate = `
+{% layout 'markdown-email' %}
+{% block content %}
+## Welcome, {{ user.name }}
+
+- Point 1
+- Point 2
+{% endblock %}
+`;
+
+const expectedRenderedMarkdownEmail = `
+<h2>Welcome, Max</h2>
+<p>You are a <em>Co-Founder</em> at our company.</p>
+<ul>
+<li>Point 1</li>
+<li>Point 2</li>
+</ul>
+`;
+
 describe("renderWithUserProperties", () => {
   it("can render markdown that passes email validation", async () => {
     const rendered = renderWithUserProperties({
@@ -56,5 +75,15 @@ describe("renderWithUserProperties", () => {
       userProperties: {},
     });
     expect(rendered.trim()).toEqual(expectedBaseLayoutTemplate.trim());
+  });
+
+  it("can render markdown email layout", async () => {
+    const rendered = renderWithUserProperties({
+      template: markdownEmailTemplate,
+      userProperties: {
+        name: "Max",
+      },
+    });
+    expect(rendered.trim()).toEqual(expectedRenderedMarkdownEmail.trim());
   });
 });
