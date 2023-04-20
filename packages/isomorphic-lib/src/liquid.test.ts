05756eeff9c0751b29f59c891499aac861f84f67
Max Gurewitz <maxgurewitz@gmail.com>
2023-04-19 18:03:36 -0700

    less failing

--- packages/isomorphic-lib/src/liquid.test.ts
+++ packages/isomorphic-lib/src/liquid.test.ts
@@ -49,12 +49,21 @@ const markdownEmailTemplate = `
 `;
 
 const expectedRenderedMarkdownEmail = `
+<!DOCTYPE html>
+<html>
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <style type="text/css"></style>
+</head>
+<body>
 <h2>Welcome, Max</h2>
-<p>You are a <em>Co-Founder</em> at our company.</p>
 <ul>
 <li>Point 1</li>
 <li>Point 2</li>
 </ul>
+</body>
+</html>
 `;
 
 describe("renderWithUserProperties", () => {
