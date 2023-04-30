7cd82e7442513c7b7fd156968b1e55fb4f38c117
Max Gurewitz <maxgurewitz@gmail.com>
2023-04-29 20:59:31 -0700

    decode jwt

--- packages/api/src/buildApp/requestContext.ts
+++ packages/api/src/buildApp/requestContext.ts
@@ -1,19 +1,30 @@
+import { Type } from "@sinclair/typebox";
 import logger from "backend-lib/src/logger";
 import prisma from "backend-lib/src/prisma";
+import { createDecoder } from "fast-jwt";
 import { FastifyInstance } from "fastify";
 import fp from "fastify-plugin";
 
 import { getWorkspaceIdFromReq } from "../workspace";
 
+const decoder = createDecoder();
+
+const DecodedJwt = Type.Object({
+  sub: Type.String(),
+  email: Type.String(),
+  picture: Type.Optional(Type.String()),
+});
+
 // eslint-disable-next-line @typescript-eslint/require-await
 const requestContext = fp(async (fastify: FastifyInstance) => {
   fastify.addHook("preHandler", async (request, reply) => {
-    logger().debug(
-      {
-        headers: request.headers,
-      },
-      "setting request context"
-    );
+    const { authorization } = request.headers;
+    const bearerToken = authorization?.replace("Bearer ", "");
+    const decoded: unknown | null = bearerToken ? decoder(bearerToken) : null;
+    logger().debug({
+      decoded,
+    });
+
     const workspaceId = getWorkspaceIdFromReq(request);
 
     const workspace = await prisma().workspace.findUnique({
