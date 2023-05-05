c9fecf92a93114b429f475760d46ad418bbc8d50
Max Gurewitz <maxgurewitz@gmail.com>
2023-05-05 09:08:32 -0700

    require workspace id in multitenant bootstrap

--- packages/api/scripts/bootstrap.ts
+++ packages/api/scripts/bootstrap.ts
@@ -1,13 +1,36 @@
 import bootstrap from "backend-lib/src/bootstrap";
 import backendConfig from "backend-lib/src/config";
 import logger from "backend-lib/src/logger";
+import yargs from "yargs";
+import { hideBin } from "yargs/helpers";
 
-function boostrapStart() {
+async function boostrapStart() {
   if (backendConfig().logConfig) {
     logger().info(backendConfig(), "Initialized with config");
   }
-  return bootstrap();
+  let workspaceId: string;
+  if (backendConfig().authMode === "multi-tenant") {
+    const argv = await yargs(hideBin(process.argv))
+      .options({
+        workspaceId: { type: "string", demandOption: true },
+      })
+      .strict()
+      .parse();
+    workspaceId = argv.workspaceId;
+  } else {
+    const argv = await yargs(hideBin(process.argv))
+      .options({
+        workspaceId: { type: "string" },
+      })
+      .strict()
+      .parse();
+
+    workspaceId = argv.workspaceId ?? backendConfig().defaultWorkspaceId;
+  }
+
+  return bootstrap({ workspaceId });
 }
+
 boostrapStart().catch((e) => {
   console.error(e);
   process.exit(1);
