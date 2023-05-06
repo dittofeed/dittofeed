6d0087c7d5e63a2d63c36222f6520807e62555a3
Max Gurewitz <maxgurewitz@gmail.com>
2023-05-06 14:43:39 -0700

    add workspace name to bootstrap

--- packages/api/scripts/bootstrap.ts
+++ packages/api/scripts/bootstrap.ts
@@ -9,26 +9,32 @@ async function boostrapStart() {
     logger().info(backendConfig(), "Initialized with config");
   }
   let workspaceId: string;
+  let workspaceName: string;
+
   if (backendConfig().authMode === "multi-tenant") {
     const argv = await yargs(hideBin(process.argv))
       .options({
         workspaceId: { type: "string", demandOption: true },
+        workspaceName: { type: "string", demandOption: true },
       })
       .strict()
       .parse();
     workspaceId = argv.workspaceId;
+    workspaceName = argv.workspaceName;
   } else {
     const argv = await yargs(hideBin(process.argv))
       .options({
         workspaceId: { type: "string" },
+        workspaceName: { type: "string" },
       })
       .strict()
       .parse();
 
     workspaceId = argv.workspaceId ?? backendConfig().defaultWorkspaceId;
+    workspaceName = argv.workspaceName ?? "Default";
   }
 
-  return bootstrap({ workspaceId });
+  return bootstrap({ workspaceId, workspaceName });
 }
 
 boostrapStart().catch((e) => {
