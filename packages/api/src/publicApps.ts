a8f0ad3f8704dea680816054079b23496a67a1a4
Max Gurewitz <maxgurewitz@gmail.com>
2023-05-27 09:08:55 -0700

    public apps api

---
+++ packages/api/src/publicApps.ts
@@ -0,0 +1,60 @@
+import { TypeBoxTypeProvider } from "@fastify/type-provider-typebox";
+import { Type } from "@sinclair/typebox";
+import backendConfig from "backend-lib/src/config";
+import { generateDigest } from "backend-lib/src/crypto";
+import prisma from "backend-lib/src/prisma";
+import { insertUserEvents } from "backend-lib/src/userEvents";
+import { FastifyInstance } from "fastify";
+import { WORKSPACE_ID_HEADER } from "isomorphic-lib/src/constants";
+import { WorkspaceId } from "isomorphic-lib/src/types";
+
+// eslint-disable-next-line @typescript-eslint/require-await
+export default async function publicAppsController(fastify: FastifyInstance) {
+  fastify.withTypeProvider<TypeBoxTypeProvider>().post(
+    "/identity",
+    {
+      schema: {},
+    },
+    async (request, reply) => {}
+  );
+
+  fastify.withTypeProvider<TypeBoxTypeProvider>().post(
+    "/track",
+    {
+      schema: {},
+    },
+    async (request, reply) => {}
+  );
+
+  fastify.withTypeProvider<TypeBoxTypeProvider>().post(
+    "/page",
+    {
+      schema: {},
+    },
+    async (request, reply) => {}
+  );
+
+  fastify.withTypeProvider<TypeBoxTypeProvider>().post(
+    "/group",
+    {
+      schema: {},
+    },
+    async (request, reply) => {}
+  );
+
+  fastify.withTypeProvider<TypeBoxTypeProvider>().post(
+    "/alias",
+    {
+      schema: {},
+    },
+    async (request, reply) => {}
+  );
+
+  fastify.withTypeProvider<TypeBoxTypeProvider>().post(
+    "/batch",
+    {
+      schema: {},
+    },
+    async (request, reply) => {}
+  );
+}
