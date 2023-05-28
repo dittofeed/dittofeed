04ac94e421fcecf02973b8ad8a622fdafbf52f0d
Max Gurewitz <maxgurewitz@gmail.com>
2023-05-28 10:34:56 -0700

    write key model

---
+++ packages/backend-lib/prisma/migrations/20230528173025_add_writekey_model/migration.sql
@@ -0,0 +1,16 @@
+-- CreateTable
+CREATE TABLE "WriteKey" (
+    "id" UUID NOT NULL,
+    "workspaceId" UUID NOT NULL,
+    "secretId" UUID NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "WriteKey_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "WriteKey" ADD CONSTRAINT "WriteKey_workspaceId_fkey" FOREIGN KEY ("workspaceId") REFERENCES "Workspace"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "WriteKey" ADD CONSTRAINT "WriteKey_secretId_fkey" FOREIGN KEY ("secretId") REFERENCES "Secret"("id") ON DELETE CASCADE ON UPDATE CASCADE;
