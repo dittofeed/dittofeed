09fd31394ea03d37ffcfe7d59f073f959ffb6f57
Max Gurewitz <max@dittofeed.com>
2023-06-01 15:31:06 -0700

    dashboard otel (#223)
    - ensures otel is included on the dashboard backend
    - update nextjs
    - update docs for running with otel locally
    - correct typo in telemetry workspace id attribute

---
+++ packages/dashboard/src/instrumentation.page.ts
@@ -0,0 +1,19 @@
+export async function register() {
+  if (process.env.NEXT_RUNTIME === "nodejs") {
+    const { initOpenTelemetry } = await import("backend-lib/src/openTelemetry");
+
+    // TODO add request context to span
+    const { start } = await initOpenTelemetry({
+      serviceName: "dittofeed-dashboard",
+      configOverrides: {
+        "@opentelemetry/instrumentation-http": {
+          ignoreIncomingPaths: ["/dashboard/api"],
+        },
+        "@opentelemetry/instrumentation-fs": {
+          enabled: false,
+        },
+      },
+    });
+    await start();
+  }
+}
