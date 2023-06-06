372840be1e2d7a3bf2d4edc34fe657598571fe4d
Max Gurewitz <maxgurewitz@gmail.com>
2023-06-06 10:34:50 -0700

    mobile push templates file

---
+++ packages/backend-lib/src/mobilePushTemplates.ts
@@ -0,0 +1,19 @@
+import { randomUUID } from "crypto";
+
+import { MobilePushTemplateResource, TemplateResourceType } from "./types";
+
+export async function getMobilePushTemplates({
+  workspaceId,
+}: {
+  workspaceId: string;
+}): Promise<MobilePushTemplateResource[]> {
+  return [
+    {
+      type: TemplateResourceType.MobilePush,
+      id: randomUUID(),
+      workspaceId,
+      message: "Hello, {{user.firstName}}!",
+      title: "Hello",
+    },
+  ];
+}
