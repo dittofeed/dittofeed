12ae530b8054c06c72c039009b64c10c52a9d4ef
Max Gurewitz <maxgurewitz@gmail.com>
2023-06-06 17:07:41 -0700

    sendgrid webhook endpoint

--- packages/api/src/controllers/webhooksController.ts
+++ packages/api/src/controllers/webhooksController.ts
@@ -1,7 +1,7 @@
 import { TypeBoxTypeProvider } from "@fastify/type-provider-typebox";
 import { Type } from "@sinclair/typebox";
-import backendConfig from "backend-lib/src/config";
 import { generateDigest } from "backend-lib/src/crypto";
+import logger from "backend-lib/src/logger";
 import prisma from "backend-lib/src/prisma";
 import { insertUserEvents } from "backend-lib/src/userEvents";
 import { FastifyInstance } from "fastify";
@@ -12,6 +12,46 @@ import { getWorkspaceIdFromReq } from "../workspace";
 
 // eslint-disable-next-line @typescript-eslint/require-await
 export default async function webhookController(fastify: FastifyInstance) {
+  fastify.withTypeProvider<TypeBoxTypeProvider>().post(
+    "/sendgrid",
+    {
+      schema: {
+        description: "Used to consume sendgrid webhook payloads.",
+      },
+    },
+    async (request, reply) => {
+      // headers: {
+      //   "host": "ce99-23-227-237-252.ngrok.io",
+      //   "user-agent": "SendGrid Event API",
+      //   "content-length": "3741",
+      //   "accept-encoding": "gzip",
+      //   "content-type": "application/json",
+      //   "x-forwarded-for": "54.70.106.128",
+      //   "x-forwarded-proto": "https",
+      //   "x-twilio-email-event-webhook-signature": "MEUCIQCzyoxjP6ZKiDJ+f+7SCa/g5TvHWSafzJLMH2kIaw0kygIgask3F/i8BIm/YD1Do1ukjkLK5s+X0PrfR7LpdlAO098=",
+      //   "x-twilio-email-event-webhook-timestamp": "1686096408"
+      // }
+      // body: [
+      //   {
+      //     "email": "example@test.com",
+      //     "timestamp": 1686096326,
+      //     "smtp-id": "<14c5d75ce93.dfd.64b469@ismtpd-555>",
+      //     "event": "processed",
+      //     "category": [
+      //       "cat facts"
+      //     ],
+      //     "sg_event_id": "ZCC3wcasi5Riq3JNT0dAVA==",
+      //     "sg_message_id": "14c5d75ce93.dfd.64b469.filter0001.16648.5515E0B88.0"
+      //   },
+      logger().debug(
+        { headers: request.headers, body: request.body },
+        "sendgrid webhook payload"
+      );
+
+      return reply.status(200).send();
+    }
+  );
+
   fastify.withTypeProvider<TypeBoxTypeProvider>().post(
     "/segment",
     {
