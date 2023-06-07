0427130c247bb420dc115d36d8ba81cea229eab1
Max Gurewitz <maxgurewitz@gmail.com>
2023-06-07 10:40:04 -0700

    working sendgrid verification

--- packages/api/src/controllers/webhooksController.ts
+++ packages/api/src/controllers/webhooksController.ts
@@ -8,7 +8,10 @@ import logger from "backend-lib/src/logger";
 import prisma from "backend-lib/src/prisma";
 import { insertUserEvents } from "backend-lib/src/userEvents";
 import { FastifyInstance } from "fastify";
-import { WORKSPACE_ID_HEADER } from "isomorphic-lib/src/constants";
+import {
+  SENDGRID_WEBHOOK_SECRET_NAME,
+  WORKSPACE_ID_HEADER,
+} from "isomorphic-lib/src/constants";
 import { WorkspaceId } from "isomorphic-lib/src/types";
 
 import { getWorkspaceIdFromReq } from "../workspace";
@@ -21,46 +24,63 @@ export default async function webhookController(fastify: FastifyInstance) {
       schema: {
         description: "Used to consume sendgrid webhook payloads.",
         headers: Type.Object({
-          "X-Twilio-Email-Event-Webhook-Signature": Type.String(),
-          "X-Twilio-Email-Event-Webhook-Timestamp": Type.String(),
+          "x-twilio-email-event-webhook-signature": Type.String(),
+          "x-twilio-email-event-webhook-timestamp": Type.String(),
         }),
-        body: Type.Array(Type.Object({})),
+        body: Type.Array(
+          Type.Object({
+            custom_args: Type.Optional(
+              Type.Record(Type.String(), Type.String())
+            ),
+          }),
+          { additionalProperties: true }
+        ),
       },
     },
     async (request, reply) => {
-      // const verified = verifyTimestampedSignature({
-      //   signature: request.headers["X-Twilio-Email-Event-Webhook-Signature"],
-      //   timestamp: request.headers["X-Twilio-Email-Event-Webhook-Timestamp"],
-      //   payload: request.rawBody as string,
-      //   secret: process.env.SENDGRID_WEBHOOK_SECRET,
-      // });
-      // headers: {
-      //   "host": "ce99-23-227-237-252.ngrok.io",
-      //   "user-agent": "SendGrid Event API",
-      //   "content-length": "3741",
-      //   "accept-encoding": "gzip",
-      //   "content-type": "application/json",
-      //   "x-forwarded-for": "54.70.106.128",
-      //   "x-forwarded-proto": "https",
-      //   "x-twilio-email-event-webhook-signature": "MEUCIQCzyoxjP6ZKiDJ+f+7SCa/g5TvHWSafzJLMH2kIaw0kygIgask3F/i8BIm/YD1Do1ukjkLK5s+X0PrfR7LpdlAO098=",
-      //   "x-twilio-email-event-webhook-timestamp": "1686096408"
-      // }
-      // body: [
-      //   {
-      //     "email": "example@test.com",
-      //     "timestamp": 1686096326,
-      //     "smtp-id": "<14c5d75ce93.dfd.64b469@ismtpd-555>",
-      //     "event": "processed",
-      //     "category": [
-      //       "cat facts"
-      //     ],
-      //     "sg_event_id": "ZCC3wcasi5Riq3JNT0dAVA==",
-      //     "sg_message_id": "14c5d75ce93.dfd.64b469.filter0001.16648.5515E0B88.0"
-      //   },
-      logger().debug(
-        { headers: request.headers, body: request.body },
-        "sendgrid webhook payload"
-      );
+      const workspaceId = request.body[0]?.custom_args?.workspaceId;
+
+      if (!workspaceId) {
+        return reply.status(400).send({
+          error: "Missing workspaceId custom arg.",
+        });
+      }
+
+      const secret = await prisma().secret.findUnique({
+        where: {
+          workspaceId_name: {
+            name: SENDGRID_WEBHOOK_SECRET_NAME,
+            workspaceId,
+          },
+        },
+      });
+
+      if (!secret) {
+        return reply.status(400).send({
+          error: "Missing secret.",
+        });
+      }
+
+      const publicKey = `-----BEGIN PUBLIC KEY-----\n${secret.value}\n-----END PUBLIC KEY-----`;
+
+      if (!request.rawBody || typeof request.rawBody !== "string") {
+        logger().error("Missing rawBody.");
+        return reply.status(500).send();
+      }
+
+      const verified = verifyTimestampedSignature({
+        signature: request.headers["x-twilio-email-event-webhook-signature"],
+        timestamp: request.headers["x-twilio-email-event-webhook-timestamp"],
+        payload: request.rawBody,
+        publicKey,
+      });
+
+      if (!verified) {
+        logger().error("Invalid signature.");
+        return reply.status(401).send({
+          message: "Invalid signature.",
+        });
+      }
 
       return reply.status(200).send();
     }
