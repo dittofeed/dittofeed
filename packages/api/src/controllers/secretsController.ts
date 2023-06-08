f132960fe8e8bcd608ee5d6cfd442c66ae9341ac
Max Gurewitz <maxgurewitz@gmail.com>
2023-06-07 17:00:13 -0700

    secrets controller

--- packages/api/src/controllers/secretsController.ts
+++ packages/api/src/controllers/secretsController.ts
@@ -1,14 +1,46 @@
-import { TypeBoxTypeProvider } from "@fastify/type-provider-typebox";
+import { Type, TypeBoxTypeProvider } from "@fastify/type-provider-typebox";
 import prisma from "backend-lib/src/prisma";
 import { FastifyInstance } from "fastify";
 import {
   DeleteSecretRequest,
   EmptyResponse,
+  ListSecretsRequest,
+  SecretResource,
   UpsertSecretRequest,
 } from "isomorphic-lib/src/types";
 
 // eslint-disable-next-line @typescript-eslint/require-await
 export default async function secretsController(fastify: FastifyInstance) {
+  fastify.withTypeProvider<TypeBoxTypeProvider>().get(
+    "/",
+    {
+      schema: {
+        description: "List secrets.",
+        querystring: ListSecretsRequest,
+        response: {
+          200: Type.Array(SecretResource),
+        },
+      },
+    },
+    async (request, reply) => {
+      const { workspaceId, name } = request.query;
+
+      const secrets = (
+        await prisma().secret.findMany({
+          where: {
+            workspaceId,
+            name,
+          },
+        })
+      ).map((secret) => ({
+        workspaceId: secret.workspaceId,
+        name: secret.name,
+        value: secret.value,
+      }));
+      return reply.status(200).send(secrets);
+    }
+  );
+
   fastify.withTypeProvider<TypeBoxTypeProvider>().put(
     "/",
     {
