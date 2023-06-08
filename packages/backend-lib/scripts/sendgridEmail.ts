a4a6b882adda4d66762436410e08c46be3ed90f3
Max Gurewitz <maxgurewitz@gmail.com>
2023-06-08 10:59:40 -0700

    script for sending sendgridEmail

---
+++ packages/backend-lib/scripts/sendgridEmail.ts
@@ -0,0 +1,53 @@
+// yarn workspace backend-lib ts-node scripts/sendgridEmail.ts --to=name@dittofeed.com --from=support@dittofeed.com
+import { randomUUID } from "crypto";
+import yargs from "yargs";
+import { hideBin } from "yargs/helpers";
+
+import config from "../src/config";
+import { sendMail } from "../src/destinations/sendgrid";
+import logger from "../src/logger";
+import prisma from "../src/prisma";
+
+async function sendgridEmail() {
+  const argv = await yargs(hideBin(process.argv))
+    .options({
+      to: { type: "string", demandOption: true },
+      from: { type: "string", demandOption: true },
+    })
+    .strict()
+    .parse();
+
+  const defaultEmailProvider = await prisma().defaultEmailProvider.findUnique({
+    where: {
+      workspaceId: config().defaultWorkspaceId,
+    },
+    include: { emailProvider: true },
+  });
+  if (!defaultEmailProvider) {
+    throw new Error("Default email provider not found");
+  }
+  const result = await sendMail({
+    mailData: {
+      to: argv.to,
+      from: argv.from,
+      subject: "Test email",
+      html: "<h1>Test email</h1>",
+      customArgs: {
+        journeyId: randomUUID(),
+        runId: randomUUID(),
+        messageId: randomUUID(),
+        userId: randomUUID(),
+        workspaceId: config().defaultWorkspaceId,
+        templateId: randomUUID(),
+        nodeId: randomUUID(),
+      },
+    },
+    apiKey: defaultEmailProvider.emailProvider.apiKey,
+  });
+  logger().info({ result }, "Sent email");
+}
+
+sendgridEmail().catch((e) => {
+  console.error(e);
+  process.exit(1);
+});
