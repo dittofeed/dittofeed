bcec5682957308d69461f26f872ed87f1a8543cf
Max Gurewitz <maxgurewitz@gmail.com>
2023-06-21 18:13:41 -0700

    correct channels

--- packages/backend-lib/prisma/migrations/20230622011312_remove_channels/migration.sql
+++ packages/backend-lib/prisma/migrations/20230622011312_remove_channels/migration.sql
@@ -3,8 +3,12 @@
 
   - You are about to drop the column `channelId` on the `SubscriptionGroup` table. All the data in the column will be lost.
   - You are about to drop the `Channel` table. If the table is not empty, all the data it contains will be lost.
+  - Added the required column `channel` to the `SubscriptionGroup` table without a default value. This is not possible if the table is not empty.
 
 */
+-- CreateEnum
+CREATE TYPE "DBChannelType" AS ENUM ('Email', 'MobilePush');
+
 -- DropForeignKey
 ALTER TABLE "Channel" DROP CONSTRAINT "Channel_workspaceId_fkey";
 
@@ -12,7 +16,8 @@ ALTER TABLE "Channel" DROP CONSTRAINT "Channel_workspaceId_fkey";
 ALTER TABLE "SubscriptionGroup" DROP CONSTRAINT "SubscriptionGroup_channelId_fkey";
 
 -- AlterTable
-ALTER TABLE "SubscriptionGroup" DROP COLUMN "channelId";
+ALTER TABLE "SubscriptionGroup" DROP COLUMN "channelId",
+ADD COLUMN     "channel" "DBChannelType" NOT NULL;
 
 -- DropTable
 DROP TABLE "Channel";
