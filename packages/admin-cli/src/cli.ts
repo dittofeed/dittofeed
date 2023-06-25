2ccd39321ce181628beb3ec0dc08053f68572828
Max Gurewitz <maxgurewitz@gmail.com>
2023-06-25 10:37:05 -0700

    add yargs

--- packages/admin-cli/src/cli.ts
+++ packages/admin-cli/src/cli.ts
@@ -1,3 +1,24 @@
+import yargs from "yargs";
+
 export async function cli() {
-  console.log("hello world");
+  // eslint-disable-next-line @typescript-eslint/no-unused-expressions
+  yargs
+    .scriptName("admin")
+    .usage("$0 <cmd> [args]")
+    .command(
+      "hello [name]",
+      "welcome ter yargs!",
+      (yargsInner) => {
+        yargsInner.positional("name", {
+          type: "string",
+          default: "Cambi",
+          describe: "the name to say hello to",
+        });
+      },
+      (argv) => {
+        console.log("hello", argv.name, "welcome to yargs!");
+      }
+    )
+    .demandCommand(1, "# Please provide a valid command")
+    .help().argv;
 }
