578f6763c650cf808349faf477ab2a07e14e8e65
Max Gurewitz <maxgurewitz@gmail.com>
2023-06-26 18:11:28 -0700

    sdk base codegen

--- packages/admin-cli/src/cli.ts
+++ packages/admin-cli/src/cli.ts
@@ -65,7 +65,7 @@ export async function cli() {
     )
     .command(
       "sdk-base-codegen",
-      "Generates an openapi client for a particular language's base sdk. Note that this requires swagger-codegen 3 to be installed https://github.com/swagger-api/swagger-codegen#prerequisites",
+      "Generates an openapi client for a particular language's base sdk. Note that this requires:\n* swagger-codegen 3 to be installed (https://github.com/swagger-api/swagger-codegen).\n* The api server to be running.",
       (cmd) =>
         cmd.options({
           lang: {
