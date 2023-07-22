44912b23d41f1b422352f8aa24257e13d8a2d8bb
Max Gurewitz <max@dittofeed.com>
2023-07-22 16:55:39 -0700

    remove fixme comment

--- packages/isomorphic-lib/src/protectedUserProperties.ts
+++ packages/isomorphic-lib/src/protectedUserProperties.ts
@@ -1,3 +1,2 @@
 const protectedUserProperties = new Set<string>(["id", "anonymousId"]);
-// FIXME add second protected category, for user properties that can be edited but not deleted
 export default protectedUserProperties;
