9df7981bbc2429056f9397b6c59eb1926f5012ad
Max Gurewitz <max@dittofeed.com>
2023-07-23 17:13:27 -0700

    wip

--- packages/dashboard/src/components/journeys/store.test.ts
+++ packages/dashboard/src/components/journeys/store.test.ts
@@ -16,238 +16,240 @@ describe("journeyToState", () => {
   let journeyId: string;
   let workspaceId: string;
 
-  beforeEach(() => {
-    journeyId = uuid();
-    workspaceId = uuid();
+  describe("when journey has split then delay", () => {
+    beforeEach(() => {
+      journeyId = uuid();
+      workspaceId = uuid();
 
-    journeyResource = {
-      id: journeyId,
-      name: "My Journey",
-      status: "NotStarted",
-      definition: {
-        entryNode: {
-          type: JourneyNodeType.EntryNode,
-          segment: uuid(),
-          child: "908b9795-60b7-4333-a57c-a30f4972fb6b",
+      journeyResource = {
+        id: journeyId,
+        name: "My Journey",
+        status: "NotStarted",
+        definition: {
+          entryNode: {
+            type: JourneyNodeType.EntryNode,
+            segment: uuid(),
+            child: "908b9795-60b7-4333-a57c-a30f4972fb6b",
+          },
+          exitNode: {
+            type: JourneyNodeType.ExitNode,
+          },
+          nodes: [
+            {
+              id: "908b9795-60b7-4333-a57c-a30f4972fb6b",
+              type: JourneyNodeType.MessageNode,
+              child: "6940ebec-a2ca-47dc-a356-42dc0245dd2e",
+              variant: {
+                type: ChannelType.Email,
+                templateId: uuid(),
+              },
+            },
+            {
+              id: "6940ebec-a2ca-47dc-a356-42dc0245dd2e",
+              type: JourneyNodeType.DelayNode,
+              child: "9d5367b0-882e-49c2-a6d2-4c28e5416d04",
+              variant: {
+                type: DelayVariantType.Second,
+                seconds: 1800,
+              },
+            },
+            {
+              id: "9d5367b0-882e-49c2-a6d2-4c28e5416d04",
+              type: JourneyNodeType.SegmentSplitNode,
+              variant: {
+                type: SegmentSplitVariantType.Boolean,
+                segment: uuid(),
+                trueChild: "6ce89301-2a35-4562-b1db-54689bfe0e05",
+                falseChild: "ExitNode",
+              },
+            },
+            {
+              id: "6ce89301-2a35-4562-b1db-54689bfe0e05",
+              type: JourneyNodeType.MessageNode,
+              child: JourneyNodeType.ExitNode,
+              variant: {
+                type: ChannelType.Email,
+                templateId: uuid(),
+              },
+            },
+          ],
         },
-        exitNode: {
-          type: JourneyNodeType.ExitNode,
-        },
-        nodes: [
+        workspaceId,
+      };
+    });
+
+    it("produces the correct ui state", () => {
+      const uiState = journeyToState(journeyResource);
+      expect(uiState).toEqual({
+        journeyNodes: expect.arrayContaining([
+          {
+            id: "EntryNode",
+            position: { x: 400, y: 100 },
+            type: "journey",
+            data: {
+              type: "JourneyNode",
+              nodeTypeProps: {
+                type: "EntryNode",
+                segmentId: journeyResource.definition.entryNode.segment,
+              },
+            },
+          },
           {
             id: "908b9795-60b7-4333-a57c-a30f4972fb6b",
-            type: JourneyNodeType.MessageNode,
-            child: "6940ebec-a2ca-47dc-a356-42dc0245dd2e",
-            variant: {
-              type: ChannelType.Email,
-              templateId: uuid(),
+            position: { x: 400, y: 300 },
+            type: "journey",
+            data: {
+              type: "JourneyNode",
+              nodeTypeProps: {
+                type: "MessageNode",
+                templateId: journeyResource.definition.nodes.flatMap((n) =>
+                  n.type === JourneyNodeType.MessageNode &&
+                  n.id === "908b9795-60b7-4333-a57c-a30f4972fb6b"
+                    ? n
+                    : []
+                )[0]?.variant.templateId,
+                name: "Message - 908b9795-60b7-4333-a57c-a30f4972fb6b",
+              },
             },
           },
           {
             id: "6940ebec-a2ca-47dc-a356-42dc0245dd2e",
-            type: JourneyNodeType.DelayNode,
-            child: "9d5367b0-882e-49c2-a6d2-4c28e5416d04",
-            variant: {
-              type: DelayVariantType.Second,
-              seconds: 1800,
+            position: { x: 400, y: 500 },
+            type: "journey",
+            data: {
+              type: "JourneyNode",
+              nodeTypeProps: { type: "DelayNode", seconds: 1800 },
             },
           },
           {
             id: "9d5367b0-882e-49c2-a6d2-4c28e5416d04",
-            type: JourneyNodeType.SegmentSplitNode,
-            variant: {
-              type: SegmentSplitVariantType.Boolean,
-              segment: uuid(),
-              trueChild: "6ce89301-2a35-4562-b1db-54689bfe0e05",
-              falseChild: "ExitNode",
+            position: { x: 400, y: 700 },
+            type: "journey",
+            data: {
+              type: "JourneyNode",
+              nodeTypeProps: {
+                type: "SegmentSplitNode",
+                name: "True / False Branch",
+                segmentId: journeyResource.definition.nodes.flatMap((n) =>
+                  n.type === JourneyNodeType.SegmentSplitNode &&
+                  n.id === "9d5367b0-882e-49c2-a6d2-4c28e5416d04"
+                    ? n
+                    : []
+                )[0]?.variant.segment,
+                trueLabelNodeId: expect.any(String),
+                falseLabelNodeId: expect.any(String),
+              },
             },
           },
           {
+            id: expect.any(String),
+            position: { x: 200, y: 900 },
+            type: "label",
+            data: { type: "LabelNode", title: "true" },
+          },
+          {
+            id: expect.any(String),
+            position: { x: 600, y: 900 },
+            type: "label",
+            data: { type: "LabelNode", title: "false" },
+          },
+          {
             id: "6ce89301-2a35-4562-b1db-54689bfe0e05",
-            type: JourneyNodeType.MessageNode,
-            child: JourneyNodeType.ExitNode,
-            variant: {
-              type: ChannelType.Email,
-              templateId: uuid(),
+            position: { x: 200, y: 1100 },
+            type: "journey",
+            data: {
+              type: "JourneyNode",
+              nodeTypeProps: {
+                type: "MessageNode",
+                templateId: journeyResource.definition.nodes.flatMap((n) =>
+                  n.type === JourneyNodeType.MessageNode &&
+                  n.id === "6ce89301-2a35-4562-b1db-54689bfe0e05"
+                    ? n
+                    : []
+                )[0]?.variant.templateId,
+                name: "Message - 6ce89301-2a35-4562-b1db-54689bfe0e05",
+              },
             },
+          },
+          {
+            id: expect.any(String),
+            position: { x: 400, y: 1300 },
+            type: "empty",
+            data: { type: "EmptyNode" },
+          },
+          {
+            id: "ExitNode",
+            position: { x: 400, y: 1500 },
+            type: "journey",
+            data: { type: "JourneyNode", nodeTypeProps: { type: "ExitNode" } },
+          },
+        ]),
+        journeyNodesIndex: expect.objectContaining({
+          EntryNode: 0,
+          "908b9795-60b7-4333-a57c-a30f4972fb6b": 1,
+          "6940ebec-a2ca-47dc-a356-42dc0245dd2e": 2,
+          "9d5367b0-882e-49c2-a6d2-4c28e5416d04": 3,
+          "6ce89301-2a35-4562-b1db-54689bfe0e05": 5,
+          ExitNode: 7,
+        }),
+        journeyEdges: [
+          {
+            id: "EntryNode=>908b9795-60b7-4333-a57c-a30f4972fb6b",
+            source: "EntryNode",
+            target: "908b9795-60b7-4333-a57c-a30f4972fb6b",
+            type: "workflow",
+          },
+          {
+            id: "908b9795-60b7-4333-a57c-a30f4972fb6b=>6940ebec-a2ca-47dc-a356-42dc0245dd2e",
+            source: "908b9795-60b7-4333-a57c-a30f4972fb6b",
+            target: "6940ebec-a2ca-47dc-a356-42dc0245dd2e",
+            type: "workflow",
+          },
+          {
+            id: "6940ebec-a2ca-47dc-a356-42dc0245dd2e=>9d5367b0-882e-49c2-a6d2-4c28e5416d04",
+            source: "6940ebec-a2ca-47dc-a356-42dc0245dd2e",
+            target: "9d5367b0-882e-49c2-a6d2-4c28e5416d04",
+            type: "workflow",
+          },
+          {
+            id: expect.any(String),
+            source: "9d5367b0-882e-49c2-a6d2-4c28e5416d04",
+            target: expect.any(String),
+            type: "placeholder",
+          },
+          {
+            id: expect.any(String),
+            source: expect.any(String),
+            target: "6ce89301-2a35-4562-b1db-54689bfe0e05",
+            type: "workflow",
+          },
+          {
+            id: expect.any(String),
+            source: "9d5367b0-882e-49c2-a6d2-4c28e5416d04",
+            target: expect.any(String),
+            type: "placeholder",
+          },
+          {
+            id: expect.any(String),
+            source: expect.any(String),
+            target: expect.any(String),
+            type: "workflow",
+          },
+          {
+            id: expect.any(String),
+            source: expect.any(String),
+            target: expect.any(String),
+            type: "workflow",
+          },
+          {
+            id: expect.any(String),
+            source: expect.any(String),
+            target: "ExitNode",
+            type: "workflow",
           },
         ],
-      },
-      workspaceId,
-    };
-  });
-
-  it("produces the correct ui state", () => {
-    const uiState = journeyToState(journeyResource);
-    expect(uiState).toEqual({
-      journeyNodes: expect.arrayContaining([
-        {
-          id: "EntryNode",
-          position: { x: 400, y: 100 },
-          type: "journey",
-          data: {
-            type: "JourneyNode",
-            nodeTypeProps: {
-              type: "EntryNode",
-              segmentId: journeyResource.definition.entryNode.segment,
-            },
-          },
-        },
-        {
-          id: "908b9795-60b7-4333-a57c-a30f4972fb6b",
-          position: { x: 400, y: 300 },
-          type: "journey",
-          data: {
-            type: "JourneyNode",
-            nodeTypeProps: {
-              type: "MessageNode",
-              templateId: journeyResource.definition.nodes.flatMap((n) =>
-                n.type === JourneyNodeType.MessageNode &&
-                n.id === "908b9795-60b7-4333-a57c-a30f4972fb6b"
-                  ? n
-                  : []
-              )[0]?.variant.templateId,
-              name: "Message - 908b9795-60b7-4333-a57c-a30f4972fb6b",
-            },
-          },
-        },
-        {
-          id: "6940ebec-a2ca-47dc-a356-42dc0245dd2e",
-          position: { x: 400, y: 500 },
-          type: "journey",
-          data: {
-            type: "JourneyNode",
-            nodeTypeProps: { type: "DelayNode", seconds: 1800 },
-          },
-        },
-        {
-          id: "9d5367b0-882e-49c2-a6d2-4c28e5416d04",
-          position: { x: 400, y: 700 },
-          type: "journey",
-          data: {
-            type: "JourneyNode",
-            nodeTypeProps: {
-              type: "SegmentSplitNode",
-              name: "True / False Branch",
-              segmentId: journeyResource.definition.nodes.flatMap((n) =>
-                n.type === JourneyNodeType.SegmentSplitNode &&
-                n.id === "9d5367b0-882e-49c2-a6d2-4c28e5416d04"
-                  ? n
-                  : []
-              )[0]?.variant.segment,
-              trueLabelNodeId: expect.any(String),
-              falseLabelNodeId: expect.any(String),
-            },
-          },
-        },
-        {
-          id: expect.any(String),
-          position: { x: 200, y: 900 },
-          type: "label",
-          data: { type: "LabelNode", title: "true" },
-        },
-        {
-          id: expect.any(String),
-          position: { x: 600, y: 900 },
-          type: "label",
-          data: { type: "LabelNode", title: "false" },
-        },
-        {
-          id: "6ce89301-2a35-4562-b1db-54689bfe0e05",
-          position: { x: 200, y: 1100 },
-          type: "journey",
-          data: {
-            type: "JourneyNode",
-            nodeTypeProps: {
-              type: "MessageNode",
-              templateId: journeyResource.definition.nodes.flatMap((n) =>
-                n.type === JourneyNodeType.MessageNode &&
-                n.id === "6ce89301-2a35-4562-b1db-54689bfe0e05"
-                  ? n
-                  : []
-              )[0]?.variant.templateId,
-              name: "Message - 6ce89301-2a35-4562-b1db-54689bfe0e05",
-            },
-          },
-        },
-        {
-          id: expect.any(String),
-          position: { x: 400, y: 1300 },
-          type: "empty",
-          data: { type: "EmptyNode" },
-        },
-        {
-          id: "ExitNode",
-          position: { x: 400, y: 1500 },
-          type: "journey",
-          data: { type: "JourneyNode", nodeTypeProps: { type: "ExitNode" } },
-        },
-      ]),
-      journeyNodesIndex: expect.objectContaining({
-        EntryNode: 0,
-        "908b9795-60b7-4333-a57c-a30f4972fb6b": 1,
-        "6940ebec-a2ca-47dc-a356-42dc0245dd2e": 2,
-        "9d5367b0-882e-49c2-a6d2-4c28e5416d04": 3,
-        "6ce89301-2a35-4562-b1db-54689bfe0e05": 5,
-        ExitNode: 7,
-      }),
-      journeyEdges: [
-        {
-          id: "EntryNode=>908b9795-60b7-4333-a57c-a30f4972fb6b",
-          source: "EntryNode",
-          target: "908b9795-60b7-4333-a57c-a30f4972fb6b",
-          type: "workflow",
-        },
-        {
-          id: "908b9795-60b7-4333-a57c-a30f4972fb6b=>6940ebec-a2ca-47dc-a356-42dc0245dd2e",
-          source: "908b9795-60b7-4333-a57c-a30f4972fb6b",
-          target: "6940ebec-a2ca-47dc-a356-42dc0245dd2e",
-          type: "workflow",
-        },
-        {
-          id: "6940ebec-a2ca-47dc-a356-42dc0245dd2e=>9d5367b0-882e-49c2-a6d2-4c28e5416d04",
-          source: "6940ebec-a2ca-47dc-a356-42dc0245dd2e",
-          target: "9d5367b0-882e-49c2-a6d2-4c28e5416d04",
-          type: "workflow",
-        },
-        {
-          id: expect.any(String),
-          source: "9d5367b0-882e-49c2-a6d2-4c28e5416d04",
-          target: expect.any(String),
-          type: "placeholder",
-        },
-        {
-          id: expect.any(String),
-          source: expect.any(String),
-          target: "6ce89301-2a35-4562-b1db-54689bfe0e05",
-          type: "workflow",
-        },
-        {
-          id: expect.any(String),
-          source: "9d5367b0-882e-49c2-a6d2-4c28e5416d04",
-          target: expect.any(String),
-          type: "placeholder",
-        },
-        {
-          id: expect.any(String),
-          source: expect.any(String),
-          target: expect.any(String),
-          type: "workflow",
-        },
-        {
-          id: expect.any(String),
-          source: expect.any(String),
-          target: expect.any(String),
-          type: "workflow",
-        },
-        {
-          id: expect.any(String),
-          source: expect.any(String),
-          target: "ExitNode",
-          type: "workflow",
-        },
-      ],
-      journeyName: "My Journey",
+        journeyName: "My Journey",
+      });
     });
   });
 });
