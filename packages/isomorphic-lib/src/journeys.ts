17e2212ff7015092cb60e007d530964783f09055
Max Gurewitz <max@dittofeed.com>
2023-07-26 05:47:51 -0700

    heritage map

--- packages/isomorphic-lib/src/journeys.ts
+++ packages/isomorphic-lib/src/journeys.ts
@@ -125,3 +125,92 @@ export function findDirectParents(
 
   return parents;
 }
+
+export function getNodeId(node: JourneyNode): string {
+  if (node.type === JourneyNodeType.EntryNode) {
+    return JourneyNodeType.EntryNode;
+  }
+  if (node.type === JourneyNodeType.ExitNode) {
+    return JourneyNodeType.ExitNode;
+  }
+  return node.id;
+}
+
+export type HeritageMap = Record<
+  // id of node for which heritage entry applies
+  string,
+  {
+    // ids of direct children nodes
+    children: Set<string>;
+    // ids of all N nested children of node
+    descendents: Set<string>;
+    // ids of direct parents of the node
+    parents: Set<string>;
+    // ids of all N nested parents of node
+    ancestors: Set<string>;
+  }
+>;
+
+export function buildHeritageMap(definition: JourneyDefinition): HeritageMap {
+  const map: HeritageMap = {};
+  const nodes: JourneyNode[] = [
+    definition.entryNode,
+    definition.exitNode,
+    ...definition.nodes,
+  ];
+
+  // initialize map
+  for (const node of nodes) {
+    const id = getNodeId(node);
+    map[id] = {
+      children: findDirectChildren(id, definition),
+      descendents: new Set<string>(),
+      parents: new Set<string>(),
+      ancestors: new Set<string>(),
+    };
+  }
+
+  // fill children, parents, and descendant, ancestor relationships
+  for (const node of nodes) {
+    const id = getNodeId(node);
+
+    const queue: JourneyNode[] = Array.from(
+      findDirectChildren(id, definition).values()
+    ).flatMap((childId) => nodes.find((n) => getNodeId(n) === childId) ?? []);
+
+    while (queue.length > 0) {
+      const currentChild = queue.shift();
+      if (!currentChild) {
+        throw new Error("Queue should not be empty");
+      }
+      const childId = getNodeId(currentChild);
+
+      // add to descendents of parent and ancestors of child
+      map[id]?.descendents.add(childId);
+      map[childId]?.ancestors.add(id);
+
+      // add parents to child and children to parent
+      for (const parentId of map[id]?.ancestors.values() ?? []) {
+        map[parentId]?.descendents.add(childId);
+        map[childId]?.ancestors.add(parentId);
+      }
+
+      // add children to parent and parents to child
+      for (const cid of map[childId]?.descendents.values() ?? []) {
+        map[id]?.descendents.add(cid);
+        map[cid]?.ancestors.add(id);
+      }
+
+      // add children of current child to queue
+      const grandchildren = Array.from(
+        map[childId]?.children.values() ?? []
+      ).flatMap(
+        (grandChildId) => nodes.find((n) => getNodeId(n) === grandChildId) ?? []
+      );
+
+      queue.push(...grandchildren);
+    }
+  }
+
+  return map;
+}
