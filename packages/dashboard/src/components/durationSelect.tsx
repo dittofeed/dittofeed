2550cd2768642ab4e2c7c333a925ce98790c76b3
Chandler Craig <hellochandlercraig@gmail.com>
2023-07-29 16:35:54 -0700

    wip

--- packages/dashboard/src/components/durationSelect.tsx
+++ packages/dashboard/src/components/durationSelect.tsx
@@ -1,6 +1,7 @@
 import { Box, Stack, TextField } from "@mui/material";
-import { ComponentProps } from "react";
+import { ComponentProps, useState } from "react";
 import { TimeUnit } from "../lib/types";
+import TimeUnitSelect, { isTimeUnit } from "./timeUnitSelect";
 
 import DurationDescription from "./durationDescription";
 
@@ -8,15 +9,22 @@ export default function DurationSelect({
   value,
   inputLabel,
   description,
-  timeUnit,
   onChange,
 }: {
   value: number | undefined;
   inputLabel: string;
   description?: string;
-  timeUnit: TimeUnit;
   onChange: ComponentProps<typeof TextField>["onChange"];
 }) {
+  const [timeUnit, setTimeUnit] = useState<TimeUnit>("hours");
+
+  const handleTimeUnitChange: ComponentProps<
+    typeof TimeUnitSelect
+  >["onChange"] = (e) => {
+    if (isTimeUnit(e.target.value)) {
+      setTimeUnit(e.target.value);
+    }
+  };
   return (
     <>
       <TextField
@@ -27,6 +35,7 @@ export default function DurationSelect({
         value={String(value)}
         onChange={onChange}
       />
+      <TimeUnitSelect value={timeUnit} onChange={handleTimeUnitChange} />
       <Stack direction="row" spacing={1}>
         {description ? <Box>{description}</Box> : null}
         <Box>
