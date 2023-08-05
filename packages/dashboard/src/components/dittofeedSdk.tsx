e2085d6424fff93987af3467449a5a784f63bba0
Max Gurewitz <max@dittofeed.com>
2023-08-05 11:38:11 -0700

    missing files

---
+++ packages/dashboard/src/components/dittofeedSdk.tsx
@@ -0,0 +1,23 @@
+import { DittofeedSdk } from "@dittofeed/sdk-web";
+import React from "react";
+import { pick } from "remeda/dist/commonjs/pick";
+
+import { apiBase } from "../lib/apiBase";
+import { useAppStore } from "../lib/appStore";
+
+export function SdkInitializer({ children }: { children: React.ReactNode }) {
+  const { dashboardWriteKey, trackDashboard } = useAppStore((store) =>
+    pick(store, ["dashboardWriteKey", "trackDashboard"])
+  );
+  React.useEffect(() => {
+    if (trackDashboard && dashboardWriteKey) {
+      DittofeedSdk.init({
+        writeKey: dashboardWriteKey,
+        host: apiBase(),
+      });
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return <>{children}</>;
+}
