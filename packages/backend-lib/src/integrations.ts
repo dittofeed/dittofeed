f9fe700dd9fdea32604402e0a6e742369794d83f
Max Gurewitz <max@dittofeed.com>
2023-08-22 17:18:56 -0700

    upsert definition

--- packages/backend-lib/src/integrations.ts
+++ packages/backend-lib/src/integrations.ts
@@ -4,10 +4,29 @@ import { err, ok, Result } from "neverthrow";
 import prisma from "./prisma";
 import {
   EnrichedIntegration,
+  Integration,
   IntegrationDefinition,
   IntegrationResource,
   UpsertIntegrationResource,
 } from "./types";
+import { pick } from "remeda";
+import { unwrap } from "isomorphic-lib/src/resultHandling/resultUtils";
+
+export function enrichIntegration(
+  integration: Integration
+): Result<EnrichedIntegration, Error> {
+  const definitionResult = schemaValidateWithErr(
+    integration.definition,
+    IntegrationDefinition
+  );
+  if (definitionResult.isErr()) {
+    return err(definitionResult.error);
+  }
+  return ok({
+    ...integration,
+    definition: definitionResult.value,
+  });
+}
 
 export async function findAllEnrichedIntegrations(
   workspaceId: string
@@ -33,8 +52,45 @@ export async function findAllEnrichedIntegrations(
   return ok(enriched);
 }
 
-export async function upsertIntegration(
-  integration: UpsertIntegrationResource
-): Promise<IntegrationResource> {
-  throw new Error("Not implemented");
+export async function upsertIntegration({
+  name,
+  workspaceId,
+  definition,
+  enabled,
+}: UpsertIntegrationResource): Promise<IntegrationResource> {
+  let integration: Integration;
+  if (definition) {
+    integration = await prisma().integration.upsert({
+      where: {
+        workspaceId_name: {
+          name,
+          workspaceId,
+        },
+      },
+      create: {
+        name,
+        workspaceId,
+        definition,
+        enabled,
+      },
+      update: {
+        definition,
+        enabled,
+      },
+    });
+  } else {
+    integration = await prisma().integration.update({
+      where: {
+        workspaceId_name: {
+          name,
+          workspaceId,
+        },
+      },
+      data: {
+        enabled,
+      },
+    });
+  }
+  const enriched = unwrap(enrichIntegration(integration));
+  return pick(enriched, ["id", "name", "workspaceId", "definition", "enabled"]);
 }
