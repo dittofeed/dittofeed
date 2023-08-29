273a135256960785fe25513acf3a2a1a1391d06d
Max Gurewitz <max@dittofeed.com>
2023-08-29 11:11:52 -0700

    catch errors compute properties workflow

--- packages/backend-lib/src/segments/computePropertiesWorkflow.ts
+++ packages/backend-lib/src/segments/computePropertiesWorkflow.ts
@@ -56,76 +56,81 @@ export async function computePropertiesWorkflow({
   subscribedJourneys = [],
 }: ComputedPropertiesWorkflowParams): Promise<ComputedPropertiesWorkflowParams> {
   let journeys = subscribedJourneys;
-  const { computePropertiesInterval } = await config([
-    "computePropertiesInterval",
-  ]);
-
-  // only use override if shouldContinueAsNew is false, in order to allow value
-  // to be reconfigured at deploy time
-  const basePollingInterval =
-    shouldContinueAsNew || !basePollingPeriodOverride
-      ? computePropertiesInterval
-      : basePollingPeriodOverride;
 
   for (let i = 0; i < maxPollingAttempts; i++) {
-    const currentTime = Date.now();
+    try {
+      const currentTime = Date.now();
 
-    logger.info("segmentsNotificationWorkflow polling attempt", {
-      i,
-      currentTime,
-      maxPollingAttempts,
-    });
+      logger.info("segmentsNotificationWorkflow polling attempt", {
+        i,
+        currentTime,
+        maxPollingAttempts,
+      });
 
-    /**
-     * scenarios, at query time:
-     *  1. new journey is NotStarted
-     *  2. new journey is Running
-     *  3. new journey is Paused
-     *
-     * handling:
-     *  1. NotStarted journeys should be filtered out with db query
-     *  2. Running journeys should be handled as normal
-     *  3. Paused journeys should be removed from subscribed journeys, so that on subsequent queries they are entirely refreshed
-     */
-    const [latestSubscribedJourneys, userProperties] = await Promise.all([
-      findAllJourneysUnsafe({
-        where: {
+      /**
+       * scenarios, at query time:
+       *  1. new journey is NotStarted
+       *  2. new journey is Running
+       *  3. new journey is Paused
+       *
+       * handling:
+       *  1. NotStarted journeys should be filtered out with db query
+       *  2. Running journeys should be handled as normal
+       *  3. Paused journeys should be removed from subscribed journeys, so that on subsequent queries they are entirely refreshed
+       */
+      const [latestSubscribedJourneys, userProperties] = await Promise.all([
+        findAllJourneysUnsafe({
+          where: {
+            workspaceId,
+            status: "Running",
+          },
+        }),
+        findAllUserProperties({
           workspaceId,
-          status: "Running",
-        },
-      }),
-      findAllUserProperties({
+        }),
+      ]);
+
+      const newJourneysDiff: JourneyMap =
+        latestSubscribedJourneys.reduce<JourneyMap>((memo, journey) => {
+          memo.set(journey.id, true);
+          return memo;
+        }, new Map());
+
+      journeys.forEach((j) => {
+        newJourneysDiff.delete(j.id);
+      });
+
+      journeys = latestSubscribedJourneys;
+
+      await computePropertiesPeriod({
+        tableVersion,
+        currentTime,
         workspaceId,
-      }),
+        newComputedIds: Object.fromEntries(newJourneysDiff),
+        subscribedJourneys: journeys,
+        userProperties,
+      });
+    } catch (e) {
+      logger.error("computePropertiesPeriod failed", { err: e });
+    }
+
+    const { computePropertiesInterval } = await config([
+      "computePropertiesInterval",
     ]);
 
-    const newJourneysDiff: JourneyMap =
-      latestSubscribedJourneys.reduce<JourneyMap>((memo, journey) => {
-        memo.set(journey.id, true);
-        return memo;
-      }, new Map());
-
-    journeys.forEach((j) => {
-      newJourneysDiff.delete(j.id);
-    });
-
-    journeys = latestSubscribedJourneys;
-
-    await computePropertiesPeriod({
-      tableVersion,
-      currentTime,
-      workspaceId,
-      newComputedIds: Object.fromEntries(newJourneysDiff),
-      subscribedJourneys: journeys,
-      userProperties,
-    });
+    // only use override if shouldContinueAsNew is false, in order to allow value
+    // to be reconfigured at deploy time
+    const basePollingInterval =
+      shouldContinueAsNew || !basePollingPeriodOverride
+        ? computePropertiesInterval
+        : basePollingPeriodOverride;
 
     // sleep for 10 seconds + up to 1 seconds of jitter for next polling period
     await sleep(basePollingInterval + Math.random() * pollingJitterCoefficient);
   }
 
   const params: ComputedPropertiesWorkflowParams = {
-    basePollingPeriod: basePollingInterval,
+    basePollingPeriod: basePollingPeriodOverride,
     maxPollingAttempts,
     pollingJitterCoefficient,
     shouldContinueAsNew,
