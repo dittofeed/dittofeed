f2cbfa6d70b111d9c9c2ab91fb5a754c1eee1f60
Max Gurewitz <max@dittofeed.com>
2023-08-28 20:27:06 -0700

    config retrieved through activity (#356)

--- packages/backend-lib/src/segments/computePropertiesWorkflow/activities.ts
+++ packages/backend-lib/src/segments/computePropertiesWorkflow/activities.ts
@@ -1,3 +1,13 @@
+import { pick } from "remeda";
+
+import getConfig, { Config } from "../../config";
+
 export { findManyJourneysUnsafe as findAllJourneysUnsafe } from "../../journeys";
 export { findAllUserProperties } from "../../userProperties";
 export * from "./activities/computeProperties";
+
+export async function config(
+  keys: (keyof Config)[]
+): Promise<Pick<Config, (typeof keys)[number]>> {
+  return pick(getConfig(), keys);
+}
