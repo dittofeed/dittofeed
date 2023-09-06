5ae4b7227b52b8305ee82d45e8258c36088bd5a1
Max Gurewitz <max@dittofeed.com>
2023-09-05 17:42:01 -0700

    enhance configurability of dashboard api base

--- packages/dashboard/src/lib/apiBase.ts
+++ packages/dashboard/src/lib/apiBase.ts
@@ -1,11 +1,17 @@
-import { hasProtocol } from "isomorphic-lib/src/urls";
-
 export function apiBase(): string {
-  if (!process.env.DASHBOARD_API_BASE) {
-    return "http://localhost:3001";
+  if (process.env.DASHBOARD_API_BASE) {
+    return process.env.DASHBOARD_API_BASE;
   }
-  if (!hasProtocol(process.env.DASHBOARD_API_BASE)) {
-    return `https://${process.env.DASHBOARD_API_BASE}`;
+  if (process.env.DASHBOARD_API_DOMAIN) {
+    const domainParts = [process.env.DASHBOARD_API_DOMAIN];
+
+    if (process.env.DASHBOARD_API_SUBDOMAIN) {
+      domainParts.unshift(process.env.DASHBOARD_API_SUBDOMAIN);
+    }
+    const domain = domainParts.join(".");
+    const protocol = process.env.DASHBOARD_API_PROTOCOL ?? "https";
+    const base = `${protocol}://${domain}`;
+    return base;
   }
-  return process.env.DASHBOARD_API_BASE;
+  return "http://localhost:3001";
 }
