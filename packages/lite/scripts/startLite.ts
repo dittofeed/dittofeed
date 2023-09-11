190ecd31432658e1d23d257c68a10145b9911927
Max Gurewitz <max@dittofeed.com>
2023-09-11 15:46:42 -0700

    start worker

--- packages/lite/scripts/startLite.ts
+++ packages/lite/scripts/startLite.ts
@@ -1,12 +1,23 @@
+import {
+  appendDefaultInterceptors,
+  defaultSinks,
+  NativeConnection,
+  Runtime,
+  Worker,
+} from "@temporalio/worker";
 import buildApp from "api/src/buildApp";
 import backendConfig from "backend-lib/src/config";
 import logger from "backend-lib/src/logger";
+import * as activities from "backend-lib/src/temporal/activities";
+import { CustomActivityInboundInterceptor } from "backend-lib/src/temporal/activityInboundInterceptor";
+import connectWorkflowCLient from "backend-lib/src/temporal/connectWorkflowClient";
 import next from "next";
 import path from "path";
+import workerConfig from "worker/src/config";
+import workerLogger from "worker/src/workerLogger";
 
 import liteConfig from "../src/config";
 
-// FIXME include admin cli
 async function startLite() {
   if (backendConfig().logConfig) {
     logger().info(
@@ -26,7 +37,15 @@ async function startLite() {
   });
   const nextHandler = nextApp.getRequestHandler();
 
-  await nextApp.prepare();
+  Runtime.install({ logger: workerLogger });
+
+  const [connection, workflowClient] = await Promise.all([
+    NativeConnection.connect({
+      address: backendConfig().temporalAddress,
+    }),
+    connectWorkflowCLient(),
+    nextApp.prepare(),
+  ]);
 
   app.route({
     // Exclude 'OPTIONS to avoid conflict with cors plugin'
@@ -37,6 +56,31 @@ async function startLite() {
       // eslint-disable-next-line no-param-reassign
       reply.sent = true;
     },
+  });
+
+  await Worker.create({
+    connection,
+    namespace: backendConfig().temporalNamespace,
+    workflowsPath: require.resolve("backend-lib/src/temporal/workflows"),
+    activities,
+    taskQueue: "default",
+    sinks: {
+      ...defaultSinks(workerLogger),
+    },
+    interceptors: appendDefaultInterceptors(
+      {
+        activityInbound: [
+          (ctx) =>
+            new CustomActivityInboundInterceptor(ctx, {
+              workflowClient,
+            }),
+        ],
+      },
+      workerLogger
+    ),
+    reuseV8Context: workerConfig().reuseContext,
+    maxCachedWorkflows: workerConfig().maxCachedWorkflows,
+    enableSDKTracing: true,
   });
 
   await app.listen({ port, host });
