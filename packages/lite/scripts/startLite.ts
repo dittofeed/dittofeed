ea35b016f4b46779eded3e693ce1fdda187615a4
Max Gurewitz <max@dittofeed.com>
2023-09-11 15:03:47 -0700

    fails on startup

--- packages/lite/scripts/startLite.ts
+++ packages/lite/scripts/startLite.ts
@@ -1,9 +1,12 @@
 import buildApp from "api/src/buildApp";
 import backendConfig from "backend-lib/src/config";
 import logger from "backend-lib/src/logger";
+import next from "next";
+import path from "path";
 
 import liteConfig from "../src/config";
 
+// FIXME include admin cli
 async function startLite() {
   if (backendConfig().logConfig) {
     logger().info(
@@ -15,16 +18,32 @@ async function startLite() {
     );
   }
   const app = await buildApp();
-  const { port, host } = liteConfig();
+  const { port, host, nodeEnv } = liteConfig();
+
+  const nextApp = next({
+    dev: nodeEnv === "development",
+    dir: path.resolve(__dirname, path.join("..", "..", "dashboard")),
+  });
+  const nextHandler = nextApp.getRequestHandler();
+
+  await nextApp.prepare();
+
+  app.all("*", async (req, reply) => {
+    await nextHandler(req.raw, reply.raw);
+    // eslint-disable-next-line no-param-reassign
+    reply.sent = true;
+  });
 
   await app.listen({ port, host });
 }
 
 startLite()
   .then(() => {
-    console.log("Lite started");
+    // eslint-disable-next-line no-console
+    console.log("dittofeed-lite started");
   })
   .catch((err) => {
+    // eslint-disable-next-line no-console
     console.error(err);
     process.exit(1);
   });
