2b20564d7d9bf859936b0132b09f62fa6e3f4395
Max Gurewitz <max@dittofeed.com>
2023-09-11 14:35:50 -0700

    lite starts api

--- packages/lite/scripts/startLite.ts
+++ packages/lite/scripts/startLite.ts
@@ -1,7 +1,23 @@
 import buildApp from "api/src/buildApp";
+import backendConfig from "backend-lib/src/config";
+import logger from "backend-lib/src/logger";
+
+import liteConfig from "../src/config";
 
 async function startLite() {
+  if (backendConfig().logConfig) {
+    logger().info(
+      {
+        ...backendConfig(),
+        ...liteConfig,
+      },
+      "Initialized with config"
+    );
+  }
   const app = await buildApp();
+  const { port, host } = liteConfig();
+
+  await app.listen({ port, host });
 }
 
 startLite()
