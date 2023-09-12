6f6beb4b09fc43cc10b89854ffb873028ecd7f98
Max Gurewitz <max@dittofeed.com>
2023-09-11 17:08:36 -0700

    dittofeed-lite (#402)
    - package to encapsulate 3 all of dittofeed's major services in an easier to run, but less scalable package

---
+++ packages/lite/scripts/startLite.ts
@@ -0,0 +1,106 @@
+import {
+  appendDefaultInterceptors,
+  defaultSinks,
+  NativeConnection,
+  Runtime,
+  Worker,
+} from "@temporalio/worker";
+import { BOOTSTRAP_OPTIONS, bootstrapHandler } from "admin-cli/src/bootstrap";
+import buildApp from "api/src/buildApp";
+import backendConfig from "backend-lib/src/config";
+import logger from "backend-lib/src/logger";
+import * as activities from "backend-lib/src/temporal/activities";
+import { CustomActivityInboundInterceptor } from "backend-lib/src/temporal/activityInboundInterceptor";
+import connectWorkflowCLient from "backend-lib/src/temporal/connectWorkflowClient";
+import next from "next";
+import path from "path";
+import workerConfig from "worker/src/config";
+import workerLogger from "worker/src/workerLogger";
+import yargs from "yargs";
+import { hideBin } from "yargs/helpers";
+
+import liteConfig from "../src/config";
+
+async function startLite() {
+  if (backendConfig().logConfig) {
+    logger().info(
+      {
+        ...backendConfig(),
+        ...liteConfig,
+      },
+      "Initialized with config"
+    );
+  }
+  const args = await yargs(hideBin(process.argv)).options(BOOTSTRAP_OPTIONS)
+    .argv;
+
+  await bootstrapHandler(args);
+
+  const app = await buildApp();
+  const { port, host, nodeEnv } = liteConfig();
+
+  const nextApp = next({
+    dev: nodeEnv === "development",
+    dir: path.resolve(__dirname, path.join("..", "..", "dashboard")),
+  });
+  const nextHandler = nextApp.getRequestHandler();
+
+  Runtime.install({ logger: workerLogger });
+
+  const [connection, workflowClient] = await Promise.all([
+    NativeConnection.connect({
+      address: backendConfig().temporalAddress,
+    }),
+    connectWorkflowCLient(),
+    nextApp.prepare(),
+  ]);
+
+  app.route({
+    // Exclude 'OPTIONS to avoid conflict with cors plugin'
+    method: ["GET", "POST", "PUT", "DELETE", "PATCH", "HEAD"],
+    url: "*",
+    handler: async (req, reply) => {
+      await nextHandler(req.raw, reply.raw);
+      // eslint-disable-next-line no-param-reassign
+      reply.sent = true;
+    },
+  });
+
+  await Worker.create({
+    connection,
+    namespace: backendConfig().temporalNamespace,
+    workflowsPath: require.resolve("backend-lib/src/temporal/workflows"),
+    activities,
+    taskQueue: "default",
+    sinks: {
+      ...defaultSinks(workerLogger),
+    },
+    interceptors: appendDefaultInterceptors(
+      {
+        activityInbound: [
+          (ctx) =>
+            new CustomActivityInboundInterceptor(ctx, {
+              workflowClient,
+            }),
+        ],
+      },
+      workerLogger
+    ),
+    reuseV8Context: workerConfig().reuseContext,
+    maxCachedWorkflows: workerConfig().maxCachedWorkflows,
+    enableSDKTracing: true,
+  });
+
+  await app.listen({ port, host });
+}
+
+startLite()
+  .then(() => {
+    // eslint-disable-next-line no-console
+    console.log("dittofeed-lite started");
+  })
+  .catch((err) => {
+    // eslint-disable-next-line no-console
+    console.error(err);
+    process.exit(1);
+  });
