f5a6290e0d5697a7900dc7bddb990a4c72f169c5
Max Gurewitz <max@dittofeed.com>
2023-09-17 15:43:59 -0700

    wip

--- packages/dashboard/src/pages/analysis/messages.page.tsx
+++ packages/dashboard/src/pages/analysis/messages.page.tsx
@@ -1,12 +1,17 @@
 import { Box, Stack, Tabs } from "@mui/material";
+import { findManyJourneys } from "backend-lib/src/journeys";
+import { unwrap } from "isomorphic-lib/src/resultHandling/resultUtils";
+import { CompletionStatus } from "isomorphic-lib/src/types";
 import { GetServerSideProps } from "next";
 import Head from "next/head";
 
 import MainLayout from "../../components/mainLayout";
 import TabLink from "../../components/tabLink";
 import { addInitialStateToProps } from "../../lib/addInitialStateToProps";
+import { useAppStorePick } from "../../lib/appStore";
 import { requestContext } from "../../lib/requestContext";
 import { PropsWithInitialState } from "../../lib/types";
+import { useJourneyStats } from "../../lib/useJourneyStats";
 
 type Tab = "messages";
 
@@ -50,6 +55,9 @@ export const getServerSideProps: GetServerSideProps<PropsWithInitialState> =
   requestContext(async (_ctx, dfContext) => {
     const { workspace } = dfContext;
     const workspaceId = workspace.id;
+    const [] = await Promise.all([
+      findManyJourneys({ where: { workspaceId } }).then(unwrap),
+    ]);
 
     return {
       props: addInitialStateToProps({
@@ -61,9 +69,39 @@ export const getServerSideProps: GetServerSideProps<PropsWithInitialState> =
   });
 
 export default function MessagesPage() {
+  const {
+    journeyStats,
+    apiBase,
+    journeys,
+    upsertJourneyStats,
+    setJourneyStatsRequest,
+    workspace,
+  } = useAppStorePick([
+    "journeyStats",
+    "journeys",
+    "upsertJourneyStats",
+    "setJourneyStatsRequest",
+    "workspace",
+  ]);
+  useJourneyStats({
+    workspaceId:
+      workspace.type === CompletionStatus.Successful
+        ? workspace.value.id
+        : undefined,
+    upsertJourneyStats,
+    apiBase,
+    setJourneyStatsRequest,
+  });
+
   return (
     <AnalysisLayout tab="messages">
       <h1>Messages</h1>
+      {journeys.type === CompletionStatus.Successful &&
+        journeys.value.map((journey) => (
+          <div key={journey.id}>
+            <h2>{journey.name}</h2>
+          </div>
+        ))}
     </AnalysisLayout>
   );
 }
