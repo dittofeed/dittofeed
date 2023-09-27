8bcd9f35cd4c02c56d9154fa08e4ab94022dfc8c
Max Gurewitz <max@dittofeed.com>
2023-09-27 14:38:48 -0700

    add newline to files without them

--- packages/dashboard/src/components/form/SimpleTextField.tsx
+++ packages/dashboard/src/components/form/SimpleTextField.tsx
@@ -1,26 +1,28 @@
 import { StyledComponent } from "@emotion/styled";
 import { styled, TextField, TextFieldProps } from "@mui/material";
 
-const SimpleTextField: StyledComponent<TextFieldProps, object, object> = styled(TextField)(({ theme }) => ({
-  'fieldset': {
-    display: 'none'
+const SimpleTextField: StyledComponent<TextFieldProps, object, object> = styled(
+  TextField
+)(({ theme }) => ({
+  fieldset: {
+    display: "none",
   },
-  'input': {
-    height: '0.8rem',
+  input: {
+    height: "0.8rem",
   },
-  'label': {
+  label: {
     transform: "none !important",
-    color: "inherit"
+    color: "inherit",
   },
-  '& .MuiOutlinedInput-root': {
-    marginTop: '1.5rem',
-    border: '1px solid',
-    borderColor: theme.palette.mode === 'light' ? '#E0E3E7' : '#2D3843',
-    borderRadius: "4px"
+  "& .MuiOutlinedInput-root": {
+    marginTop: "1.5rem",
+    border: "1px solid",
+    borderColor: theme.palette.mode === "light" ? "#E0E3E7" : "#2D3843",
+    borderRadius: "4px",
   },
-  '& .MuiFormHelperText-root': {
-    marginLeft: 0
-  }
+  "& .MuiFormHelperText-root": {
+    marginLeft: 0,
+  },
 }));
 
-export default SimpleTextField
+export default SimpleTextField;
