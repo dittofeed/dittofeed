84f4efb90e463e258654e5eb83cc0b95cd85f6ec
Promise <>
2023-09-27 21:18:37 +0100

    save page title props to layout context and render title in navigation

--- packages/dashboard/src/components/layout/drawer/drawerContent/navigation.tsx
+++ packages/dashboard/src/components/layout/drawer/drawerContent/navigation.tsx
@@ -1,23 +1,28 @@
 // material-ui
-import { Box, Typography } from "@mui/material";
+import { ArrowBackIos } from "@mui/icons-material";
+import { Box, List, Stack, Typography } from "@mui/material";
+import Link from "next/link";
 import { useContext } from "react";
 
 import { LayoutContext } from "../../context";
+import MinimalNavGroup from "./navigation/minimalNavGroup";
 // project import
 import NavGroup from "./navigation/navGroup";
 
 // ==============================|| DRAWER CONTENT - NAVIGATION ||============================== //
 
 function Navigation() {
-  const items = useContext(LayoutContext)?.items;
-  if (!items) {
-    return null;
-  }
+  const layout = useContext(LayoutContext)
+  const items = layout?.items;
+  const title = layout?.pageTitle
+  const backLink = layout?.backLink
+  const navigationRenderer = layout?.navigationRenderer
+  const isMinimal = navigationRenderer === "minimal"
 
-  const navGroups = items.map((item) => {
+  const navGroups = items ? items.map((item) => {
     switch (item.type) {
       case "group":
-        return <NavGroup key={item.id} item={item} />;
+        return isMinimal ? <MinimalNavGroup key={item.id} item={item} /> : <NavGroup key={item.id} item={item} />;
       default:
         return (
           <Typography key={item.id} variant="h6" color="error" align="center">
@@ -25,9 +30,33 @@ function Navigation() {
           </Typography>
         );
     }
-  });
+  }) : null;
 
-  return <Box sx={{ pt: 2 }}>{navGroups}</Box>;
+  return (
+    <Box sx={{ pt: 2, px: isMinimal ? 3 : undefined }}>
+      {/* {backLink ?
+        <Link {...backLink} style={{ textDecoration: "none", color: "inherit", opacity: 0.6, display: "flex", alignItems: "center", margin: "4px 0 20px" }}>
+          <ArrowBackIos fontSize="inherit" />
+          {backLink.children}
+        </Link> : null} */}
+      {title ?
+        (
+          <Stack direction="row" sx={{ ml: -1 }}>
+            {backLink ? <Link {...backLink} style={{
+              textDecoration: "none",
+              color: "inherit",
+              opacity: 0.6,
+              display: "flex",
+              alignItems: "center",
+              margin: "4px 0 20px",
+            }}>
+              <ArrowBackIos fontSize="inherit" />
+            </Link> : null}
+            <Typography mb={2} variant="h1" fontSize={21}>{title}</Typography>
+          </Stack>
+        ) : null}
+      {navGroups}
+    </Box>);
 }
 
 export default Navigation;
