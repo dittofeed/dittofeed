30da2638d8456a3e4ce08427312e70c09ed74970
Max Gurewitz <max@dittofeed.com>
2023-10-03 17:58:50 -0700

    modify install deps to not fail on cleanup step (#440)

--- packages/admin-cli/scripts/install-deps.sh
+++ packages/admin-cli/scripts/install-deps.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 apt-get install -y apt-transport-https ca-certificates dirmngr
 GNUPGHOME=$(mktemp -d)
 GNUPGHOME="$GNUPGHOME" gpg --no-default-keyring --keyring /usr/share/keyrings/clickhouse-keyring.gpg --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 8919F6BD2B48D754
-rm -r "$GNUPGHOME"
+rm -rf "$GNUPGHOME"
 chmod +r /usr/share/keyrings/clickhouse-keyring.gpg
 
 echo "deb [signed-by=/usr/share/keyrings/clickhouse-keyring.gpg] https://packages.clickhouse.com/deb stable main" | tee \
