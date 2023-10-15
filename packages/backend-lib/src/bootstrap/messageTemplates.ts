9d1650ffcfe0f664d0b778b5817e0b9136275fae
Max Gurewitz <max@dittofeed.com>
2023-10-14 21:31:58 -0700

    plaintext email as default email (#451)
    - restructure existing example templates
    - add plaintext welcome email to docs

--- packages/backend-lib/src/bootstrap/messageTemplates.ts
+++ packages/backend-lib/src/bootstrap/messageTemplates.ts
@@ -7,52 +7,37 @@ import {
 } from "../types";
 
 const welcomeBody = `<mjml>
-  <mj-body background-color="#F7F8FA">
-    <mj-raw>
-      <!-- Image Header -->
-    </mj-raw>
-    <mj-section align="center">
-      <mj-column>
-        <mj-image width="200px" src="https://storage.googleapis.com/dittofeed-public/logo.png" />
-      </mj-column>
-    </mj-section>
-    <mj-section background-color="#fff">
-      <mj-column width="400px">
-        <mj-text color="#525252" font-size="16px"
-          >Hi {{user.firstName | default: "there"}}
-          <br />
-          <br />
-          A warm welcome to CompanyName! During this 14-day trial, you have the
-          opportunity to use our product to make a difference in your
-          day-to-day. We'll be providing you with a VIP tour of how to make the
-          most of CompanyName.
-          <br />
-          <br />
-          To get started, we'll integrate your data into the platform.
-        </mj-text>
-        <mj-button background-color="#49BCD5" href="#"
-          >Setup CompanyName</mj-button
-        >
-        <mj-text color="#525252" font-size="16px">
-          Take a look at our <a href="#">docs</a> for more info.
-        </mj-text>
-      </mj-column>
-    </mj-section>
+  <mj-head>
+    <mj-style inline="inline">
+      .df-unsubscribe {
+        color: #a8a8a8;
+        text-decoration: underline;
+      }
+    </mj-style>
+  </mj-head>
+  <mj-body>
     <mj-section>
-      <mj-column width="60%">
-        <mj-text align="center" color="#525252">
-          MyCompany, Inc., 40 Rosewood Lane, New York, NY 10003
-          <br/>
-          <br/>
-          Don't want to receive these emails? You can {% unsubscribe_link %} from them here.
-          <br/>
-          <br/>
-          Powered by <a href="https://dittofeed.com" target="_blank" rel="noopener noreferrer">Dittofeed</a>.
+      <mj-column>
+        <mj-text font-size="16px" align="left">
+          Hi {{ user.firstName | default: "there"}}<br/><br/>
+          A warm welcome to CompanyName! During this 14-day trial, you have the opportunity to use our product to make a difference in your day-to-day. We'll be providing you with a VIP tour of how to make the most of CompanyName.<br/><br/>
+          To get started, we'll integrate your data into the platform.<br/><br/>
+Take a look at our <a href="#" target="_blank">docs</a> for more info.
+        </mj-text>
+      </mj-column>
+    </mj-section>
+    <mj-section padding-top="10px">
+      <mj-column>
+        <mj-text font-size="12px" align="center" color="#a8a8a8">
+          MyCompany, Inc., 40 Rosewood Lane, New York, NY 10003<br/>
+          Don't want to receive these emails? You can {% unsubscribe_link %} from them here.<br/>
+          Powered by Dittofeed.
         </mj-text>
       </mj-column>
     </mj-section>
   </mj-body>
-</mjml>`;
+</mjml>
+`;
 
 export const WELCOME_TEMPLATE: EmailTemplateResource = {
   type: ChannelType.Email,
