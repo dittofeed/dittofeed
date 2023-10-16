0440bbf2ea29ba50aeb304485f5120dde078514b
Max Gurewitz <max@dittofeed.com>
2023-10-15 20:59:10 -0700

    wip

--- packages/backend-lib/src/messageTemplates.ts
+++ packages/backend-lib/src/messageTemplates.ts
@@ -9,6 +9,7 @@ import { sendMail as sendEmailSendgrid } from "./destinations/sendgrid";
 import { renderLiquid } from "./liquid";
 import logger from "./logger";
 import prisma from "./prisma";
+import { SubscriptionGroupDetails } from "./subscriptionGroups";
 import {
   BackendMessageSendResult,
   BadWorkspaceConfigurationType,
@@ -152,14 +153,12 @@ async function getSendMessageModels({
   templateId,
   workspaceId,
   channel,
-  subscriptionGroupAction,
-  subscriptionGroupType,
+  subscriptionGroupDetails,
 }: {
   workspaceId: string;
   templateId: string;
   channel: ChannelType;
-  subscriptionGroupAction: UserSubscriptionAction;
-  subscriptionGroupType: SubscriptionGroupType;
+  subscriptionGroupDetails?: SubscriptionGroupDetails;
 }): Promise<
   Result<
     {
@@ -169,31 +168,35 @@ async function getSendMessageModels({
     MessageSendFailure
   >
 > {
-  if (
-    subscriptionGroupType === SubscriptionGroupType.OptIn &&
-    subscriptionGroupAction !== UserSubscriptionAction.Subscribe
-  ) {
-    return err({
-      type: InternalEventType.MessageSkipped,
-      variant: {
-        type: MessageSkippedType.SubscriptionState,
-        action: subscriptionGroupAction,
-        subscriptionGroupType,
-      },
-    });
-  }
-  if (
-    subscriptionGroupType === SubscriptionGroupType.OptOut &&
-    subscriptionGroupAction === UserSubscriptionAction.Unsubscribe
-  ) {
-    return err({
-      type: InternalEventType.MessageSkipped,
-      variant: {
-        type: MessageSkippedType.SubscriptionState,
-        action: subscriptionGroupAction,
-        subscriptionGroupType,
-      },
-    });
+  if (subscriptionGroupDetails) {
+    const { type: subscriptionGroupType, action: subscriptionGroupAction } =
+      subscriptionGroupDetails;
+    if (
+      subscriptionGroupType === SubscriptionGroupType.OptIn &&
+      subscriptionGroupAction !== UserSubscriptionAction.Subscribe
+    ) {
+      return err({
+        type: InternalEventType.MessageSkipped,
+        variant: {
+          type: MessageSkippedType.SubscriptionState,
+          action: subscriptionGroupAction,
+          subscriptionGroupType,
+        },
+      });
+    }
+    if (
+      subscriptionGroupType === SubscriptionGroupType.OptOut &&
+      subscriptionGroupAction === UserSubscriptionAction.Unsubscribe
+    ) {
+      return err({
+        type: InternalEventType.MessageSkipped,
+        variant: {
+          type: MessageSkippedType.SubscriptionState,
+          action: subscriptionGroupAction,
+          subscriptionGroupType,
+        },
+      });
+    }
   }
 
   const [messageTemplateResult, subscriptionGroupSecret] = await Promise.all([
@@ -255,9 +258,7 @@ interface SendMessageParameters {
   templateId: string;
   userPropertyAssignments: UserPropertyAssignments;
   channel: ChannelType;
-  subscriptionGroupAction: UserSubscriptionAction;
-  subscriptionGroupType: SubscriptionGroupType;
-  subscriptionGroupId: string;
+  subscriptionGroupDetails?: SubscriptionGroupDetails;
   messageTags?: Record<string, string>;
 }
 
@@ -309,9 +310,7 @@ export async function sendEmail({
   workspaceId,
   templateId,
   userPropertyAssignments,
-  subscriptionGroupId,
-  subscriptionGroupAction,
-  subscriptionGroupType,
+  subscriptionGroupDetails,
   messageTags,
 }: Omit<SendMessageParameters, "channel">): Promise<BackendMessageSendResult> {
   const [getSendModelsResult, defaultEmailProvider] = await Promise.all([
@@ -319,8 +318,7 @@ export async function sendEmail({
       workspaceId,
       templateId,
       channel: ChannelType.Email,
-      subscriptionGroupAction,
-      subscriptionGroupType,
+      subscriptionGroupDetails,
     }),
     prisma().defaultEmailProvider.findUnique({
       where: {
@@ -357,7 +355,7 @@ export async function sendEmail({
   const renderedValuesResult = renderValues({
     userProperties: userPropertyAssignments,
     identifierKey,
-    subscriptionGroupId,
+    subscriptionGroupId: subscriptionGroupDetails?.id,
     workspaceId,
     templates: {
       from: {
--- packages/backend-lib/src/subscriptionGroups.ts
+++ packages/backend-lib/src/subscriptionGroups.ts
@@ -24,6 +24,7 @@ import {
   SubscriptionGroupType,
   SubscriptionParams,
   UpsertSubscriptionGroupResource,
+  UserSubscriptionAction,
   UserSubscriptionLookup,
   UserSubscriptionResource,
   UserSubscriptionsUpdate,
@@ -35,6 +36,33 @@ export type SubscriptionGroupWithAssignment = SubscriptionGroup & {
     SegmentAssignment: SegmentAssignment[];
   })[];
 };
+
+export interface SubscriptionGroupDetails {
+  id: string;
+  action: UserSubscriptionAction;
+  type: SubscriptionGroupType;
+}
+
+export function getSubscriptionGroupDetails(
+  sg: SubscriptionGroupWithAssignment
+): SubscriptionGroupDetails {
+  let action: UserSubscriptionAction;
+  if (sg.Segment[0]?.SegmentAssignment[0] !== undefined) {
+    action = sg.Segment[0].SegmentAssignment[0].inSegment
+      ? UserSubscriptionAction.Subscribe
+      : UserSubscriptionAction.Unsubscribe;
+  } else {
+    action = null;
+  }
+  return {
+    type:
+      sg.type === "OptIn"
+        ? SubscriptionGroupType.OptIn
+        : SubscriptionGroupType.OptOut,
+    action,
+    id: sg.id,
+  };
+}
 
 export async function getSubscriptionGroupWithAssignment({
   subscriptionGroupId,
