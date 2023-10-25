a02aad4c96e9d3e69cfd2ed0aff510f784fd3cd9
Max Gurewitz <max@dittofeed.com>
2023-10-25 12:20:02 -0700

    wip

--- packages/dashboard/src/pages/users/[id].page.tsx
+++ packages/dashboard/src/pages/users/[id].page.tsx
@@ -1,4 +1,12 @@
-import { Stack, useTheme } from "@mui/material";
+import {
+  Box,
+  List,
+  ListItem,
+  ListItemText,
+  Stack,
+  Typography,
+  useTheme,
+} from "@mui/material";
 import logger from "backend-lib/src/logger";
 import { getUsers } from "backend-lib/src/users";
 import { json as codeMirrorJson } from "@codemirror/lang-json";
@@ -11,6 +19,7 @@ import MainLayout from "../../components/mainLayout";
 import { addInitialStateToProps } from "../../lib/addInitialStateToProps";
 import { requestContext } from "../../lib/requestContext";
 import { PropsWithInitialState } from "../../lib/types";
+import { ResourceListItemButton } from "../../components/resourceList";
 
 interface UserPageProps {
   user: GetUsersResponse["users"][0];
@@ -63,26 +72,73 @@ export const getServerSideProps: GetServerSideProps<
 const User: NextPage<UserPageProps> = function User(props) {
   const { user } = props;
   const theme = useTheme();
-  const properties = user.properties
-
+  const properties = JSON.stringify(
+    Object.values(user.properties).reduce<Record<string, string>>(
+      (acc, property) => {
+        acc[property.name] = property.value;
+        return acc;
+      },
+      {}
+    ),
+    null,
+    2
+  );
 
   return (
     <MainLayout>
-      <Stack>
-        <ReactCodeMirror
-          value={user.}
-          extensions={[
-            codeMirrorJson(),
-            EditorView.lineWrapping,
-            EditorView.theme({
-              "&": {
-                fontFamily: theme.typography.fontFamily,
-              },
-            }),
-          ]}
-        />
-
-        {JSON.stringify(user)}
+      <Stack direction="row" sx={{ padding: 1 }}>
+        <Stack sx={{ flex: 1 }} spacing={1}>
+          <Stack spacing={1}>
+            <Typography
+              variant="h2"
+              fontWeight={300}
+              sx={{ fontSize: 20, marginBottom: 0.5 }}
+            >
+              User Properties
+            </Typography>
+            <ReactCodeMirror
+              value={properties}
+              readOnly
+              extensions={[
+                codeMirrorJson(),
+                EditorView.lineWrapping,
+                EditorView.theme({
+                  "&": {
+                    fontFamily: theme.typography.fontFamily,
+                  },
+                }),
+              ]}
+            />
+          </Stack>
+          <Stack spacing={1}>
+            <Typography
+              variant="h2"
+              fontWeight={300}
+              sx={{ fontSize: 20, marginBottom: 0.5 }}
+            >
+              Segments
+            </Typography>
+            <List
+              sx={{
+                width: "100%",
+                bgcolor: "background.paper",
+                minHeight: "5rem",
+                borderRadius: 1,
+              }}
+            >
+              {user.segments.map((segment) => (
+                <ListItem>
+                  <ResourceListItemButton
+                    href={`/dashboard/segments/${segment.id}`}
+                  >
+                    <ListItemText>{segment.name}</ListItemText>
+                  </ResourceListItemButton>
+                </ListItem>
+              ))}
+            </List>
+          </Stack>
+        </Stack>
+        <Box sx={{ flex: 1 }}>{JSON.stringify(user)}</Box>
       </Stack>
     </MainLayout>
   );
