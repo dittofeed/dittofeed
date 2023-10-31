e490ad3836574d6fbd82c1bec824a7600cce4b91
Max Gurewitz <max@dittofeed.com>
2023-10-31 14:14:26 -0700

    queries method

--- packages/backend-lib/src/deliveries.ts
+++ packages/backend-lib/src/deliveries.ts
@@ -1,10 +1,42 @@
-import { SearchDeliveriesRequest, SearchDeliveriesResponse } from "./types";
+import {
+  clickhouseClient,
+  ClickHouseQueryBuilder,
+  streamClickhouseQuery,
+} from "./clickhouse";
+import {
+  SearchDeliveriesRequest,
+  SearchDeliveriesResponse,
+  SearchDeliveriesResponseItem,
+} from "./types";
+import { getTableVersion } from "./userEvents";
+import { buildUserEventsTableName } from "./userEvents/clickhouse";
 
 export async function searchDeliveries({
   workspaceId,
 }: SearchDeliveriesRequest): Promise<SearchDeliveriesResponse> {
+  const queryBuilder = new ClickHouseQueryBuilder();
+  const workspaceIdParam = queryBuilder.addQueryValue(workspaceId, "String");
+
+  const tableVersion = await getTableVersion({ workspaceId });
+  const query = `SELECT * FROM ${buildUserEventsTableName(
+    tableVersion
+  )} WHERE workspace_id = ${workspaceIdParam}`;
+
+  const result = await clickhouseClient().query({
+    query,
+    query_params: queryBuilder.getQueries(),
+    format: "JSONEachRow",
+  });
+
+  const items: SearchDeliveriesResponseItem[] = [];
+  await streamClickhouseQuery(result, (rows) => {
+    for (const row of rows) {
+      items.push(row as SearchDeliveriesResponseItem);
+    }
+  });
+
   return {
     workspaceId,
-    items: [],
+    items,
   };
 }
