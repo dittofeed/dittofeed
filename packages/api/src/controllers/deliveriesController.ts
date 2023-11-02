c6ed84b41f3a76bd9d0a475f543af2749e002d36
Max Gurewitz <max@dittofeed.com>
2023-11-02 09:19:21 -0700

    missing controller

---
+++ packages/api/src/controllers/deliveriesController.ts
@@ -0,0 +1,27 @@
+import { TypeBoxTypeProvider } from "@fastify/type-provider-typebox";
+import { searchDeliveries } from "backend-lib/src/deliveries";
+import {
+  SearchDeliveriesRequest,
+  SearchDeliveriesResponse,
+} from "backend-lib/src/types";
+import { FastifyInstance } from "fastify";
+
+// eslint-disable-next-line @typescript-eslint/require-await
+export default async function deliveriesController(fastify: FastifyInstance) {
+  fastify.withTypeProvider<TypeBoxTypeProvider>().get(
+    "/",
+    {
+      schema: {
+        description: "Search through deliveries.",
+        querystring: SearchDeliveriesRequest,
+        response: {
+          200: SearchDeliveriesResponse,
+        },
+      },
+    },
+    async (request, reply) => {
+      const deliveries = await searchDeliveries(request.query);
+      return reply.status(200).send(deliveries);
+    }
+  );
+}
