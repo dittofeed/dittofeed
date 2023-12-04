fb76276926dc33db08716912e63e04e68e432a93
Max Gurewitz <max@dittofeed.com>
2023-12-04 14:18:28 -0800

    integrate incremental into broadcast

--- packages/backend-lib/src/segments/broadcastWorkflow.ts
+++ packages/backend-lib/src/segments/broadcastWorkflow.ts
@@ -1,11 +1,14 @@
 import { proxyActivities } from "@temporalio/workflow";
 
 // Only import the activity types
-import type * as activities from "./computePropertiesWorkflow/activities";
+import type * as activities from "./../temporal/activities";
+import * as wf from "@temporalio/workflow";
+import { FEATURE_INCREMENTAL_COMP } from "../constants";
 
-const { performBroadcast } = proxyActivities<typeof activities>({
-  startToCloseTimeout: "5 minutes",
-});
+const { performBroadcast, getFeature, performBroadcastIncremental } =
+  proxyActivities<typeof activities>({
+    startToCloseTimeout: "5 minutes",
+  });
 
 export function generateBroadcastWorkflowId({
   workspaceId,
@@ -22,9 +25,28 @@ export interface BroadcastWorkflowParams {
   broadcastId: string;
 }
 
+async function useIncremental({
+  workspaceId,
+}: {
+  workspaceId: string;
+}): Promise<boolean> {
+  if (!wf.patched(FEATURE_INCREMENTAL_COMP)) {
+    return false;
+  }
+
+  return getFeature({
+    workspaceId,
+    name: FEATURE_INCREMENTAL_COMP,
+  });
+}
+
 export async function broadcastWorkflow({
   workspaceId,
   broadcastId,
 }: BroadcastWorkflowParams): Promise<void> {
-  await performBroadcast({ workspaceId, broadcastId });
+  if (await useIncremental({ workspaceId })) {
+    await performBroadcastIncremental({ workspaceId, broadcastId });
+  } else {
+    await performBroadcast({ workspaceId, broadcastId });
+  }
 }
