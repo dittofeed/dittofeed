eac756749f6241bca9ae21d0aa209e6fd3612785
Max Gurewitz <max@dittofeed.com>
2023-12-08 21:03:06 -0800

    flex out state

--- packages/dashboard/src/components/secretEditor.tsx
+++ packages/dashboard/src/components/secretEditor.tsx
@@ -1,6 +1,66 @@
-export interface SecretEditorProps {}
+import {
+  CompletionStatus,
+  EphemeralRequestStatus,
+} from "isomorphic-lib/src/types";
+import { useImmer } from "use-immer";
 
-export default function SecretEditor({}: SecretEditorProps) {
+import { useAppStorePick } from "../lib/appStore";
+
+export enum SecretStateType {
+  Saved = "Saved",
+  SavedEditing = "SavedEditing",
+  UnSaved = "UnSaved",
+}
+
+export interface SavedSecretState {
+  type: SecretStateType.Saved;
+}
+
+export interface SavedEditingSecretState {
+  type: SecretStateType.SavedEditing;
+}
+
+export interface UnSavedSecretState {
+  type: SecretStateType.UnSaved;
+}
+
+export type EditingSecretState =
+  | SavedEditingSecretState
+  | UnSavedSecretState
+  | SavedSecretState;
+
+export interface SecretState {
+  updateRequest: EphemeralRequestStatus<Error>;
+  editingState: EditingSecretState;
+}
+
+export interface SecretEditorProps {
+  name: string;
+  key: string;
+  saved: boolean;
+}
+
+function initialState(saved: boolean): SecretState {
+  return {
+    updateRequest: {
+      type: CompletionStatus.NotStarted,
+    },
+    editingState: saved
+      ? { type: SecretStateType.Saved }
+      : { type: SecretStateType.UnSaved },
+  };
+}
+
+export default function SecretEditor({ name, saved, key }: SecretEditorProps) {
+  const { workspace: workspaceResult } = useAppStorePick(["workspace"]);
+
+  const { editingState, updateRequest } = useImmer<SecretState>(() =>
+    initialState(saved)
+  );
+
+  if (workspaceResult.type !== CompletionStatus.Successful) {
+    return null;
+  }
   return (
     <div>
       <p>secretEditor</p>
