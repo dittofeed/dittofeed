6257b87a7568f1a2af7ed5152a5418540233ac26
Max Gurewitz <max@dittofeed.com>
2023-12-20 13:30:54 -0800

    simplify

--- packages/backend-lib/src/dates.ts
+++ packages/backend-lib/src/dates.ts
@@ -1,15 +1,8 @@
-import { add, set } from "date-fns";
-import {
-  format,
-  getTimezoneOffset,
-  utcToZonedTime,
-  formatInTimeZone,
-} from "date-fns-tz";
+import { add } from "date-fns";
+import { getTimezoneOffset } from "date-fns-tz";
 import { find as findTz } from "geo-tz";
 
-import logger from "./logger";
 import { LocalTimeDelayVariantFields } from "./types";
-import { findAllUserPropertyAssignments } from "./userProperties";
 
 const DEFAULT_TIMEZONE = "UTC";
 const EVERY_DAY_IN_WEEK = new Set([0, 1, 2, 3, 4, 5, 6]);
@@ -28,21 +21,6 @@ function getTimezone({ latLon }: { latLon: string }): string {
   return tz;
 }
 
-function modDiff(a: number, b: number, mod: number): number {
-  let diff = (a - b) % mod;
-
-  // Adjust diff to be non-negative
-  if (diff < 0) {
-    diff += mod;
-  }
-
-  return diff;
-}
-
-function padWithLeadingZeros(number: number) {
-  return String(number).padStart(2, "0");
-}
-
 export function findNextLocalizedTimeInner({
   latLon,
   now,
@@ -59,25 +37,16 @@ export function findNextLocalizedTimeInner({
   const timezone =
     typeof latLon === "string" ? getTimezone({ latLon }) : DEFAULT_TIMEZONE;
   const offset = getTimezoneOffset(timezone);
-  const zoned = new Date(offset + now);
-  let adjusted = new Date(
-    format(
-      zoned,
-      `yyyy-MM-dd'T'${padWithLeadingZeros(hour)}:${padWithLeadingZeros(
-        minute
-      )}:00.000'Z'`
-    )
-  );
+  const zoned = offset + now;
+  let adjusted = new Date(zoned);
+  adjusted.setUTCHours(hour, minute, 0, 0);
 
   const allowedDays = allowedDaysOfWeek
     ? new Set(allowedDaysOfWeek)
     : EVERY_DAY_IN_WEEK;
 
   for (let i = 0; i < 8; i++) {
-    if (
-      adjusted.getTime() > zoned.getTime() &&
-      allowedDays.has(adjusted.getDay())
-    ) {
+    if (adjusted.getTime() > zoned && allowedDays.has(adjusted.getDay())) {
       return adjusted.getTime() - offset;
     }
     adjusted = add(adjusted, { days: 1 });
