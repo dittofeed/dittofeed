930314c389647a5cbaabf804a9ee0087d279ba0c
Max Gurewitz <max@dittofeed.com>
2023-12-26 09:58:43 -0800

    more corrections for sendgrid webhook (#553)

--- packages/backend-lib/src/types.ts
+++ packages/backend-lib/src/types.ts
@@ -160,24 +160,29 @@ export enum SendgridEventType {
   GroupResubscribe = "group_resubscribe",
 }
 
-export const SendgridEvent = Type.Intersect([
-  Type.Object({
-    email: Type.String(),
-    timestamp: Type.Integer(),
-    event: Type.Enum(SendgridEventType),
-    sg_event_id: Type.String(),
-    sg_message_id: Type.String(),
-    ip: Type.Optional(Type.String()),
-    reason: Type.Optional(Type.String()),
-    pool: Type.Optional(
-      Type.Object({
-        id: Type.Number(),
-        name: Type.String(),
-      })
-    ),
-  }),
-  Type.Record(Type.String(), Type.Any()),
-]);
+export const SendgridEvent = Type.Object({
+  email: Type.String(),
+  timestamp: Type.Integer(),
+  event: Type.Enum(SendgridEventType),
+  sg_event_id: Type.String(),
+  sg_message_id: Type.String(),
+  ip: Type.Optional(Type.String()),
+  reason: Type.Optional(Type.String()),
+  pool: Type.Optional(
+    Type.Object({
+      id: Type.Number(),
+      name: Type.String(),
+    })
+  ),
+  workspaceId: Type.Optional(Type.String()),
+  runId: Type.Optional(Type.String()),
+  messageId: Type.Optional(Type.String()),
+  userId: Type.Optional(Type.String()),
+  templateId: Type.Optional(Type.String()),
+  nodeId: Type.Optional(Type.String()),
+  journeyId: Type.Optional(Type.String()),
+  anonymousId: Type.Optional(Type.String()),
+});
 
 export type SendgridEvent = Static<typeof SendgridEvent>;
 
