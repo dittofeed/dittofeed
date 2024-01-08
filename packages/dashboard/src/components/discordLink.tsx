ab86b2d9527b532c3c551b3eef3268749c7e49c1
Chandler Craig <hellochandlercraig@gmail.com>
2024-01-08 13:55:21 -0800

    add discord to docs & dashboard (#558)

--- packages/dashboard/src/components/discordLink.tsx
+++ packages/dashboard/src/components/discordLink.tsx
@@ -1,13 +1,13 @@
 import ExternalLink from "./externalLink";
 
-export default function SlackLink({
+export default function DiscordLink({
   children,
 }: {
   children?: React.ReactNode;
 }) {
   return (
     <ExternalLink
-      href="https://join.slack.com/t/dittofeed-community/shared_invite/zt-1u3lyts83-P6npff1AbjniNRLVlrlM5A"
+      href="https://discord.gg/HajPkCG4Mm"
       enableLinkStyling
     >
       {children}
--- packages/dashboard/src/pages/contact.page.tsx
+++ packages/dashboard/src/pages/contact.page.tsx
@@ -1,8 +1,8 @@
 import { Box, Stack, useTheme } from "@mui/material";
 import Head from "next/head";
 
+import DiscordLink from "../components/discordLink";
 import MainLayout from "../components/mainLayout";
-import SlackLink from "../components/slackLink";
 import SupportEmailLink from "../components/supportEmailLink";
 
 export default function Contact() {
@@ -32,7 +32,7 @@ export default function Contact() {
             <p>If you have questions or feedback, feel reach out at</p>
             <SupportEmailLink />
             <span>, or </span>
-            <SlackLink>join the Dittofeed Slack community</SlackLink>
+            <DiscordLink>join the Dittofeed Discord community</DiscordLink>
             <span>!</span>
           </Box>
         </Stack>
