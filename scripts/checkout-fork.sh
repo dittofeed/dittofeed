b7cae5ac407402c94c9bbf875e7cd6fd9f8f1637
Max Gurewitz <max@dittofeed.com>
2024-01-20 12:36:44 -0800

    wip

--- scripts/checkout-fork.sh
+++ scripts/checkout-fork.sh
@@ -9,6 +9,7 @@ fi
 REMOTE_NAME=$1
 CONTRIBUTOR_USERNAME=$2
 BRANCH_NAME=$3
+NEW_LOCAL_BRANCH_NAME="${CONTRIBUTOR_USERNAME}-${BRANCH_NAME}"
 
 # Adding the contributor's repository as a remote (if not already added)
 if ! git remote | grep -q "$CONTRIBUTOR_USERNAME"; then
@@ -16,12 +17,12 @@ if ! git remote | grep -q "$CONTRIBUTOR_USERNAME"; then
     git remote add "$CONTRIBUTOR_USERNAME" "https://github.com/$CONTRIBUTOR_USERNAME/dittofeed.git"
 fi
 
-# Fetching the pull request branch
+# Fetching the branch from the contributor's repository
 echo "Fetching from $CONTRIBUTOR_USERNAME..."
 git fetch "$CONTRIBUTOR_USERNAME"
 
-# Checking out the branch as a new local branch
-echo "Checking out the branch $BRANCH_NAME..."
-git checkout -b "$BRANCH_NAME" "$CONTRIBUTOR_USERNAME/$BRANCH_NAME"
+# Checking out the branch as a new local branch with a unique name
+echo "Checking out the branch $BRANCH_NAME as $NEW_LOCAL_BRANCH_NAME..."
+git checkout -b "$NEW_LOCAL_BRANCH_NAME" "$CONTRIBUTOR_USERNAME/$BRANCH_NAME"
 
-echo "Branch $BRANCH_NAME has been checked out."
+echo "Branch $NEW_LOCAL_BRANCH_NAME has been checked out."
