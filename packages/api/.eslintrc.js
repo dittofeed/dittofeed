5b19dce1a31a8bae5a38f74a2b8012ddd28ebc50
neo773 <62795688+neo773@users.noreply.github.com>
2024-01-26 02:39:17 +0530

    fix: prettier

--- packages/api/.eslintrc.js
+++ packages/api/.eslintrc.js
@@ -1,3 +1,3 @@
 module.exports = {
-  extends: ["plugin:@typescript-eslint/recommended-requiring-type-checking"],
+  extends: ["plugin:@typescript-eslint/recommended-requiring-type-checking", "plugin:prettier/recommended"],
 };
