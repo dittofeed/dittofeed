c0bc1050ab26d17d0fc564fdd66b686b02a2fbbf
oliverqx <8559757+oliverqx@users.noreply.github.com>
2024-01-30 17:22:59 -0800

    feat(ui): implement endpoint
    by no means functional.

--- packages/dashboard/src/pages/users.page.tsx
+++ packages/dashboard/src/pages/users.page.tsx
@@ -1,11 +1,11 @@
-import { Typography, useTheme } from "@mui/material";
+import { Fab, Typography, useTheme } from "@mui/material";
 import Stack from "@mui/material/Stack";
 import { Type } from "@sinclair/typebox";
 import { schemaValidate } from "isomorphic-lib/src/resultHandling/schemaValidation";
-import { CompletionStatus, GetUsersRequest } from "isomorphic-lib/src/types";
+import { CompletionStatus, EphemeralRequestStatus, GetUserPropertiesResponse, GetUsersRequest, UserPropertyResource } from "isomorphic-lib/src/types";
 import { GetServerSideProps } from "next";
 import { useRouter } from "next/router";
-import { useMemo } from "react";
+import React, { useMemo } from "react";
 
 import MainLayout from "../components/mainLayout";
 import UsersTable, { OnPaginationChangeProps } from "../components/usersTable";
@@ -13,6 +13,10 @@ import { addInitialStateToProps } from "../lib/addInitialStateToProps";
 import { useAppStore } from "../lib/appStore";
 import { requestContext } from "../lib/requestContext";
 import { PropsWithInitialState } from "../lib/types";
+import { create } from "zustand";
+import { immer } from "zustand/middleware/immer";
+import apiRequestHandlerFactory from "../lib/apiRequestHandlerFactory";
+import UserFilter from "../components/usersFilter";
 
 const QueryParams = Type.Pick(GetUsersRequest, ["cursor", "direction"]);
 
@@ -25,6 +29,35 @@ export const getServerSideProps: GetServerSideProps<PropsWithInitialState> =
     }),
   }));
 
+interface UserPropertiesState {
+    properties: Record<string, UserPropertyResource>,
+    getUserPropertiesRequest: EphemeralRequestStatus<Error>
+}
+
+interface UserPropertiesActions {
+    setProperties: (val: UserPropertyResource[]) => void;
+    setGetUserPropertiesRequest: (val: EphemeralRequestStatus<Error>) => void;
+}
+
+export const propertiesStore = create(
+    immer<UserPropertiesState & UserPropertiesActions>((set) => ({
+        properties: {},
+        getUserPropertiesRequest: {
+            type: CompletionStatus.NotStarted,
+        },
+        setGetUserPropertiesRequest: (request) =>
+          set((state) => {
+            state.getUserPropertiesRequest = request;
+          }),
+        setProperties: (properties) => 
+          set((state) => {
+              for (const property of properties) {
+                  state.properties[property.id] = property;
+          }})
+    })
+))
+
+
 export default function SegmentUsers() {
   const theme = useTheme();
   const router = useRouter();
@@ -36,6 +69,41 @@ export default function SegmentUsers() {
   if (workspace.type !== CompletionStatus.Successful) {
     return null;
   }
+  const apiBase = useAppStore((state) => state.apiBase);
+
+  const properties = propertiesStore((store) => store.properties);
+  const getUserPropertiesRequest = propertiesStore((store) => store.getUserPropertiesRequest);
+  const setGetUserPropertiesRequest = propertiesStore((store) => store.setGetUserPropertiesRequest);
+  const setProperties = propertiesStore((store) => store.setProperties);
+
+
+  React.useEffect(() => {
+    const setLoadResponse = (response: GetUserPropertiesResponse) => {
+        setProperties(response.properties);
+    };
+
+    const handler = apiRequestHandlerFactory({
+      request: getUserPropertiesRequest,
+      setRequest: setGetUserPropertiesRequest,
+      responseSchema: GetUserPropertiesResponse,
+      setResponse: setLoadResponse,
+      requestConfig: {
+        method: "GET",
+        url: `${apiBase}/api/user-properties`,
+        params: {
+           workspaceId: workspace.value.id 
+        },
+        headers: {
+          "Content-Type": "application/json",
+        },
+      },
+    });
+    handler();
+  }, [])
+
+  console.log({properties})
+
+
 
   const onUsersTablePaginate = ({
     direction,
@@ -50,6 +118,8 @@ export default function SegmentUsers() {
       },
     });
   };
+
+
   return (
     <MainLayout>
       <Stack
@@ -61,7 +131,10 @@ export default function SegmentUsers() {
           backgroundColor: theme.palette.grey[100],
         }}
       >
-        <Typography variant="h4">Users</Typography>
+        <Stack direction="row">
+            <Typography variant="h4">Users</Typography>
+            <UserFilter properties={Object.values(properties)}/>
+        </Stack>
         <UsersTable
           {...queryParams}
           workspaceId={workspace.value.id}
