2703d90e9fde759077677ed28b84e65c4fadad16
Mehul Mathur <64700961+mehulmathur16@users.noreply.github.com>
2024-02-14 11:55:19 +0530

    feat: added preview in deliveries table (#645)

--- packages/dashboard/src/components/deliveriesTable.tsx
+++ packages/dashboard/src/components/deliveriesTable.tsx
@@ -2,7 +2,16 @@ import {
   ArrowBackIosNewOutlined,
   ArrowForwardIosOutlined,
 } from "@mui/icons-material";
-import { Button, IconButton, Stack, Tooltip } from "@mui/material";
+import VisibilityIcon from "@mui/icons-material/Visibility";
+import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
+import {
+  Button,
+  Drawer,
+  IconButton,
+  Stack,
+  Tooltip,
+  useTheme,
+} from "@mui/material";
 import { DataGrid, GridColDef, GridRenderCellParams } from "@mui/x-data-grid";
 import axios, { AxiosResponse } from "axios";
 import { schemaValidate } from "isomorphic-lib/src/resultHandling/schemaValidation";
@@ -17,7 +26,7 @@ import {
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { ParsedUrlQuery } from "querystring";
-import React from "react";
+import React, { useState } from "react";
 import { omit } from "remeda/dist/commonjs/omit";
 import { pick } from "remeda/dist/commonjs/pick";
 import { create } from "zustand";
@@ -26,6 +35,9 @@ import { immer } from "zustand/middleware/immer";
 import { useAppStorePick } from "../lib/appStore";
 import { LinkCell, monospaceCell } from "../lib/datagridCells";
 import { getTemplatesLink } from "../lib/templatesLink";
+import EmailPreviewHeader from "./emailPreviewHeader";
+import SmsPreviewBody from "./smsPreviewBody";
+import TemplatePreview from "./templatePreview";
 
 interface TableItem {
   userId: string;
@@ -40,6 +52,10 @@ interface TableItem {
   templateId?: string;
   templateName?: string;
   channel: ChannelType;
+  body: string | null;
+  from: string | null | undefined;
+  subject: string | null | undefined;
+  replyTo: string | null | undefined;
 }
 
 interface DeliveriesState {
@@ -47,6 +63,28 @@ interface DeliveriesState {
   items: Map<string, SearchDeliveriesResponseItem>;
   paginationRequest: EphemeralRequestStatus<Error>;
 }
+
+interface PreviewObjectInterface {
+  subject: string | null | undefined;
+  from: string | null | undefined;
+  replyTo: string | null | undefined;
+  to: string;
+  body: string | null;
+  show: boolean;
+  channelType: string;
+}
+
+const initPreviewObject = () => {
+  return {
+    subject: "",
+    from: "",
+    replyTo: "",
+    to: "",
+    body: "",
+    show: false,
+    channelType: "",
+  };
+};
 
 const baseColumn: Partial<GridColDef<TableItem>> = {
   flex: 1,
@@ -143,7 +181,11 @@ export function DeliveriesTable({
   userId,
 }: Pick<SearchDeliveriesRequest, "journeyId" | "userId">) {
   const [pageItems, setPageItems] = React.useState(new Set<string>());
+  const [previewObject, setPreviewObject] =
+    useState<PreviewObjectInterface>(initPreviewObject());
   const router = useRouter();
+  const theme = useTheme();
+
   const previousCursor = getQueryValue(
     router.query,
     QUERY_PARAMETERS.PREVIOUS_CURSOR,
@@ -319,12 +361,27 @@ export function DeliveriesTable({
 
         let to: string | null = null;
         let channel: ChannelType | null = null;
+        let body: string | null = null;
+        let from: string | null | undefined = null;
+        let subject: string | null | undefined = null;
+        let replyTo: string | null | undefined = null;
         if ("variant" in item) {
           to = item.variant.to;
           channel = item.variant.type;
+          body = item.variant.body;
+
+          if (item.variant.type === ChannelType.Email) {
+            from = item.variant.from;
+            subject = item.variant.subject;
+            replyTo = item.variant.replyTo;
+          }
         } else {
           to = item.to;
+          from = item.from;
+          subject = item.subject;
+          replyTo = item.replyTo;
           channel = item.channel;
+          body = item.body ?? null;
         }
         if (!to || !channel) {
           return [];
@@ -335,8 +392,12 @@ export function DeliveriesTable({
           updatedAt: item.updatedAt,
           userId: item.userId,
           to,
+          from,
+          subject,
+          replyTo,
           status: item.status,
           channel,
+          body,
           ...origin,
           ...template,
         };
@@ -345,120 +406,222 @@ export function DeliveriesTable({
     [items, pageItems],
   );
 
-  return (
-    <Stack sx={{ width: "100%" }} spacing={1}>
-      <DataGrid
-        rows={rows}
-        loading={paginationRequest.type === CompletionStatus.InProgress}
-        columns={[
-          {
-            field: "userId",
-            headerName: "User ID",
-            renderCell: ({ row }: GridRenderCellParams<TableItem>) => {
-              const href = `/users/${row.userId}`;
-              return <LinkCell href={href} title={row.userId} />;
-            },
-          },
-          {
-            field: "to",
-            headerName: "To",
-            renderCell: ({ row }: GridRenderCellParams<TableItem>) => {
-              const href = `/users/${row.userId}`;
-              return <LinkCell href={href} title={row.to} />;
-            },
-          },
-          {
-            field: "status",
-            headerName: "Status",
-          },
-          {
-            field: "originId",
-            flex: 1,
-            headerName: "Journey / Broadcast",
-            renderCell: ({ row }: GridRenderCellParams<TableItem>) => {
-              const href =
-                row.originType === "broadcast"
-                  ? `/broadcasts/review/${row.originId}`
-                  : `/journeys/configure/${row.originId}`;
-              return <ButtonLinkCell href={href} value={row.originName} />;
-            },
-          },
-          {
-            field: "templateId",
-            headerName: "Template",
-            renderCell: ({ row }: GridRenderCellParams<TableItem>) => {
-              const href =
-                row.originType === "broadcast"
-                  ? `/broadcasts/template/${row.originId}`
-                  : getTemplatesLink({
-                      channel: row.channel,
-                      id: row.originId,
-                    });
-              let value: string;
-              if (!row.templateName) {
-                if (row.originType !== "broadcast") {
-                  return null;
-                }
-                value = "Broadcast Template";
-              } else {
-                value = row.templateName;
-              }
-              return <ButtonLinkCell href={href} value={value} />;
-            },
-          },
-          {
-            field: "sentAt",
-            headerName: "Sent At",
-          },
-          {
-            field: "updatedAt",
-            headerName: "Updated At",
-          },
-        ].map((c) => ({ ...baseColumn, ...c }))}
-        pagination
-        paginationMode="server"
-        pageSizeOptions={[pageSize]}
-        autoHeight
-        hideFooter
+  const renderEmailPreviewBody = (body: string) => {
+    return (
+      <iframe
+        srcDoc={`<!DOCTYPE html>${body ?? ""}`}
+        title="email-body-preview"
+        style={{
+          border: "none",
+          height: "100%",
+          width: "100%",
+          padding: theme.spacing(1),
+        }}
       />
-      <Stack
-        direction="row"
-        alignItems="center"
-        justifyContent="flex-end"
-        spacing={1}
-      >
-        <IconButton
-          disabled={!currentCursor}
-          onClick={() => {
-            router.push({
-              pathname: router.pathname,
-              query: {
-                ...omit(router.query, [QUERY_PARAMETERS.PREVIOUS_CURSOR]),
-                [QUERY_PARAMETERS.CURRENT_CURSOR]: previousCursor,
-                [QUERY_PARAMETERS.NEXT_CURSOR]: currentCursor,
+    );
+  };
+
+  const renderSmsPreviewBody = (body: string) => {
+    return <SmsPreviewBody body={body} />;
+  };
+
+  const renderPreviewBody = (po: PreviewObjectInterface) => {
+    if (!po.body) return null;
+    if (po.channelType === "Email") {
+      return renderEmailPreviewBody(po.body);
+    }
+
+    return renderSmsPreviewBody(po.body);
+  };
+
+  const renderEmailPreviewHeader = (po: PreviewObjectInterface) => {
+    return (
+      <EmailPreviewHeader
+        email={po.to}
+        from={po.from}
+        subject={po.subject}
+        replyTo={po.replyTo}
+      />
+    );
+  };
+
+  const renderPreviewHeader = (po: PreviewObjectInterface) => {
+    if (po.channelType === "Email") {
+      return renderEmailPreviewHeader(po);
+    }
+    return null;
+  };
+
+  const getPreviewVisibilityHandler = () => {
+    return (
+      <VisibilityOffIcon
+        fontSize="small"
+        onClick={() => setPreviewObject(initPreviewObject())}
+        sx={{ cursor: "pointer" }}
+      />
+    );
+  };
+
+  const preview = (
+    <TemplatePreview
+      previewHeader={renderPreviewHeader(previewObject)}
+      previewBody={renderPreviewBody(previewObject)}
+      visibilityHandler={getPreviewVisibilityHandler()}
+      bodyPreviewHeading="Delivery Preview"
+    />
+  );
+
+  return (
+    <>
+      <Stack sx={{ width: "100%" }} spacing={1}>
+        <DataGrid
+          rows={rows}
+          loading={paginationRequest.type === CompletionStatus.InProgress}
+          columns={[
+            {
+              field: "userId",
+              headerName: "User ID",
+              renderCell: ({ row }: GridRenderCellParams<TableItem>) => {
+                const href = `/users/${row.userId}`;
+                return <LinkCell href={href} title={row.userId} />;
               },
-            });
-          }}
+            },
+            {
+              field: "to",
+              headerName: "To",
+              renderCell: ({ row }: GridRenderCellParams<TableItem>) => {
+                const href = `/users/${row.userId}`;
+                return <LinkCell href={href} title={row.to} />;
+              },
+            },
+            {
+              field: "status",
+              headerName: "Status",
+            },
+            {
+              field: "originId",
+              flex: 1,
+              headerName: "Journey / Broadcast",
+              renderCell: ({ row }: GridRenderCellParams<TableItem>) => {
+                const href =
+                  row.originType === "broadcast"
+                    ? `/broadcasts/review/${row.originId}`
+                    : `/journeys/configure/${row.originId}`;
+                return <ButtonLinkCell href={href} value={row.originName} />;
+              },
+            },
+            {
+              field: "templateId",
+              headerName: "Template",
+              renderCell: ({ row }: GridRenderCellParams<TableItem>) => {
+                const href =
+                  row.originType === "broadcast"
+                    ? `/broadcasts/template/${row.originId}`
+                    : getTemplatesLink({
+                        channel: row.channel,
+                        id: row.originId,
+                      });
+                let value: string;
+                if (!row.templateName) {
+                  if (row.originType !== "broadcast") {
+                    return null;
+                  }
+                  value = "Broadcast Template";
+                } else {
+                  value = row.templateName;
+                }
+                return <ButtonLinkCell href={href} value={value} />;
+              },
+            },
+            {
+              field: "sentAt",
+              headerName: "Sent At",
+            },
+            {
+              field: "updatedAt",
+              headerName: "Updated At",
+            },
+            {
+              field: "Preview",
+              headerName: "Preview",
+              renderCell: ({ row }: GridRenderCellParams<TableItem>) => {
+                return (
+                  <VisibilityIcon
+                    sx={{ color: "#262626", cursor: "pointer" }}
+                    onClick={() => {
+                      setPreviewObject({
+                        body: row.body,
+                        show: true,
+                        channelType: row.channel,
+                        subject: row.subject,
+                        from: row.from,
+                        to: row.to,
+                        replyTo: row.replyTo,
+                      });
+                    }}
+                    fontSize="small"
+                  />
+                );
+              },
+            },
+          ].map((c) => ({ ...baseColumn, ...c }))}
+          pagination
+          paginationMode="server"
+          pageSizeOptions={[pageSize]}
+          autoHeight
+          hideFooter
+        />
+        <Stack
+          direction="row"
+          alignItems="center"
+          justifyContent="flex-end"
+          spacing={1}
         >
-          <ArrowBackIosNewOutlined />
-        </IconButton>
-        <IconButton
-          disabled={!nextCursor}
-          onClick={() => {
-            const query = {
-              ...omit(router.query, [QUERY_PARAMETERS.NEXT_CURSOR]),
-              [QUERY_PARAMETERS.PREVIOUS_CURSOR]: currentCursor,
-              [QUERY_PARAMETERS.CURRENT_CURSOR]: nextCursor,
-            };
-            router.push({
-              pathname: router.pathname,
-              query,
-            });
-          }}
-        >
-          <ArrowForwardIosOutlined />
-        </IconButton>
+          <IconButton
+            disabled={!currentCursor}
+            onClick={() => {
+              router.push({
+                pathname: router.pathname,
+                query: {
+                  ...omit(router.query, [QUERY_PARAMETERS.PREVIOUS_CURSOR]),
+                  [QUERY_PARAMETERS.CURRENT_CURSOR]: previousCursor,
+                  [QUERY_PARAMETERS.NEXT_CURSOR]: currentCursor,
+                },
+              });
+            }}
+          >
+            <ArrowBackIosNewOutlined />
+          </IconButton>
+          <IconButton
+            disabled={!nextCursor}
+            onClick={() => {
+              const query = {
+                ...omit(router.query, [QUERY_PARAMETERS.NEXT_CURSOR]),
+                [QUERY_PARAMETERS.PREVIOUS_CURSOR]: currentCursor,
+                [QUERY_PARAMETERS.CURRENT_CURSOR]: nextCursor,
+              };
+              router.push({
+                pathname: router.pathname,
+                query,
+              });
+            }}
+          >
+            <ArrowForwardIosOutlined />
+          </IconButton>
+        </Stack>
       </Stack>
-    </Stack>
+      <Drawer
+        open={previewObject.show}
+        onClose={() => {
+          setPreviewObject(initPreviewObject());
+        }}
+        anchor="bottom"
+        sx={{
+          zIndex: "2000",
+        }}
+      >
+        {preview}
+      </Drawer>
+    </>
   );
 }
