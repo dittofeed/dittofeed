2e00b074889cad64ab7c21b387a69827a452a81c
Max Gurewitz <max@dittofeed.com>
2024-02-16 17:58:08 -0800

    wip

--- packages/dashboard/src/components/journeys/nodeEditor.tsx
+++ packages/dashboard/src/components/journeys/nodeEditor.tsx
@@ -24,6 +24,7 @@ import {
   ChannelType,
   CompletionStatus,
   DelayVariantType,
+  EntryNode,
   JourneyNodeType,
   MessageTemplateResource,
   SegmentResource,
@@ -177,7 +178,47 @@ function EntryNodeFields({
       assertUnreachable(nodeVariant);
   }
   // TODO implement variant selector
-  return <>{variant}</>;
+  return (
+    <>
+      <Select
+        value={nodeProps.variant.type}
+        onChange={(e) => {
+          updateJourneyNodeData(nodeId, (node) => {
+            const props = node.data.nodeTypeProps;
+            if (props.type !== AdditionalJourneyNodeType.UiEntryNode) {
+              return;
+            }
+            const type = e.target.value as EntryNode["type"];
+            if (props.variant.type === type) {
+              return;
+            }
+            switch (type) {
+              case JourneyNodeType.SegmentEntryNode:
+                props.variant = {
+                  type: JourneyNodeType.SegmentEntryNode,
+                };
+                break;
+              case JourneyNodeType.EventEntryNode:
+                props.variant = {
+                  type: JourneyNodeType.EventEntryNode,
+                };
+                break;
+              default:
+                assertUnreachable(type);
+            }
+          });
+        }}
+      >
+        <MenuItem value={JourneyNodeType.SegmentEntryNode}>
+          Segment Entry
+        </MenuItem>
+        <MenuItem value={JourneyNodeType.EventEntryNode}>
+          Event Triggered Entry
+        </MenuItem>
+      </Select>
+      {variant}
+    </>
+  );
 }
 
 function getTemplateLabel(tr: MessageTemplateResource) {
