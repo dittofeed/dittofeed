41afeaf608a7ace068b2340168e1c49a7ea104c8
Max Gurewitz <max@dittofeed.com>
2024-02-16 17:22:57 -0800

    wip

--- packages/backend-lib/src/apps.ts
+++ packages/backend-lib/src/apps.ts
@@ -1,17 +1,8 @@
 import * as R from "remeda";
 
 import { submitTrack } from "./apps/track";
-import {
-  triggerEventEntryJourneys,
-} from "./journeys";
-import {
-  BatchAppData,
-  EventType,
-  IdentifyData,
-  PageData,
-  ScreenData,
-  TrackData,
-} from "./types";
+import { triggerEventEntryJourneys } from "./journeys";
+import { IdentifyData, PageData, ScreenData, TrackData } from "./types";
 import { InsertUserEvent, insertUserEvents } from "./userEvents";
 
 export async function submitIdentify({
@@ -114,43 +105,5 @@ export async function submitScreen({
   await insertUserEvents({
     workspaceId,
     userEvents: [userEvent],
-  });
-}
-
-export interface SubmitBatchOptions {
-  workspaceId: string;
-  data: BatchAppData;
-}
-
-export function buildBatchUserEvents(data: BatchAppData): InsertUserEvent[] {
-  const { context, batch } = data;
-
-  return batch.map((message) => {
-    let rest: Record<string, unknown>;
-    let timestamp: string;
-    const messageRaw: Record<string, unknown> = { context };
-
-    if (message.type === EventType.Identify) {
-      rest = R.omit(message, ["timestamp", "traits"]);
-      timestamp = message.timestamp ?? new Date().toISOString();
-      messageRaw.traits = message.traits ?? {};
-    } else {
-      rest = R.omit(message, ["timestamp", "properties"]);
-      timestamp = message.timestamp ?? new Date().toISOString();
-      messageRaw.properties = message.properties ?? {};
-    }
-
-    Object.assign(
-      messageRaw,
-      {
-        timestamp,
-      },
-      rest,
-    );
-
-    return {
-      messageId: message.messageId,
-      messageRaw: JSON.stringify(messageRaw),
-    };
   });
 }
--- packages/backend-lib/src/apps/batch.ts
+++ packages/backend-lib/src/apps/batch.ts
@@ -1,7 +1,45 @@
-import { SubmitBatchOptions, buildBatchUserEvents } from "../apps";
+import * as R from "remeda";
 
-import { insertUserEvents } from "../userEvents";
+import { BatchAppData, EventType } from "../types";
+import { InsertUserEvent, insertUserEvents } from "../userEvents";
 
+export interface SubmitBatchOptions {
+  workspaceId: string;
+  data: BatchAppData;
+}
+
+export function buildBatchUserEvents(data: BatchAppData): InsertUserEvent[] {
+  const { context, batch } = data;
+
+  return batch.map((message) => {
+    let rest: Record<string, unknown>;
+    let timestamp: string;
+    const messageRaw: Record<string, unknown> = { context };
+
+    if (message.type === EventType.Identify) {
+      rest = R.omit(message, ["timestamp", "traits"]);
+      timestamp = message.timestamp ?? new Date().toISOString();
+      messageRaw.traits = message.traits ?? {};
+    } else {
+      rest = R.omit(message, ["timestamp", "properties"]);
+      timestamp = message.timestamp ?? new Date().toISOString();
+      messageRaw.properties = message.properties ?? {};
+    }
+
+    Object.assign(
+      messageRaw,
+      {
+        timestamp,
+      },
+      rest,
+    );
+
+    return {
+      messageId: message.messageId,
+      messageRaw: JSON.stringify(messageRaw),
+    };
+  });
+}
 
 // FIXME do event triggers
 export async function submitBatch({ workspaceId, data }: SubmitBatchOptions) {
