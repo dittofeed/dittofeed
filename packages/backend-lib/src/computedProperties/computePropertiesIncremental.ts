ccfa3430b743846f5424118e7989aaf22255a4da
Max Gurewitz <max@dittofeed.com>
2024-03-16 16:46:36 -0700

    wip

--- packages/backend-lib/src/computedProperties/computePropertiesIncremental.ts
+++ packages/backend-lib/src/computedProperties/computePropertiesIncremental.ts
@@ -1640,6 +1640,7 @@ function assignUserPropertiesQuery({
   if (!ac.stateIds.length) {
     return null;
   }
+  // FIXME
   const lowerBoundClause =
     periodBound && periodBound !== 0
       ? `and computed_at >= toDateTime64(${periodBound / 1000}, 3)`
@@ -1679,97 +1680,39 @@ function assignUserPropertiesQuery({
         user_id,
         CAST((groupArray(state_id), groupArray(last_value)), 'Map(String, String)') as last_value,
         CAST((groupArray(state_id), groupArray(unique_count)), 'Map(String, Int32)') as unique_count,
-        CAST((groupArray(state_id), groupArray(max_event_time)), 'Map(String, DateTime64(3))') as max_event_time,
-        CAST(
-          (
-            groupArray(state_id),
-            groupArray(events)
-          ),
-          'Map(String, Array(Tuple(String, DateTime64(3), String)))'
-        ) as grouped_events
+        CAST((groupArray(state_id), groupArray(max_event_time)), 'Map(String, DateTime64(3))') as max_event_time
       from (
         select
-          inner2.workspace_id as workspace_id,
-          inner2.type as type,
-          inner2.computed_property_id as computed_property_id,
-          inner2.state_id as state_id,
-          inner2.user_id as user_id,
-          inner2.last_value as last_value,
-          inner2.unique_count as unique_count,
-          inner2.max_event_time as max_event_time,
-          groupArray((inner2.event, inner2.event_time, inner2.properties)) as events
-        from (
-          select
-            inner1.workspace_id as workspace_id,
-            inner1.type as type,
-            inner1.computed_property_id as computed_property_id,
-            inner1.state_id as state_id,
-            inner1.user_id as user_id,
-            inner1.last_value as last_value,
-            inner1.unique_count as unique_count,
-            inner1.max_event_time as max_event_time,
-            ue.event as event,
-            ue.event_time as event_time,
-            ue.properties as properties
-          from user_events_v2 ue
-          right any join (
-            select
-              workspace_id,
-              type,
-              computed_property_id,
-              state_id,
-              user_id,
-              argMaxMerge(last_value) last_value,
-              uniqMerge(unique_count) unique_count,
-              max(event_time) max_event_time,
-              arrayJoin(groupArrayMerge(cps.grouped_message_ids)) message_id
-            from computed_property_state_v2 cps
-            where
-              (
-                workspace_id,
-                type,
-                computed_property_id,
-                state_id,
-                user_id
-              ) in (${boundedQuery})
-            group by
-              workspace_id,
-              type,
-              computed_property_id,
-              state_id,
-              user_id
-          ) as inner1 on
-            inner1.message_id != ''
-            and inner1.message_id = ue.message_id
-          group by
-            workspace_id,
-            type,
-            computed_property_id,
-            state_id,
-            user_id,
-            last_value,
-            unique_count,
-            max_event_time,
-            event,
-            event_time,
-            properties
-        ) inner2
-        group by
           workspace_id,
           type,
           computed_property_id,
           state_id,
           user_id,
-          last_value,
-          unique_count,
-          max_event_time
-      ) inner3
+          argMaxMerge(last_value) last_value,
+          uniqMerge(unique_count) unique_count,
+          max(event_time) max_event_time
+        from computed_property_state_v2 cps
+        where
+          (
+            workspace_id,
+            type,
+            computed_property_id,
+            state_id,
+            user_id
+          ) in (${boundedQuery})
+        group by
+          workspace_id,
+          type,
+          computed_property_id,
+          state_id,
+          user_id
+      )
       group by
         workspace_id,
         type,
         computed_property_id,
         user_id
-    ) inner4
+    )
   `;
   return query;
 }
