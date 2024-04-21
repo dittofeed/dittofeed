dfd9af94ecbcdb9bd5417d2115bca0301751b327
Max Gurewitz <max@dittofeed.com>
2024-04-20 23:45:50 -0700

    wip

--- packages/dashboard/src/pages/auth/single-tenant.page.tsx
+++ packages/dashboard/src/pages/auth/single-tenant.page.tsx
@@ -1,16 +1,22 @@
 import { LoadingButton } from "@mui/lab";
 import { Stack, TextField, useTheme } from "@mui/material";
 import axios, { AxiosError } from "axios";
-import backendConfig from "backend-lib/src/config";
+import backendConfig, { DEFAULT_BACKEND_CONFIG } from "backend-lib/src/config";
 import { SESSION_KEY } from "backend-lib/src/requestContext";
 import { UNAUTHORIZED_PAGE } from "isomorphic-lib/src/constants";
-import { GetServerSideProps } from "next";
+import { GetServerSideProps, NextPage } from "next";
 import { useRouter } from "next/router";
 import React from "react";
 
 import NavCard from "../../components/layout/drawer/drawerContent/navCard";
 
-export const getServerSideProps: GetServerSideProps = async (ctx) => {
+interface SingleTenantAuthProps {
+  warnings: string[];
+}
+
+export const getServerSideProps: GetServerSideProps<
+  SingleTenantAuthProps
+> = async (ctx) => {
   if (backendConfig().authMode !== "single-tenant") {
     return {
       redirect: {
@@ -27,79 +33,118 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
       },
     };
   }
+  const warnings: string[] = [];
+
+  const {
+    password,
+    databasePassword,
+    clickhousePassword,
+    secretKey,
+    singleTenantCookieSecure,
+  } = backendConfig();
+
+  if (password === DEFAULT_BACKEND_CONFIG.password) {
+    warnings.push(
+      "Default password is being used. Please configure the PASSWORD environment variable.",
+    );
+  }
+
+  if (databasePassword === DEFAULT_BACKEND_CONFIG.databasePassword) {
+    warnings.push(
+      "Default database password is being used. Please configure the DATABASE_PASSWORD environment variable.",
+    );
+  }
+
+  if (clickhousePassword === DEFAULT_BACKEND_CONFIG.clickhousePassword) {
+    warnings.push(
+      "Default clickhouse password is being used. Please configure the CLICKHOUSE_PASSWORD environment variable.",
+    );
+  }
+
+  if (!singleTenantCookieSecure) {
+    warnings.push(
+      "Single tenant cookie is not secure. Please use tls and set SINGLE_TENANT_COOKIE_SECURE='true'.",
+    );
+  }
+
   return {
-    props: {},
+    props: {
+      warnings,
+    },
   };
 };
 
 const APPLICATION_ERROR = "API Error: something wen't wrong.";
 
-export default function SingleTenantAuth() {
-  const path = useRouter();
-  const theme = useTheme();
-  const [password, setPassword] = React.useState("");
-  const [error, setError] = React.useState("");
-  const [loading, setLoading] = React.useState(false);
-  const submit = async () => {
-    if (loading) {
-      return;
-    }
-    setLoading(true);
-    try {
-      await axios.post("/api/public/single-tenant/login", {
-        password,
-      });
-      path.push("/");
-    } catch (e) {
-      setLoading(false);
-      if (!(e instanceof AxiosError) || e.response?.status !== 401) {
-        setError(APPLICATION_ERROR);
+const SingleTenantAuth: NextPage<SingleTenantAuthProps> =
+  function SingleTenantAuth() {
+    const path = useRouter();
+    const theme = useTheme();
+    const [password, setPassword] = React.useState("");
+    const [error, setError] = React.useState("");
+    const [loading, setLoading] = React.useState(false);
+    const submit = async () => {
+      if (loading) {
         return;
       }
-      setError("Invalid password");
-    }
+      setLoading(true);
+      try {
+        await axios.post("/api/public/single-tenant/login", {
+          password,
+        });
+        path.push("/");
+      } catch (e) {
+        setLoading(false);
+        if (!(e instanceof AxiosError) || e.response?.status !== 401) {
+          setError(APPLICATION_ERROR);
+          return;
+        }
+        setError("Invalid password");
+      }
+    };
+
+    return (
+      <Stack
+        sx={{ width: "100%", height: "100vh" }}
+        alignItems="center"
+        justifyContent="center"
+        direction="column"
+        spacing={1}
+      >
+        <NavCard />
+        <Stack direction="row" spacing={1}>
+          <TextField
+            error={!!error}
+            sx={{
+              maxWidth: theme.spacing(75),
+              height: "3.3rem",
+            }}
+            label="Admin Password"
+            type="password"
+            value={password}
+            onKeyDown={(e) => {
+              if (e.key === "Enter") {
+                submit();
+              }
+            }}
+            helperText={error}
+            onChange={(e) => {
+              setError("");
+              setPassword(e.target.value);
+            }}
+          />
+          <LoadingButton
+            disabled={loading}
+            loading={loading}
+            onClick={submit}
+            sx={{ height: "3.3rem" }}
+            variant="contained"
+          >
+            Login
+          </LoadingButton>
+        </Stack>
+      </Stack>
+    );
   };
 
-  return (
-    <Stack
-      sx={{ width: "100%", height: "100vh" }}
-      alignItems="center"
-      justifyContent="center"
-      direction="column"
-      spacing={1}
-    >
-      <NavCard />
-      <Stack direction="row" spacing={1}>
-        <TextField
-          error={!!error}
-          sx={{
-            maxWidth: theme.spacing(75),
-            height: "3.3rem",
-          }}
-          label="Admin Password"
-          type="password"
-          value={password}
-          onKeyDown={(e) => {
-            if (e.key === "Enter") {
-              submit();
-            }
-          }}
-          helperText={error}
-          onChange={(e) => {
-            setError("");
-            setPassword(e.target.value);
-          }}
-        />
-        <LoadingButton
-          disabled={loading}
-          loading={loading}
-          onClick={submit}
-          sx={{ height: "3.3rem" }}
-          variant="contained"
-        >
-          Login
-        </LoadingButton>
-      </Stack>
-    </Stack>
-  );
-}
+export default SingleTenantAuth;
