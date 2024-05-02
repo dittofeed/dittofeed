b38de6fc3952d800049ec67215e90bfbb387c509
Max Gurewitz <max@dittofeed.com>
2024-05-02 10:06:22 -0700

    wip

---
+++ packages/api/src/openTelemetry.ts
@@ -0,0 +1,50 @@
+import {
+  ExplicitBucketHistogramAggregation,
+  InstrumentType,
+  View,
+} from "@opentelemetry/sdk-metrics";
+import { initOpenTelemetry } from "backend-lib/src/openTelemetry";
+import { FastifyRequest } from "fastify";
+
+import config from "./config";
+import { getWorkspaceIdFromReq } from "./workspace";
+
+const apiConfig = config();
+const { apiServiceName: serviceName } = apiConfig;
+
+const telemetryConfig: Parameters<
+  typeof initOpenTelemetry
+>[0]["configOverrides"] = {
+  "@opentelemetry/instrumentation-http": {
+    ignoreIncomingPaths: ["/api"],
+  },
+  "@opentelemetry/instrumentation-fs": {
+    enabled: false,
+  },
+  "@opentelemetry/instrumentation-fastify": {
+    requestHook: (span, info) => {
+      const request = info.request as FastifyRequest;
+      const workspaceId = getWorkspaceIdFromReq(request);
+      if (workspaceId) {
+        span.updateName("api-custom-request-hook");
+        span.setAttribute("workspaceId", workspaceId);
+      }
+    },
+  },
+};
+
+export function initApiOpenTelemetry() {
+  return initOpenTelemetry({
+    serviceName,
+    configOverrides: telemetryConfig,
+    meterProviderViews: [
+      new View({
+        aggregation: new ExplicitBucketHistogramAggregation([
+          200, 300, 400, 500,
+        ]),
+        instrumentName: "api-statuses",
+        instrumentType: InstrumentType.HISTOGRAM,
+      }),
+    ],
+  });
+}
