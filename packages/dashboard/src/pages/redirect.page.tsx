cbd5ae074e4179da727c712c60b7e7a5d864d9b3
Max Gurewitz <max@dittofeed.com>
2024-05-02 19:51:18 -0700

    wip

--- packages/dashboard/src/pages/redirect.page.tsx
+++ packages/dashboard/src/pages/redirect.page.tsx
@@ -1,20 +1,53 @@
 import { CircularProgress, Stack } from "@mui/material";
+import logger from "backend-lib/src/logger";
+import { GetServerSideProps, NextPage } from "next";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
+import { URL } from "url";
 
-export function RedirectPage() {
+interface RedirectPageProps {
+  returnTo: string;
+}
+
+export const getServerSideProps: GetServerSideProps<RedirectPageProps> = async (
+  ctx,
+) => {
+  const { host, referer } = ctx.req.headers;
+  const referrerHost =
+    typeof referer === "string" ? new URL(referer).host : null;
+
+  if (host !== referrerHost) {
+    logger().error({ host, referer }, "Host and referer do not match.");
+    return {
+      notFound: true,
+    };
+  }
+
+  const { returnTo } = ctx.query;
+
+  if (typeof returnTo !== "string") {
+    logger().error({ returnTo }, "Invalid returnTo query parameter.");
+    return {
+      notFound: true,
+    };
+  }
+
+  logger().debug({ returnTo }, "Redirecting to returnTo.");
+  return {
+    props: {
+      returnTo,
+    },
+  };
+};
+
+const RedirectPage: NextPage<RedirectPageProps> = function RedirectPage({
+  returnTo,
+}) {
   const router = useRouter();
   useEffect(() => {
-    const { returnTo } = router.query;
-    if (typeof returnTo !== "string") {
-      router.push("/");
-      return;
-    }
     router.push(returnTo);
   });
-  return (
-    <Stack alignContent="center" justifyContent="center">
-      <CircularProgress />
-    </Stack>
-  );
-}
+  return <div />;
+};
+
+export default RedirectPage;
