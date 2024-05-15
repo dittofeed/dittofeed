728e1532dcdec4a1a12976ce72a51983e279e2f3
Max Gurewitz <max@dittofeed.com>
2024-05-15 14:09:27 -0700

    wip

--- packages/dashboard/src/pages/contact.page.tsx
+++ packages/dashboard/src/pages/contact.page.tsx
@@ -1,42 +1,35 @@
 import { Box, Stack, useTheme } from "@mui/material";
-import Head from "next/head";
 
+import DashboardContent from "../components/dashboardContent";
 import DiscordLink from "../components/discordLink";
-import MainLayout from "../components/mainLayout";
 import SupportEmailLink from "../components/supportEmailLink";
 
 export default function Contact() {
   const theme = useTheme();
   return (
-    <>
-      <Head>
-        <title>Dittofeed</title>
-        <meta name="description" content="Open Source Customer Engagement" />
-      </Head>
-      <MainLayout>
-        <Stack
-          direction="column"
-          alignItems="center"
-          justifyContent="center"
-          sx={{ width: "100%", height: "100%" }}
+    <DashboardContent>
+      <Stack
+        direction="column"
+        alignItems="center"
+        justifyContent="center"
+        sx={{ width: "100%", height: "100%" }}
+      >
+        <Box
+          sx={{
+            backgroundColor: "background.paper",
+            border: `1px solid ${theme.palette.grey[200]}`,
+            padding: 2,
+            borderRadius: 1,
+          }}
         >
-          <Box
-            sx={{
-              backgroundColor: "background.paper",
-              border: `1px solid ${theme.palette.grey[200]}`,
-              padding: 2,
-              borderRadius: 1,
-            }}
-          >
-            <p>We&apos;re a small team, and we&apos;d love to hear from you.</p>
-            <p>If you have questions or feedback, feel reach out at</p>
-            <SupportEmailLink />
-            <span>, or </span>
-            <DiscordLink>join the Dittofeed Discord community</DiscordLink>
-            <span>!</span>
-          </Box>
-        </Stack>
-      </MainLayout>
-    </>
+          <p>We&apos;re a small team, and we&apos;d love to hear from you.</p>
+          <p>If you have questions or feedback, feel reach out at</p>
+          <SupportEmailLink />
+          <span>, or </span>
+          <DiscordLink>join the Dittofeed Discord community</DiscordLink>
+          <span>!</span>
+        </Box>
+      </Stack>
+    </DashboardContent>
   );
 }
