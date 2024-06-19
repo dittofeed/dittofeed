90598d16efdb1f870497b6745e3cc7692f93f675
Max Gurewitz <max@dittofeed.com>
2024-06-19 15:54:34 -0700

    wip

--- packages/backend-lib/src/apps/files.ts
+++ packages/backend-lib/src/apps/files.ts
@@ -1,12 +1,27 @@
+import { PutObjectCommand } from "@aws-sdk/client-s3";
+import { storage } from "../blobStorage";
 import { AppDataFiles, TrackEventProperties } from "../types";
+import config from "../config";
 
 interface TrackEventForFiles {
   files: AppDataFiles;
+  messageId: string;
   properties: TrackEventProperties;
 }
 
-export async function persistFiles(
-  _events: TrackEventForFiles[],
+export async function persistFile(
+  event: TrackEventForFiles,
 ): Promise<TrackEventProperties[]> {
+  const promises = event.files.map((file) => {
+    const body = new TextEncoder().encode(file.data);
+    const uploadFiles = storage().send(
+      new PutObjectCommand({
+        Bucket: config().blobStorageBucket,
+        Key: `event-files/${event.messageId}/${file.name}]}`,
+        Body: body,
+      }),
+    );
+  });
+  await Promise.all(promises);
   return [];
 }
