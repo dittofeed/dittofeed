04d039e03cd4269397dc671a3c7bdc88fec87312
Max Gurewitz <max@dittofeed.com>
2024-06-28 17:35:19 -0700

    render webhook delivery body

---
+++ packages/dashboard/src/components/messages/webhookPreview.tsx
@@ -0,0 +1,29 @@
+import { json as codeMirrorJson, jsonParseLinter } from "@codemirror/lang-json";
+import { linter, lintGutter } from "@codemirror/lint";
+import { EditorView } from "@codemirror/view";
+import { useTheme } from "@mui/material";
+import ReactCodeMirror from "@uiw/react-codemirror";
+
+export function WebhookPreviewBody({ body }: { body: string }) {
+  const theme = useTheme();
+  return (
+    <ReactCodeMirror
+      value={body}
+      height="100%"
+      readOnly
+      editable={false}
+      extensions={[
+        codeMirrorJson(),
+        linter(jsonParseLinter()),
+        EditorView.lineWrapping,
+        EditorView.editable.of(false),
+        EditorView.theme({
+          "&": {
+            fontFamily: theme.typography.fontFamily,
+          },
+        }),
+        lintGutter(),
+      ]}
+    />
+  );
+}
