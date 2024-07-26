545bfd8a896819a53c9ef5ce8976feac6eb29c37
Max Gurewitz <max@dittofeed.com>
2024-07-25 17:36:43 -0700

    wip

--- packages/backend-lib/src/openTelemetry/constants.ts
+++ packages/backend-lib/src/openTelemetry/constants.ts
@@ -1,12 +1,9 @@
-let SERVICE_NAME: string | null = null;
+let SERVICE_NAME = "backend-lib";
 
 export function setServiceName(serviceName: string) {
   SERVICE_NAME = serviceName;
 }
 
 export function getServiceName() {
-  if (!SERVICE_NAME) {
-    throw new Error("Service name not set");
-  }
   return SERVICE_NAME;
 }
