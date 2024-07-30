69764d4375b7964e95baa7375c913af9fb548a1d
Max Gurewitz <max@dittofeed.com>
2024-07-30 16:12:56 -0700

    rough draft admin api key

--- packages/api/src/buildApp/adminAuth.ts
+++ packages/api/src/buildApp/adminAuth.ts
@@ -1,11 +1,68 @@
 import logger from "backend-lib/src/logger";
-import prisma from "backend-lib/src/prisma";
+import prisma, { Prisma } from "backend-lib/src/prisma";
 import { FastifyInstance } from "fastify";
 import fp from "fastify-plugin";
-import { schemaValidate } from "isomorphic-lib/src/resultHandling/schemaValidation";
+import {
+  jsonParseSafe,
+  schemaValidate,
+} from "isomorphic-lib/src/resultHandling/schemaValidation";
 import { AdminApiKeyDefinition } from "isomorphic-lib/src/types";
 
 import { getWorkspaceId } from "../workspace";
+
+export async function authenticateAdminApiKey({
+  workspaceId,
+  actualKey,
+}: {
+  workspaceId: string;
+  actualKey: string;
+}): Promise<boolean> {
+  const apiKeysQuery = Prisma.sql`
+      SELECT
+        aak.id,
+        s."configValue"
+      FROM "AdminApiKey" aak
+      JOIN "Secret" s ON aak."secretId" = s.id
+      WHERE
+        aak."workspaceId" = ${workspaceId}
+        OR aak."workspaceId" IN (
+          SELECT wr."childWorkspaceId"
+          FROM "WorkspaceRelation" wr
+          WHERE wr."parentWorkspaceId" = ${workspaceId}
+        )
+    `;
+  const apiKeys =
+    await prisma().$queryRaw<{ id: string; configValue: string }[]>(
+      apiKeysQuery,
+    );
+
+  if (!actualKey) {
+    return false;
+  }
+  let matchingKey = false;
+  for (const apiKey of apiKeys) {
+    const definitionResult = jsonParseSafe(apiKey.configValue).andThen(
+      (configValue) => schemaValidate(configValue, AdminApiKeyDefinition),
+    );
+    if (definitionResult.isErr()) {
+      logger().error(
+        {
+          workspaceId,
+          apiKeyId: apiKey.id,
+        },
+        "Invalid admin API key definition",
+      );
+      continue;
+    }
+    if (definitionResult.value.key) {
+      if (definitionResult.value.key === actualKey) {
+        matchingKey = true;
+        break;
+      }
+    }
+  }
+  return matchingKey;
+}
 
 // eslint-disable-next-line @typescript-eslint/require-await
 const adminAuth = fp(async (fastify: FastifyInstance) => {
@@ -14,42 +71,15 @@ const adminAuth = fp(async (fastify: FastifyInstance) => {
     if (!workspaceId) {
       return reply.status(401).send();
     }
-    const apiKeys = await prisma().adminApiKey.findMany({
-      where: {
-        workspaceId,
-      },
-      include: {
-        secret: true,
-      },
-    });
     const actualKey = request.headers.authorization?.trim().split(" ")[1];
     if (!actualKey) {
       return reply.status(401).send();
     }
-    let matchingKey = false;
-    for (const apiKey of apiKeys) {
-      const definitionResult = schemaValidate(
-        apiKey.secret.configValue,
-        AdminApiKeyDefinition,
-      );
-      if (definitionResult.isErr()) {
-        logger().error(
-          {
-            workspaceId,
-            apiKeyId: apiKey.id,
-          },
-          "Invalid admin API key definition",
-        );
-        continue;
-      }
-      if (definitionResult.value.key) {
-        if (definitionResult.value.key === actualKey) {
-          matchingKey = true;
-          break;
-        }
-      }
-    }
-    if (!matchingKey) {
+    const authenticated = await authenticateAdminApiKey({
+      workspaceId,
+      actualKey,
+    });
+    if (!authenticated) {
       return reply.status(401).send();
     }
   });
