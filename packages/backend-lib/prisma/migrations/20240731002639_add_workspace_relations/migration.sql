73fc758d3864d897790bd755577034eecc57c648
Max Gurewitz <max@dittofeed.com>
2024-07-30 17:27:33 -0700

    partially passing

--- packages/backend-lib/prisma/migrations/20240731002639_add_workspace_relations/migration.sql
+++ packages/backend-lib/prisma/migrations/20240731002639_add_workspace_relations/migration.sql
@@ -6,15 +6,15 @@ ALTER TABLE "Workspace" ADD COLUMN     "type" "WorkspaceType" NOT NULL DEFAULT '
 
 -- CreateTable
 CREATE TABLE "WorkspaceRelation" (
-    "workspaceId" UUID NOT NULL,
+    "parentWorkspaceId" UUID NOT NULL,
     "childWorkspaceId" UUID NOT NULL
 );
 
 -- CreateIndex
-CREATE UNIQUE INDEX "WorkspaceRelation_workspaceId_childWorkspaceId_key" ON "WorkspaceRelation"("workspaceId", "childWorkspaceId");
+CREATE UNIQUE INDEX "WorkspaceRelation_parentWorkspaceId_childWorkspaceId_key" ON "WorkspaceRelation"("parentWorkspaceId", "childWorkspaceId");
 
 -- AddForeignKey
-ALTER TABLE "WorkspaceRelation" ADD CONSTRAINT "WorkspaceRelation_workspaceId_fkey" FOREIGN KEY ("workspaceId") REFERENCES "Workspace"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "WorkspaceRelation" ADD CONSTRAINT "WorkspaceRelation_parentWorkspaceId_fkey" FOREIGN KEY ("parentWorkspaceId") REFERENCES "Workspace"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "WorkspaceRelation" ADD CONSTRAINT "WorkspaceRelation_childWorkspaceId_fkey" FOREIGN KEY ("childWorkspaceId") REFERENCES "Workspace"("id") ON DELETE CASCADE ON UPDATE CASCADE;
