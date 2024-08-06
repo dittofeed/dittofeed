af5c70d348b6ede45892d6236c8e440eb5b5a184
Max Gurewitz <max@dittofeed.com>
2024-08-05 19:23:19 -0700

    wip

---
+++ packages/dashboard/src/pages/journeys/editor.page.tsx
@@ -0,0 +1,29 @@
+import { Box } from "@mui/material";
+import { useRouter } from "next/router";
+
+import JourneysBuilder from "../../components/journeys/journeysBuilder";
+import JourneyLayout from "../../components/journeys/layout";
+import {
+  JourneyGetServerSideProps,
+  journeyGetServerSideProps,
+} from "./getServerSideProps";
+
+export const getServerSideProps: JourneyGetServerSideProps = (ctx) =>
+  journeyGetServerSideProps(ctx);
+
+function Journey() {
+  const path = useRouter();
+  const id = typeof path.query.jId === "string" ? path.query.jId : undefined;
+  if (!id) {
+    return null;
+  }
+
+  return (
+    <Box sx={{ display: "flex", width: "100%", height: "100vh" }}>
+      <JourneyLayout journeyId={id}>
+        <JourneysBuilder journeyId={id} />
+      </JourneyLayout>
+    </Box>
+  );
+}
+export default Journey;
