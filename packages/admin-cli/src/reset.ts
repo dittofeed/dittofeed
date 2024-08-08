000d0fe6392f60d197ed46b05de5992ceac3d2c0
Max Gurewitz <max@dittofeed.com>
2024-08-08 14:48:11 -0700

    resetting workspace data

---
+++ packages/admin-cli/src/reset.ts
@@ -0,0 +1,74 @@
+import { ClickHouseQueryBuilder, command } from "backend-lib/src/clickhouse";
+import logger from "backend-lib/src/logger";
+import prisma from "backend-lib/src/prisma";
+
+export async function resetWorkspaceData({
+  workspaceId,
+}: {
+  workspaceId: string;
+}) {
+  logger().info(
+    {
+      workspaceId,
+    },
+    "Resetting workspace data for workspace",
+  );
+
+  const qb = new ClickHouseQueryBuilder();
+  const workspaceIdParam = qb.addQueryValue(workspaceId, "String");
+
+  await Promise.all([
+    command({
+      query: `ALTER TABLE user_events_v2 DELETE WHERE workspace_id = ${workspaceIdParam}`,
+      query_params: qb.getQueries(),
+    }),
+    command({
+      query: `ALTER TABLE computed_property_state_v2 DELETE WHERE workspace_id = ${workspaceIdParam}`,
+      query_params: qb.getQueries(),
+    }),
+    command({
+      query: `ALTER TABLE computed_property_assignments_v2 DELETE WHERE workspace_id = ${workspaceIdParam}`,
+      query_params: qb.getQueries(),
+    }),
+    command({
+      query: `ALTER TABLE processed_computed_properties_v2 DELETE WHERE workspace_id = ${workspaceIdParam}`,
+      query_params: qb.getQueries(),
+    }),
+    command({
+      query: `ALTER TABLE updated_computed_property_state DELETE WHERE workspace_id = ${workspaceIdParam}`,
+      query_params: qb.getQueries(),
+    }),
+    command({
+      query: `ALTER TABLE updated_property_assignments_v2 DELETE WHERE workspace_id = ${workspaceIdParam}`,
+      query_params: qb.getQueries(),
+    }),
+    command({
+      query: `ALTER TABLE computed_property_state_index DELETE WHERE workspace_id = ${workspaceIdParam}`,
+      query_params: qb.getQueries(),
+    }),
+    command({
+      query: `ALTER TABLE resolved_segment_state DELETE WHERE workspace_id = ${workspaceIdParam}`,
+      query_params: qb.getQueries(),
+    }),
+  ]);
+
+  await Promise.all([
+    prisma().segmentAssignment.deleteMany({
+      where: {
+        workspaceId,
+      },
+    }),
+    prisma().userPropertyAssignment.deleteMany({
+      where: {
+        workspaceId,
+      },
+    }),
+  ]);
+
+  logger().info(
+    {
+      workspaceId,
+    },
+    "Finished resetting workspace data for workspace",
+  );
+}
