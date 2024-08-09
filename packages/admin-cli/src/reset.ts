843c0a74e9a9b08b78f0895fa118cdfce3aff882
Max Gurewitz <max@dittofeed.com>
2024-08-08 17:07:09 -0700

    sync clear workspaces ch queries

--- packages/admin-cli/src/reset.ts
+++ packages/admin-cli/src/reset.ts
@@ -16,39 +16,45 @@ export async function resetWorkspaceData({
 
   const qb = new ClickHouseQueryBuilder();
   const workspaceIdParam = qb.addQueryValue(workspaceId, "String");
+  const baseChParams = {
+    query_params: qb.getQueries(),
+    settings: {
+      wait_end_of_query: 1,
+    },
+  };
 
   await Promise.all([
     command({
       query: `ALTER TABLE user_events_v2 DELETE WHERE workspace_id = ${workspaceIdParam}`,
-      query_params: qb.getQueries(),
+      ...baseChParams,
     }),
     command({
       query: `ALTER TABLE computed_property_state_v2 DELETE WHERE workspace_id = ${workspaceIdParam}`,
-      query_params: qb.getQueries(),
+      ...baseChParams,
     }),
     command({
       query: `ALTER TABLE computed_property_assignments_v2 DELETE WHERE workspace_id = ${workspaceIdParam}`,
-      query_params: qb.getQueries(),
+      ...baseChParams,
     }),
     command({
       query: `ALTER TABLE processed_computed_properties_v2 DELETE WHERE workspace_id = ${workspaceIdParam}`,
-      query_params: qb.getQueries(),
+      ...baseChParams,
     }),
     command({
       query: `ALTER TABLE updated_computed_property_state DELETE WHERE workspace_id = ${workspaceIdParam}`,
-      query_params: qb.getQueries(),
+      ...baseChParams,
     }),
     command({
       query: `ALTER TABLE updated_property_assignments_v2 DELETE WHERE workspace_id = ${workspaceIdParam}`,
-      query_params: qb.getQueries(),
+      ...baseChParams,
     }),
     command({
       query: `ALTER TABLE computed_property_state_index DELETE WHERE workspace_id = ${workspaceIdParam}`,
-      query_params: qb.getQueries(),
+      ...baseChParams,
     }),
     command({
       query: `ALTER TABLE resolved_segment_state DELETE WHERE workspace_id = ${workspaceIdParam}`,
-      query_params: qb.getQueries(),
+      ...baseChParams,
     }),
   ]);
 
