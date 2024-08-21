643979b062821fbd592dd55af766028ea84bf111
Max Gurewitz <max@dittofeed.com>
2024-08-20 17:17:22 -0700

    passing

--- packages/backend-lib/src/computedProperties/computePropertiesIncremental.ts
+++ packages/backend-lib/src/computedProperties/computePropertiesIncremental.ts
@@ -2682,24 +2682,14 @@ export async function computeAssignments({
               max(max_event_time) as max_state_event_time
             from resolved_segment_state
             where
-              (
-                workspace_id,
-                segment_id,
-              ) in (
-                select
-                  workspace_id,
-                  segment_id
-                from resolved_segment_state
-                where
-                  workspace_id = ${workspaceIdParam}
-                  and segment_id = ${segmentIdParam}
-                  and computed_at <= toDateTime64(${nowSeconds}, 3)
-                  ${lowerBoundClause}
-              )
+              workspace_id = ${workspaceIdParam}
+              and segment_id = ${segmentIdParam}
+              and computed_at <= toDateTime64(${nowSeconds}, 3)
               and state_id in ${qb.addQueryValue(
                 assignmentConfig.stateIds,
                 "Array(String)",
               )}
+              ${lowerBoundClause}
             group by
               workspace_id,
               segment_id,
