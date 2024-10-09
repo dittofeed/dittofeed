66090a8334f3badcf56f9f532b87dc7663c11bff
Max Gurewitz <max@dittofeed.com>
2024-10-08 17:17:37 -0700

    wip

--- packages/dashboard/src/pages/deliveries.page.tsx
+++ packages/dashboard/src/pages/deliveries.page.tsx
@@ -1,47 +1,19 @@
-import { toBroadcastResource } from "backend-lib/src/broadcasts";
-import { toJourneyResource } from "backend-lib/src/journeys";
-import { findMessageTemplates } from "backend-lib/src/messaging";
-import { CompletionStatus } from "isomorphic-lib/src/types";
 import { GetServerSideProps } from "next";
 
 import DashboardContent from "../components/dashboardContent";
 import { DeliveriesTable } from "../components/deliveriesTable";
 import { addInitialStateToProps } from "../lib/addInitialStateToProps";
-import prisma from "../lib/prisma";
 import { requestContext } from "../lib/requestContext";
-import { PreloadedState, PropsWithInitialState } from "../lib/types";
+import { PropsWithInitialState } from "../lib/types";
+import { getDeliveriesData } from "./deliveries/getServerSideProps";
 
 export const getServerSideProps: GetServerSideProps<PropsWithInitialState> =
   requestContext(async (ctx, dfContext) => {
-    const [messageTemplates, broadcasts, journeys] = await Promise.all([
-      findMessageTemplates({
-        workspaceId: dfContext.workspace.id,
-      }),
-      prisma().broadcast.findMany({
-        where: {
-          workspaceId: dfContext.workspace.id,
-        },
-      }),
-      prisma().journey.findMany({
-        where: {
-          workspaceId: dfContext.workspace.id,
-        },
-      }),
-    ]);
-    const serverInitialState: PreloadedState = {
-      messages: {
-        type: CompletionStatus.Successful,
-        value: messageTemplates,
-      },
-      broadcasts: broadcasts.map(toBroadcastResource),
-      journeys: {
-        type: CompletionStatus.Successful,
-        value: journeys.flatMap((j) => toJourneyResource(j).unwrapOr([])),
-      },
-    };
     return {
       props: addInitialStateToProps({
-        serverInitialState,
+        serverInitialState: await getDeliveriesData({
+          workspaceId: dfContext.workspace.id,
+        }),
         props: {},
         dfContext,
       }),
