44c02d4de8f46fd562c1b377992dff6e1328c16d
Max Gurewitz <max@dittofeed.com>
2024-10-16 11:32:58 -0400

    show journey status in table

--- packages/dashboard/src/components/journeysTable.tsx
+++ packages/dashboard/src/components/journeysTable.tsx
@@ -8,6 +8,7 @@ import React from "react";
 import apiRequestHandlerFactory from "../lib/apiRequestHandlerFactory";
 import { useAppStorePick } from "../lib/appStore";
 import { BaseResourceRow, ResourceTable } from "./resourceTable";
+import { Chip, ChipProps } from "@mui/material";
 
 export default function JourneysTable() {
   const {
@@ -48,6 +49,7 @@ export default function JourneysTable() {
       id: journey.id,
       name: journey.name,
       updatedAt: new Date(journey.updatedAt).toISOString(),
+      status: journey.status,
     };
     journeysRow.push(row);
   });
@@ -62,6 +64,29 @@ export default function JourneysTable() {
     <ResourceTable
       rows={journeysRow}
       getHref={(id) => `/journeys/${id}`}
+      additionalColumns={[
+        {
+          field: "status",
+          headerName: "Status",
+          renderCell: (params) => {
+            let color: ChipProps["color"];
+            switch (params.value) {
+              case "NotStarted":
+                color = "default";
+                break;
+              case "Running":
+                color = "success";
+                break;
+              case "Paused":
+                color = "warning";
+                break;
+              default:
+                return null;
+            }
+            return <Chip label={params.value} color={color} />;
+          },
+        },
+      ]}
       onDelete={({ row }) => {
         const currentRow = row;
         const handleDelete = apiRequestHandlerFactory({
