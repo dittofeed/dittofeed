602075245b25c2ca25b971062557620ef5438e26
Max Gurewitz <max@dittofeed.com>
2024-10-21 16:20:25 -0700

    fix ordering of emailo build in dev setup

--- dev-setup.sh
+++ dev-setup.sh
@@ -4,6 +4,6 @@ set -euo pipefail
 yarn
 yarn workspace backend-lib prisma generate
 yarn workspace backend-lib tsc --build
-yarn workspace dashboard tsc --build
 yarn workspace emailo build
+yarn workspace dashboard tsc --build
 yarn admin bootstrap
