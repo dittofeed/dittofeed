73bc21ef8798eedaac5014cb9357162e6992ae16
Max Gurewitz <max@dittofeed.com>
2024-11-06 20:43:12 -0800

    wip

--- packages/backend-lib/src/journeys/keyedEventEntry.test.ts
+++ packages/backend-lib/src/journeys/keyedEventEntry.test.ts
@@ -1,22 +1,53 @@
+import { TestWorkflowEnvironment } from "@temporalio/testing";
+import { Worker } from "@temporalio/worker";
 import { randomUUID } from "crypto";
 
+import { createEnvAndWorker } from "../../test/temporal";
+import prisma from "../prisma";
 import {
   ChannelType,
+  Journey,
   JourneyDefinition,
   JourneyNodeType,
   KeyedPerformedSegmentNode,
+  Segment,
   SegmentDefinition,
   SegmentNodeType,
   SegmentOperatorType,
+  Workspace,
 } from "../types";
+import { getUserJourneyWorkflowId, userJourneyWorkflow } from "./userWorkflow";
 
 describe("keyedEventEntry journeys", () => {
+  let workspace: Workspace;
+  let testEnv: TestWorkflowEnvironment;
+  let worker: Worker;
+
+  beforeEach(async () => {
+    workspace = await prisma().workspace.create({
+      data: {
+        name: `event-entry-${randomUUID()}`,
+      },
+    });
+
+    const testActivities = {
+      sendMessageV2: jest.fn().mockReturnValue(true),
+    };
+    const envAndWorker = await createEnvAndWorker({
+      activityOverrides: testActivities,
+    });
+    testEnv = envAndWorker.testEnv;
+    worker = envAndWorker.worker;
+  });
   describe("when a journey is keyed on appointmentId and waits for a cancellation event before sending a message", () => {
+    let journey: Journey;
+    let segment: Segment;
+
     beforeEach(async () => {
       const appointmentCancelledSegmentId = randomUUID();
       const templateId = randomUUID();
 
-      const definition: JourneyDefinition = {
+      const journeyDefinition: JourneyDefinition = {
         entryNode: {
           type: JourneyNodeType.EventEntryNode,
           event: "APPOINTMENT_UPDATE",
@@ -66,12 +97,48 @@ describe("keyedEventEntry journeys", () => {
             },
           ],
         } satisfies KeyedPerformedSegmentNode,
+        nodes: [],
       };
+      [segment, journey] = await Promise.all([
+        prisma().segment.create({
+          data: {
+            name: "appointment-cancelled",
+            definition: segmentDefinition,
+            workspaceId: workspace.id,
+          },
+        }),
+        prisma().journey.create({
+          data: {
+            name: "appointment-cancelled-journey",
+            definition: journeyDefinition,
+            workspaceId: workspace.id,
+          },
+        }),
+      ]);
       // create a journey with a wait-for node conditioned on a cancellation event
     });
     describe("when two journeys are triggered concurrently for the same user with different appointmentIds but only one is cancelled ", () => {
-      it("only the cancelled journey should send a message", () => {
-        expect(true).toBe(true);
+      let userId: string;
+
+      beforeEach(() => {
+        userId = randomUUID();
+      });
+
+      it("only the cancelled journey should send a message", async () => {
+        await worker.runUntil(async () => {
+          await testEnv.client.workflow.start(userJourneyWorkflow, {
+            workflowId: getUserJourneyWorkflowId({
+              userId,
+              journeyId: journey.id,
+            }),
+            taskQueue: "default",
+            args: [
+              // {
+              //   journeyId: journey.id,
+              // },
+            ],
+          });
+        });
       });
     });
   });
