17d8f089dadb8fdf1d455fad6de84a42be9c9389
Max Gurewitz <max@dittofeed.com>
2024-11-29 12:55:24 -0800

    wip

--- packages/backend-lib/src/apps.test.ts
+++ packages/backend-lib/src/apps.test.ts
@@ -1,8 +1,16 @@
+import NodeCache from "node-cache";
 import { v4 as uuidv4 } from "uuid";
 
+import { submitBatchWithTriggers } from "./apps";
 import { submitBatch } from "./apps/batch";
+import { triggerEventEntryJourneysFactory } from "./journeys";
 import prisma from "./prisma";
-import { EventType } from "./types";
+import {
+  EventType,
+  JourneyDefinition,
+  JourneyNodeType,
+  JourneyStatus,
+} from "./types";
 import { findManyEventsWithCount } from "./userEvents";
 
 describe("apps", () => {
@@ -43,6 +51,104 @@ describe("apps", () => {
           "{}",
           "{}",
         ]);
+      });
+    });
+  });
+  describe("submitBatchWithTriggers", () => {
+    let workspaceId: string;
+    let startKeyedJourneyImpl: jest.Mock;
+    let notStartedJourneyId: string;
+    let startedEventTriggeredJourneyId: string;
+    let segmentEntryJourneyId: string;
+
+    beforeEach(async () => {
+      workspaceId = uuidv4();
+      await prisma().workspace.create({
+        data: { id: workspaceId, name: `test-${workspaceId}` },
+      });
+      const eventTriggeredJourneyDefinition: JourneyDefinition = {
+        entryNode: {
+          type: JourneyNodeType.EventEntryNode,
+          event: "Purchase",
+          child: JourneyNodeType.ExitNode,
+        },
+        nodes: [],
+        exitNode: {
+          type: JourneyNodeType.ExitNode,
+        },
+      };
+      segmentEntryJourneyId = uuidv4();
+      notStartedJourneyId = uuidv4();
+      startedEventTriggeredJourneyId = uuidv4();
+
+      await Promise.all([
+        prisma().journey.create({
+          data: {
+            id: notStartedJourneyId,
+            name: "not started",
+            status: JourneyStatus.NotStarted,
+            workspaceId,
+            definition: eventTriggeredJourneyDefinition,
+          },
+        }),
+        prisma().journey.create({
+          data: {
+            id: startedEventTriggeredJourneyId,
+            name: "started event triggered",
+            status: JourneyStatus.Running,
+            workspaceId,
+            definition: eventTriggeredJourneyDefinition,
+          },
+        }),
+        prisma().journey.create({
+          data: {
+            id: segmentEntryJourneyId,
+            name: "segment entry",
+            status: JourneyStatus.Running,
+            workspaceId,
+            definition: eventTriggeredJourneyDefinition,
+          },
+        }),
+      ]);
+
+      startKeyedJourneyImpl = jest.fn();
+
+      // Custom implementation
+      jest.mock("./journeys", () => {
+        return triggerEventEntryJourneysFactory({
+          journeyCache: new NodeCache(),
+          startKeyedJourneyImpl,
+        });
+      });
+    });
+
+    afterEach(() => {
+      jest.resetModules();
+    });
+
+    it("should trigger journeys for each event", async () => {
+      const userId1 = uuidv4();
+      const userId2 = uuidv4();
+
+      await submitBatchWithTriggers({
+        workspaceId,
+        data: {
+          batch: [
+            {
+              type: EventType.Track,
+              event: "Purchase",
+              messageId: uuidv4(),
+              userId: userId1,
+              properties: { amount: 100 },
+            },
+            {
+              type: EventType.Identify,
+              messageId: uuidv4(),
+              userId: userId2,
+              traits: { name: "Test User" },
+            },
+          ],
+        },
       });
     });
   });
