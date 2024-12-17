a3aa3da95a2f26c2869348156be9a2f4b2e78a6c
Max Gurewitz <max@dittofeed.com>
2024-12-17 12:27:13 -0800

    cc and bcc ui (#1217)
    - add additional validations on backend

--- packages/dashboard/src/components/messages/emailEditor.tsx
+++ packages/dashboard/src/components/messages/emailEditor.tsx
@@ -151,106 +151,156 @@ function EmailOptions({ draft, setDraft, disabled }: RenderEditorParams) {
       >
         <DialogTitle> Email Options </DialogTitle>
         <DialogContent>
-          <Autocomplete
-            sx={{ p: 1 }}
-            filterSelectedOptions
-            value={draft.attachmentUserProperties ?? []}
-            renderTags={(value: readonly string[], getTagProps) =>
-              value.map((option: string, index: number) => {
-                const { key, ...tagProps } = getTagProps({ index });
-                const id =
-                  userProperties.type === CompletionStatus.Successful
-                    ? userProperties.value.find((up) => up.name === option)?.id
-                    : undefined;
-
-                if (!id) {
-                  return null;
-                }
-                return (
-                  <Chip
-                    clickable
-                    component={Link}
-                    href={`/user-properties/${id}`}
-                    label={option}
-                    key={key}
-                    {...tagProps}
-                  />
-                );
-              })
-            }
-            onChange={(_event, value) => {
-              setDraft((defn) => {
-                if (defn.type !== ChannelType.Email) {
-                  return defn;
-                }
-                return { ...defn, attachmentUserProperties: value };
-              });
-            }}
-            options={options}
-            disabled={disabled}
-            multiple
-            renderInput={(params) => (
-              <TextField {...params} label="Attachments" variant="outlined" />
-            )}
-          />
-
-          <Button
-            startIcon={<AddIcon />}
-            onClick={addCustomHeader}
-            disabled={disabled}
-            sx={{ mt: 2, mb: 1 }}
-          >
-            Add Custom Header
-          </Button>
-
-          <Stack
-            spacing={2}
-            sx={{
-              maxHeight: "200px",
-              overflowY: "auto",
-              width: "100%",
-              pt: 1,
-              pr: 1,
-            }}
-          >
-            {(draft.headers ?? []).map((header, index) => (
-              <Stack
-                // eslint-disable-next-line react/no-array-index-key
-                key={index}
-                direction="row"
-                spacing={2}
-                sx={{ width: "100%" }}
-                alignItems="center"
-              >
-                <TextField
-                  label="Header Name"
-                  value={header.name}
-                  sx={{ flex: 1 }}
-                  onChange={(e) =>
-                    updateCustomHeader(index, "name", e.target.value)
+          <Stack sx={{ pt: 1 }}>
+            <TextField
+              label="CC"
+              variant="outlined"
+              fullWidth
+              disabled={disabled}
+              onChange={(e) => {
+                setDraft((defn) => {
+                  if (defn.type !== ChannelType.Email) {
+                    return defn;
                   }
-                  disabled={disabled}
-                  size="small"
-                />
-                <TextField
-                  sx={{ flex: 1 }}
-                  label="Header Value"
-                  value={header.value}
-                  onChange={(e) =>
-                    updateCustomHeader(index, "value", e.target.value)
+                  return { ...defn, cc: e.target.value };
+                });
+              }}
+              value={draft.cc ?? ""}
+              sx={{ mb: 2 }}
+            />
+            <TextField
+              label="BCC"
+              variant="outlined"
+              fullWidth
+              disabled={disabled}
+              onChange={(e) => {
+                setDraft((defn) => {
+                  if (defn.type !== ChannelType.Email) {
+                    return defn;
                   }
-                  disabled={disabled}
-                  size="small"
-                />
-                <IconButton
-                  onClick={() => removeCustomHeader(index)}
-                  disabled={disabled}
-                  size="small"
+                  return { ...defn, bcc: e.target.value };
+                });
+              }}
+              value={draft.bcc ?? ""}
+              sx={{ mb: 2 }}
+            />
+            <TextField
+              label="Reply-To"
+              variant="outlined"
+              fullWidth
+              disabled={disabled}
+              onChange={(e) => {
+                setDraft((defn) => {
+                  if (defn.type !== ChannelType.Email) {
+                    return defn;
+                  }
+                  return { ...defn, replyTo: e.target.value };
+                });
+              }}
+              value={draft.replyTo ?? ""}
+              sx={{ mb: 2 }}
+            />
+            <Autocomplete
+              filterSelectedOptions
+              value={draft.attachmentUserProperties ?? []}
+              renderTags={(value: readonly string[], getTagProps) =>
+                value.map((option: string, index: number) => {
+                  const { key, ...tagProps } = getTagProps({ index });
+                  const id =
+                    userProperties.type === CompletionStatus.Successful
+                      ? userProperties.value.find((up) => up.name === option)
+                          ?.id
+                      : undefined;
+
+                  if (!id) {
+                    return null;
+                  }
+                  return (
+                    <Chip
+                      clickable
+                      component={Link}
+                      href={`/user-properties/${id}`}
+                      label={option}
+                      key={key}
+                      {...tagProps}
+                    />
+                  );
+                })
+              }
+              onChange={(_event, value) => {
+                setDraft((defn) => {
+                  if (defn.type !== ChannelType.Email) {
+                    return defn;
+                  }
+                  return { ...defn, attachmentUserProperties: value };
+                });
+              }}
+              options={options}
+              disabled={disabled}
+              multiple
+              renderInput={(params) => (
+                <TextField {...params} label="Attachments" variant="outlined" />
+              )}
+            />
+
+            <Button
+              startIcon={<AddIcon />}
+              onClick={addCustomHeader}
+              disabled={disabled}
+              sx={{ mt: 2, mb: 1 }}
+            >
+              Add Custom Header
+            </Button>
+
+            <Stack
+              spacing={2}
+              sx={{
+                maxHeight: "200px",
+                overflowY: "auto",
+                width: "100%",
+                pt: 1,
+                pr: 1,
+              }}
+            >
+              {(draft.headers ?? []).map((header, index) => (
+                <Stack
+                  // eslint-disable-next-line react/no-array-index-key
+                  key={index}
+                  direction="row"
+                  spacing={2}
+                  sx={{ width: "100%" }}
+                  alignItems="center"
                 >
-                  <DeleteIcon />
-                </IconButton>
-              </Stack>
-            ))}
+                  <TextField
+                    label="Header Name"
+                    value={header.name}
+                    sx={{ flex: 1 }}
+                    onChange={(e) =>
+                      updateCustomHeader(index, "name", e.target.value)
+                    }
+                    disabled={disabled}
+                    size="small"
+                  />
+                  <TextField
+                    sx={{ flex: 1 }}
+                    label="Header Value"
+                    value={header.value}
+                    onChange={(e) =>
+                      updateCustomHeader(index, "value", e.target.value)
+                    }
+                    disabled={disabled}
+                    size="small"
+                  />
+                  <IconButton
+                    onClick={() => removeCustomHeader(index)}
+                    disabled={disabled}
+                    size="small"
+                  >
+                    <DeleteIcon />
+                  </IconButton>
+                </Stack>
+              ))}
+            </Stack>
           </Stack>
         </DialogContent>
       </Dialog>
@@ -398,27 +448,6 @@ export default function EmailEditor({
               }}
               value={draft.subject}
             />
-            <TextField
-              label="Reply-To"
-              variant="filled"
-              disabled={disabled}
-              onChange={(e) => {
-                setDraft((defn) => {
-                  if (defn.type !== ChannelType.Email) {
-                    return defn;
-                  }
-                  defn.replyTo = e.target.value;
-                  return defn;
-                });
-              }}
-              InputProps={{
-                sx: {
-                  fontSize: ".75rem",
-                  borderTopRightRadius: 0,
-                },
-              }}
-              value={draft.replyTo ?? ""}
-            />
           </Stack>
         );
       }}
@@ -455,7 +484,6 @@ export default function EmailEditor({
           email={up.email}
           from={rendered.from}
           subject={rendered.subject}
-          replyTo={rendered.replyTo}
         />
       )}
       renderPreviewBody={({ rendered }) => (
