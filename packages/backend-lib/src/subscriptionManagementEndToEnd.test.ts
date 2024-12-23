afe82ce50b0940066f39f2d304d4d653967e4bc9
Max Gurewitz <max@dittofeed.com>
2024-12-23 11:43:20 -0800

    wip

---
+++ packages/backend-lib/src/subscriptionManagementEndToEnd.test.ts
@@ -0,0 +1,5 @@
+describe("subscriptionManagementEndToEnd", () => {
+  describe("when a user unsubscribes from an opt-out subscription group", () => {
+    it("should remove the user from the subscription group", async () => {});
+  });
+});
