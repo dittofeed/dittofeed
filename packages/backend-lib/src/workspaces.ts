db7ddc8ef5bdf12c16ef63afc3433d752708df22
Max Gurewitz <max@dittofeed.com>
2025-01-06 14:58:51 -0800

    wip

--- packages/backend-lib/src/workspaces.ts
+++ packages/backend-lib/src/workspaces.ts
@@ -1,11 +1,13 @@
-import { WorkspaceStatus } from "@prisma/client";
+import { and, eq } from "drizzle-orm";
 import { WORKSPACE_TOMBSTONE_PREFIX } from "isomorphic-lib/src/constants";
 import { err, ok, Result } from "neverthrow";
 import { validate as validateUuid } from "uuid";
 
 import { bootstrapComputeProperties } from "./bootstrap";
 import { terminateComputePropertiesWorkflow } from "./computedProperties/computePropertiesWorkflow/lifecycle";
-import prisma from "./prisma";
+import { db } from "./db";
+import { workspace as dbWorkspace } from "./db/schema";
+import { WorkspaceStatusDbEnum } from "./types";
 
 export enum TombstoneWorkspaceErrorType {
   WorkspaceNotFound = "WorkspaceNotFound",
@@ -22,25 +24,25 @@ export async function tombstoneWorkspace(
       type: TombstoneWorkspaceErrorType.WorkspaceNotFound,
     });
   }
-  const result = await prisma().$transaction(async (tx) => {
-    const workspace = await tx.workspace.findUnique({
-      where: { id: workspaceId },
+  const result = await db().transaction(async (tx) => {
+    const workspace = await tx.query.workspace.findFirst({
+      where: eq(dbWorkspace.id, workspaceId),
     });
     if (!workspace) {
       return err({
         type: TombstoneWorkspaceErrorType.WorkspaceNotFound,
       });
     }
-    if (workspace.status !== WorkspaceStatus.Active) {
+    if (workspace.status !== WorkspaceStatusDbEnum.Active) {
       return ok(undefined);
     }
-    await tx.workspace.update({
-      where: { id: workspaceId },
-      data: {
+    await tx
+      .update(dbWorkspace)
+      .set({
         name: `${WORKSPACE_TOMBSTONE_PREFIX}-${workspace.name}`,
-        status: "Tombstoned",
-      },
-    });
+        status: WorkspaceStatusDbEnum.Tombstoned,
+      })
+      .where(eq(dbWorkspace.id, workspaceId));
     return ok(undefined);
   });
   if (result.isErr()) {
@@ -60,29 +62,29 @@ export interface ActivateTombstonedWorkspaceError {
 export async function activateTombstonedWorkspace(
   workspaceId: string,
 ): Promise<Result<void, ActivateTombstonedWorkspaceError>> {
-  const result = await prisma().$transaction(async (tx) => {
-    const workspace = await tx.workspace.findUnique({
-      where: { id: workspaceId },
+  const result = await db().transaction(async (tx) => {
+    const workspace = await tx.query.workspace.findFirst({
+      where: eq(dbWorkspace.id, workspaceId),
     });
     if (!workspace) {
       return err({
         type: ActivateTombstonedWorkspaceErrorType.WorkspaceNotFound,
       });
     }
-    if (workspace.status !== WorkspaceStatus.Tombstoned) {
+    if (workspace.status !== WorkspaceStatusDbEnum.Tombstoned) {
       return ok(undefined);
     }
     const newName = workspace.name.replace(
       `${WORKSPACE_TOMBSTONE_PREFIX}-`,
       "",
     );
-    await tx.workspace.update({
-      where: { id: workspaceId },
-      data: {
-        status: WorkspaceStatus.Active,
+    await tx
+      .update(dbWorkspace)
+      .set({
+        status: WorkspaceStatusDbEnum.Active,
         name: newName,
-      },
-    });
+      })
+      .where(eq(dbWorkspace.id, workspaceId));
     return ok(undefined);
   });
   if (result.isErr()) {
