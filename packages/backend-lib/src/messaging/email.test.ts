ae842b7f4ed370d8cd55345aa82054f0990ca8ca
Max Gurewitz <max@dittofeed.com>
2025-01-14 23:07:44 -0800

    wip:

---
+++ packages/backend-lib/src/messaging/email.test.ts
@@ -0,0 +1,50 @@
+import { randomUUID } from "crypto";
+import { unwrap } from "isomorphic-lib/src/resultHandling/resultUtils";
+
+import { db } from "../db";
+import { EmailProviderType } from "../types";
+import { createWorkspace } from "../workspaces";
+import { upsertEmailProvider } from "./email";
+
+describe("email", () => {
+  describe("upsertEmailProvider", () => {
+    it("should be able to update an existing email provider", async () => {
+      const workspace = await createWorkspace({
+        name: randomUUID(),
+      }).then(unwrap);
+
+      await upsertEmailProvider({
+        workspaceId: workspace.id,
+        setDefault: true,
+        config: {
+          type: EmailProviderType.Sendgrid,
+          apiKey: "123",
+        },
+      });
+
+      await upsertEmailProvider({
+        workspaceId: workspace.id,
+        setDefault: true,
+        config: {
+          type: EmailProviderType.Sendgrid,
+          apiKey: "456",
+        },
+      });
+
+      const provider = await db().query.defaultEmailProvider.findFirst({
+        where: (table, { eq }) => eq(table.workspaceId, workspace.id),
+        with: {
+          emailProvider: {
+            with: {
+              secret: true,
+            },
+          },
+        },
+      });
+
+      expect(provider?.emailProvider.secret?.configValue).toEqual({
+        apiKey: "456",
+      });
+    });
+  });
+});
