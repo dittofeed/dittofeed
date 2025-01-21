b767621a678005ffc8a91d1efe404d0648ee2c94
Max Gurewitz <max@dittofeed.com>
2025-01-21 14:09:07 -0800

    wip

--- packages/backend-lib/src/eventEntry.test.ts
+++ packages/backend-lib/src/eventEntry.test.ts
@@ -148,6 +148,7 @@ describe("eventEntry journeys", () => {
           name: "test-journey",
           workspaceId: workspace.id,
           definition: journeyDefinition,
+          status: "Running",
         });
       });
       it("should call the inner send message", async () => {
