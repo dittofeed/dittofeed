ef4633bd8f7585ff39ae1752940cee67c3536f96
Max Gurewitz <max@dittofeed.com>
2025-01-20 23:47:20 -0800

    wip

--- packages/backend-lib/src/eventEntry.test.ts
+++ packages/backend-lib/src/eventEntry.test.ts
@@ -37,141 +37,141 @@ describe("eventEntry journeys", () => {
   let workspace: Workspace;
   let testEnv: TestWorkflowEnvironment;
   let worker: Worker;
-
-  const testActivities = {
-    sendMessageV2: jest.fn().mockReturnValue(true),
-  };
-
   beforeEach(async () => {
     workspace = await createWorkspace({
       name: `event-entry-${randomUUID()}`,
-      updatedAt: new Date(),
-      id: randomUUID(),
     }).then(unwrap);
-
-    const envAndWorker = await createEnvAndWorker({
-      activityOverrides: testActivities,
-    });
-    testEnv = envAndWorker.testEnv;
-    worker = envAndWorker.worker;
   });
 
-  afterEach(async () => {
-    await testEnv.teardown();
-  });
-
-  describe("when a user is pre-assigned to a segment", () => {
-    let userId: string;
-    let journey: Journey;
-    let journeyDefinition: JourneyDefinition;
+  describe("testing calls to sendMessageV2", () => {
+    const testActivities = {
+      sendMessageV2: jest.fn().mockReturnValue(true),
+    };
     beforeEach(async () => {
-      userId = "user1";
-      const segmentId = randomUUID();
-      await db()
-        .insert(dbSegment)
-        .values({
-          id: segmentId,
-          workspaceId: workspace.id,
-          name: "test-segment",
-          updatedAt: new Date(),
-          definition: {
-            entryNode: {
-              id: randomUUID(),
-              type: SegmentNodeType.Trait,
-              path: "key",
-              operator: {
-                type: SegmentOperatorType.Equals,
-                value: "val",
-              },
-            },
-            nodes: [],
-          } satisfies SegmentDefinition,
-        });
-      await insertSegmentAssignments([
-        {
-          segmentId,
-          userId,
-          workspaceId: workspace.id,
-          inSegment: true,
-        },
-      ]);
-      journeyDefinition = {
-        entryNode: {
-          type: JourneyNodeType.EventEntryNode,
-          event: "test",
-          child: "segment-split-node",
-        },
-        nodes: [
-          {
-            type: JourneyNodeType.SegmentSplitNode,
-            id: "segment-split-node",
-            variant: {
-              type: SegmentSplitVariantType.Boolean,
-              segment: segmentId,
-              trueChild: "message-node",
-              falseChild: JourneyNodeType.ExitNode,
-            },
-          } satisfies SegmentSplitNode,
-          {
-            type: JourneyNodeType.MessageNode,
-            id: "message-node",
-            child: JourneyNodeType.ExitNode,
-            variant: {
-              type: ChannelType.Email,
-              templateId: "test-template",
-            },
-          } satisfies MessageNode,
-        ],
-        exitNode: {
-          type: JourneyNodeType.ExitNode,
-        },
-      };
-      journey = await insert({
-        table: dbJourney,
-        values: {
-          id: randomUUID(),
-          workspaceId: workspace.id,
-          name: "test-journey",
-          definition: journeyDefinition,
-          createdAt: new Date(),
-          updatedAt: new Date(),
-        },
-      }).then(unwrap);
+      const envAndWorker = await createEnvAndWorker({
+        activityOverrides: testActivities,
+      });
+      testEnv = envAndWorker.testEnv;
+      worker = envAndWorker.worker;
     });
-    it("segment splits should respect the pre-assignment", async () => {
-      await worker.runUntil(async () => {
-        await testEnv.client.workflow.execute(userJourneyWorkflow, {
-          workflowId: "workflow1",
-          taskQueue: "default",
-          args: [
-            {
-              journeyId: journey.id,
-              workspaceId: workspace.id,
-              userId,
-              definition: journeyDefinition,
-              version: UserJourneyWorkflowVersion.V2,
-              event: {
-                event: "test",
-                properties: {
-                  key: "val",
+
+    afterEach(async () => {
+      await testEnv.teardown();
+    });
+
+    describe("when a user is pre-assigned to a segment", () => {
+      let userId: string;
+      let journey: Journey;
+      let journeyDefinition: JourneyDefinition;
+      beforeEach(async () => {
+        userId = "user1";
+        const segmentId = randomUUID();
+        await db()
+          .insert(dbSegment)
+          .values({
+            id: segmentId,
+            workspaceId: workspace.id,
+            name: "test-segment",
+            updatedAt: new Date(),
+            definition: {
+              entryNode: {
+                id: randomUUID(),
+                type: SegmentNodeType.Trait,
+                path: "key",
+                operator: {
+                  type: SegmentOperatorType.Equals,
+                  value: "val",
                 },
-                messageId: randomUUID(),
-                timestamp: new Date().toISOString(),
               },
-            },
+              nodes: [],
+            } satisfies SegmentDefinition,
+          });
+        await insertSegmentAssignments([
+          {
+            segmentId,
+            userId,
+            workspaceId: workspace.id,
+            inSegment: true,
+          },
+        ]);
+        journeyDefinition = {
+          entryNode: {
+            type: JourneyNodeType.EventEntryNode,
+            event: "test",
+            child: "segment-split-node",
+          },
+          nodes: [
+            {
+              type: JourneyNodeType.SegmentSplitNode,
+              id: "segment-split-node",
+              variant: {
+                type: SegmentSplitVariantType.Boolean,
+                segment: segmentId,
+                trueChild: "message-node",
+                falseChild: JourneyNodeType.ExitNode,
+              },
+            } satisfies SegmentSplitNode,
+            {
+              type: JourneyNodeType.MessageNode,
+              id: "message-node",
+              child: JourneyNodeType.ExitNode,
+              variant: {
+                type: ChannelType.Email,
+                templateId: "test-template",
+              },
+            } satisfies MessageNode,
           ],
+          exitNode: {
+            type: JourneyNodeType.ExitNode,
+          },
+        };
+        journey = await insert({
+          table: dbJourney,
+          values: {
+            id: randomUUID(),
+            workspaceId: workspace.id,
+            name: "test-journey",
+            definition: journeyDefinition,
+            createdAt: new Date(),
+            updatedAt: new Date(),
+          },
+        }).then(unwrap);
+      });
+      it("segment splits should respect the pre-assignment", async () => {
+        await worker.runUntil(async () => {
+          await testEnv.client.workflow.execute(userJourneyWorkflow, {
+            workflowId: "workflow1",
+            taskQueue: "default",
+            args: [
+              {
+                journeyId: journey.id,
+                workspaceId: workspace.id,
+                userId,
+                definition: journeyDefinition,
+                version: UserJourneyWorkflowVersion.V2,
+                event: {
+                  event: "test",
+                  properties: {
+                    key: "val",
+                  },
+                  messageId: randomUUID(),
+                  timestamp: new Date().toISOString(),
+                },
+              },
+            ],
+          });
+          expect(testActivities.sendMessageV2).toHaveBeenCalledTimes(1);
+          const events = await db()
+            .select()
+            .from(dbUserJourneyEvent)
+            .where(
+              and(
+                eq(dbUserJourneyEvent.journeyId, journey.id),
+                eq(dbUserJourneyEvent.userId, userId),
+              ),
+            );
+          expect(events).toHaveLength(4);
         });
-        expect(testActivities.sendMessageV2).toHaveBeenCalledTimes(1);
-        const events = await db()
-          .select()
-          .from(dbUserJourneyEvent)
-          .where(
-            and(
-              eq(dbUserJourneyEvent.journeyId, journey.id),
-              eq(dbUserJourneyEvent.userId, userId),
-            ),
-          );
-        expect(events).toHaveLength(4);
       });
     });
   });
