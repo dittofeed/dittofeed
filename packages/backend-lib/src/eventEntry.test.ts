35ae0806954cabcb1c70899691d7ea0a256b91ee
Max Gurewitz <max@dittofeed.com>
2025-01-21 11:15:55 -0800

    wip

--- packages/backend-lib/src/eventEntry.test.ts
+++ packages/backend-lib/src/eventEntry.test.ts
@@ -3,21 +3,27 @@ import { Worker } from "@temporalio/worker";
 import { randomUUID } from "crypto";
 import { and, eq } from "drizzle-orm";
 import { unwrap } from "isomorphic-lib/src/resultHandling/resultUtils";
+import { ok } from "neverthrow";
 
 import { createEnvAndWorker } from "../test/temporal";
 import { db, insert } from "./db";
 import {
   journey as dbJourney,
+  messageTemplate as dbMessageTemplate,
   segment as dbSegment,
   userJourneyEvent as dbUserJourneyEvent,
+  userProperty as dbUserProperty,
 } from "./db/schema";
 import {
   userJourneyWorkflow,
   UserJourneyWorkflowVersion,
 } from "./journeys/userWorkflow";
+import { sendMessageFactory } from "./journeys/userWorkflow/activities";
 import { insertSegmentAssignments } from "./segments";
 import {
   ChannelType,
+  EmailProviderType,
+  InternalEventType,
   Journey,
   JourneyDefinition,
   JourneyNodeType,
@@ -27,6 +33,8 @@ import {
   SegmentOperatorType,
   SegmentSplitNode,
   SegmentSplitVariantType,
+  UserPropertyDefinition,
+  UserPropertyDefinitionType,
   Workspace,
 } from "./types";
 import { createWorkspace } from "./workspaces";
@@ -37,12 +45,147 @@ describe("eventEntry journeys", () => {
   let workspace: Workspace;
   let testEnv: TestWorkflowEnvironment;
   let worker: Worker;
+
   beforeEach(async () => {
     workspace = await createWorkspace({
       name: `event-entry-${randomUUID()}`,
     }).then(unwrap);
   });
 
+  afterEach(async () => {
+    await testEnv.teardown();
+  });
+
+  describe.only("testing calls to the inner send message", () => {
+    const senderMock = jest.fn().mockReturnValue(
+      ok({
+        type: InternalEventType.MessageSent,
+        variant: {
+          type: ChannelType.Email,
+          from: "test@test.com",
+          body: "test",
+          to: "test@test.com",
+          subject: "test",
+          headers: {},
+          replyTo: "test@test.com",
+          provider: {
+            type: EmailProviderType.Test,
+          },
+        },
+      }),
+    );
+
+    const testActivities = {
+      sendMessageV2: sendMessageFactory(senderMock),
+    };
+    beforeEach(async () => {
+      const envAndWorker = await createEnvAndWorker({
+        activityOverrides: testActivities,
+      });
+      testEnv = envAndWorker.testEnv;
+      worker = envAndWorker.worker;
+    });
+    describe("when messaging a user with an anyof performed user property", () => {
+      let journeyId: string;
+      let journeyDefinition: JourneyDefinition;
+      beforeEach(async () => {
+        await db()
+          .insert(dbUserProperty)
+          .values([
+            {
+              workspaceId: workspace.id,
+              name: "carrier",
+              updatedAt: new Date(),
+              definition: {
+                type: UserPropertyDefinitionType.Group,
+                entry: "0",
+                nodes: [
+                  {
+                    id: "0",
+                    type: UserPropertyDefinitionType.AnyOf,
+                    children: ["1", "2", "3", "4"],
+                  },
+                  {
+                    id: "1",
+                    type: UserPropertyDefinitionType.Performed,
+                    event: "tracking_update",
+                    path: "data.carrier",
+                  },
+                ],
+              } satisfies UserPropertyDefinition,
+            },
+          ]);
+        const templateId = randomUUID();
+        await db().insert(dbMessageTemplate).values({
+          id: templateId,
+          workspaceId: workspace.id,
+          name: "test-template",
+        });
+        journeyId = randomUUID();
+        journeyDefinition = {
+          entryNode: {
+            type: JourneyNodeType.EventEntryNode,
+            event: "tracking_update",
+            child: "message-node",
+          },
+          exitNode: {
+            type: JourneyNodeType.ExitNode,
+          },
+          nodes: [
+            {
+              type: JourneyNodeType.MessageNode,
+              id: "message-node",
+              child: JourneyNodeType.ExitNode,
+              variant: {
+                type: ChannelType.Email,
+                templateId,
+              },
+            },
+          ],
+        };
+        await db().insert(dbJourney).values({
+          id: journeyId,
+          name: "test-journey",
+          workspaceId: workspace.id,
+          definition: journeyDefinition,
+        });
+      });
+      it("should call the inner send message", async () => {
+        await worker.runUntil(async () => {
+          const userId = randomUUID();
+          await testEnv.client.workflow.execute(userJourneyWorkflow, {
+            workflowId: "workflow1",
+            taskQueue: "default",
+            args: [
+              {
+                journeyId,
+                workspaceId: workspace.id,
+                userId,
+                definition: journeyDefinition,
+                version: UserJourneyWorkflowVersion.V2,
+                event: {
+                  event: "tracking_update",
+                  messageId: randomUUID(),
+                  timestamp: new Date().toISOString(),
+                  properties: {
+                    carrier: "UPS",
+                  },
+                },
+              },
+            ],
+          });
+          expect(senderMock).toHaveBeenCalledTimes(1);
+          expect(senderMock).toHaveBeenCalledWith(
+            expect.objectContaining({
+              userPropertyAssignments: expect.objectContaining({
+                carrier: "UPS",
+              }),
+            }),
+          );
+        });
+      });
+    });
+  });
   describe("testing calls to sendMessageV2", () => {
     const testActivities = {
       sendMessageV2: jest.fn().mockReturnValue(true),
@@ -53,10 +196,6 @@ describe("eventEntry journeys", () => {
       });
       testEnv = envAndWorker.testEnv;
       worker = envAndWorker.worker;
-    });
-
-    afterEach(async () => {
-      await testEnv.teardown();
     });
 
     describe("when a user is pre-assigned to a segment", () => {
@@ -128,12 +267,9 @@ describe("eventEntry journeys", () => {
         journey = await insert({
           table: dbJourney,
           values: {
-            id: randomUUID(),
             workspaceId: workspace.id,
             name: "test-journey",
             definition: journeyDefinition,
-            createdAt: new Date(),
-            updatedAt: new Date(),
           },
         }).then(unwrap);
       });
