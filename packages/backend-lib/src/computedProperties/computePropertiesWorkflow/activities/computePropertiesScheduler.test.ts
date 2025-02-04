98da4edffcdfc88207eb51817ea957cce12c23b8
Max Gurewitz <max@dittofeed.com>
2025-02-03 21:33:14 -0800

    wip

--- packages/backend-lib/src/computedProperties/computePropertiesWorkflow/activities/computePropertiesScheduler.test.ts
+++ packages/backend-lib/src/computedProperties/computePropertiesWorkflow/activities/computePropertiesScheduler.test.ts
@@ -1,8 +1,10 @@
 import { randomUUID } from "crypto";
 import { unwrap } from "isomorphic-lib/src/resultHandling/resultUtils";
+import { Result } from "neverthrow";
 
-import { db } from "../../../db";
+import { db, insert } from "../../../db";
 import * as schema from "../../../db/schema";
+import { toSegmentResource } from "../../../segments";
 import {
   ComputedPropertyStep,
   FeatureNamesEnum,
@@ -61,27 +63,51 @@ describe("computePropertiesScheduler activities", () => {
         notDueSegmentId = randomUUID();
 
         const segments1: SavedSegmentResource[] = [
-          {
-            id: dueSegmentId,
-            name: randomUUID(),
-            createdAt: now - 1000,
-            updatedAt: now - 1000,
-            workspaceId: workspace.id,
-            definitionUpdatedAt: now - 1000,
-            definition: createSegmentDefinition(),
-          },
+          unwrap(
+            (
+              await insert({
+                table: schema.segment,
+                values: {
+                  id: dueSegmentId,
+                  name: randomUUID(),
+                  createdAt: new Date(now - 1000),
+                  updatedAt: new Date(now - 1000),
+                  workspaceId: workspace.id,
+                  definitionUpdatedAt: new Date(now - 1000),
+                  definition: createSegmentDefinition(),
+                },
+              })
+            ).andThen((s) => {
+              if (s === null) {
+                throw new Error("Segment not found");
+              }
+              return toSegmentResource(s);
+            }),
+          ),
         ];
 
         const segments2: SavedSegmentResource[] = [
-          {
-            id: notDueSegmentId,
-            name: randomUUID(),
-            createdAt: now - 1000,
-            updatedAt: now - 1000,
-            workspaceId: workspace.id,
-            definitionUpdatedAt: now - 1000,
-            definition: createSegmentDefinition(),
-          },
+          unwrap(
+            (
+              await insert({
+                table: schema.segment,
+                values: {
+                  id: notDueSegmentId,
+                  name: randomUUID(),
+                  createdAt: new Date(now - 1000),
+                  updatedAt: new Date(now - 1000),
+                  workspaceId: workspace.id,
+                  definitionUpdatedAt: new Date(now - 1000),
+                  definition: createSegmentDefinition(),
+                },
+              })
+            ).andThen((s) => {
+              if (s === null) {
+                throw new Error("Segment not found");
+              }
+              return toSegmentResource(s);
+            }),
+          ),
         ];
 
         await Promise.all([
@@ -137,6 +163,14 @@ describe("computePropertiesScheduler activities", () => {
           name: FeatureNamesEnum.ComputePropertiesGlobal,
           enabled: true,
         });
+        await insert({
+          table: schema.segment,
+          values: {
+            name: randomUUID(),
+            workspaceId: workspace.id,
+            definition: createSegmentDefinition(),
+          },
+        }).then(unwrap);
       });
       it("should return the workspace", async () => {
         const dueWorkspaces = await findDueWorkspaces({
