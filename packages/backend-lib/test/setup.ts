7061a9487fbf1be993e01a058c14b556bece889f
Max Gurewitz <max@dittofeed.com>
2025-02-13 00:57:13 -0800

    wip

--- packages/backend-lib/test/setup.ts
+++ packages/backend-lib/test/setup.ts
@@ -1,8 +1,5 @@
 import { endPool } from "../src/db";
 
 afterAll(async () => {
-  console.log("tearing down after all");
   await endPool();
-  console.log("setupFiles afterAll PID:", process.pid);
-  console.log("teardown complete after all");
 });
