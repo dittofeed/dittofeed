7c365a03aa89f5f033f78b2a0b46bee5b57d208f
Max Gurewitz <max@dittofeed.com>
2025-02-27 21:17:58 -0800

    wip

--- packages/backend-lib/src/groups.test.ts
+++ packages/backend-lib/src/groups.test.ts
@@ -1,13 +1,29 @@
+import { randomUUID } from "crypto";
+import { unwrap } from "isomorphic-lib/src/resultHandling/resultUtils";
+
 import { getUsersForGroup } from "./groups";
+import { createWorkspace } from "./workspaces";
 
 describe("groups", () => {
+  let workspaceId: string;
+  beforeEach(async () => {
+    const workspace = unwrap(
+      await createWorkspace({
+        name: randomUUID(),
+      }),
+    );
+    workspaceId = workspace.id;
+  });
+
   describe("getUsersForGroup", () => {
-    it("should get users for group", async () => {
-      const users = await getUsersForGroup({
-        workspaceId: "1",
-        groupId: "1",
+    describe("when user is assigned to group and then removed from group", () => {
+      it("the return value should reflect the addition and removal", async () => {
+        const users = await getUsersForGroup({
+          workspaceId: "1",
+          groupId: "1",
+        });
+        expect(users).toEqual([]);
       });
-      expect(users).toEqual([]);
     });
   });
 });
