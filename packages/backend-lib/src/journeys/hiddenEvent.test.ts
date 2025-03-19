9a7efff85cbb01f3a08650f8029e699fabe010c5
Max Gurewitz <max@dittofeed.com>
2025-03-18 17:59:40 -0700

    wip

---
+++ packages/backend-lib/src/journeys/hiddenEvent.test.ts
@@ -0,0 +1,149 @@
+import { TestWorkflowEnvironment } from "@temporalio/testing";
+import { Worker } from "@temporalio/worker";
+import { randomUUID } from "crypto";
+import { unwrap } from "isomorphic-lib/src/resultHandling/resultUtils";
+import { ok } from "neverthrow";
+
+import { createEnvAndWorker } from "../../test/temporal";
+import { insert } from "../db";
+import { journey as dbJourney } from "../db/schema";
+import {
+  ChannelType,
+  EmailProviderType,
+  InternalEventType,
+  Journey,
+  JourneyDefinition,
+  JourneyNodeType,
+  Workspace,
+} from "../types";
+import { createWorkspace } from "../workspaces";
+import {
+  userJourneyWorkflow,
+  UserJourneyWorkflowVersion,
+} from "./userWorkflow";
+import { sendMessageFactory } from "./userWorkflow/activities";
+
+jest.setTimeout(15000);
+
+describe("eventEntry journeys with hidden triggering events", () => {
+  let workspace: Workspace;
+  let testEnv: TestWorkflowEnvironment;
+  let worker: Worker;
+  const senderMock = jest.fn().mockReturnValue(
+    ok({
+      type: InternalEventType.MessageSent,
+      variant: {
+        type: ChannelType.Email,
+        from: "test@test.com",
+        body: "test",
+        to: "test@test.com",
+        subject: "test",
+        headers: {},
+        replyTo: "test@test.com",
+        provider: {
+          type: EmailProviderType.Smtp,
+          messageId: "test",
+        },
+      },
+    }),
+  );
+
+  const testActivities = {
+    sendMessageV2: sendMessageFactory(senderMock),
+  };
+
+  beforeEach(async () => {
+    workspace = unwrap(
+      await createWorkspace({
+        id: randomUUID(),
+        name: randomUUID(),
+        updatedAt: new Date(),
+      }),
+    );
+
+    const envAndWorker = await createEnvAndWorker({
+      activityOverrides: testActivities,
+    });
+    testEnv = envAndWorker.testEnv;
+    worker = envAndWorker.worker;
+  });
+
+  afterEach(async () => {
+    await testEnv.teardown();
+  });
+
+  describe("when a journey is keyed on appointmentId and waits for a cancellation event before sending a message", () => {
+    let journey: Journey;
+    let journeyDefinition: JourneyDefinition;
+    let dateUserPropertyId: string;
+    const oneDaySeconds = 60 * 60 * 24;
+
+    beforeEach(async () => {
+      const templateId = randomUUID();
+      dateUserPropertyId = randomUUID();
+
+      journeyDefinition = {
+        entryNode: {
+          type: JourneyNodeType.EventEntryNode,
+          event: "APPOINTMENT_UPDATE",
+          child: "message",
+        },
+        exitNode: {
+          type: JourneyNodeType.ExitNode,
+        },
+        nodes: [
+          {
+            type: JourneyNodeType.MessageNode,
+            id: "message",
+            variant: {
+              type: ChannelType.Email,
+              templateId,
+            },
+            child: JourneyNodeType.ExitNode,
+          },
+        ],
+      };
+      await insert({
+        table: dbJourney,
+        values: {
+          id: randomUUID(),
+          name: "appointment-cancelled-journey",
+          definition: journeyDefinition,
+          workspaceId: workspace.id,
+          status: "Running",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      }).then(unwrap);
+    });
+
+    it("should hide the message sent event", async () => {
+      const userId = randomUUID();
+      await worker.runUntil(async () => {
+        await testEnv.client.workflow.execute(userJourneyWorkflow, {
+          workflowId: "workflow1",
+          taskQueue: "default",
+          args: [
+            {
+              journeyId: journey.id,
+              workspaceId: workspace.id,
+              userId,
+              definition: journeyDefinition,
+              version: UserJourneyWorkflowVersion.V2,
+              event: {
+                event: "APPOINTMENT_UPDATE",
+                messageId: randomUUID(),
+                context: {
+                  hidden: true,
+                },
+                timestamp: new Date().toISOString(),
+              },
+            },
+          ],
+        });
+      });
+
+      expect(senderMock).toHaveBeenCalledTimes(1);
+    });
+  });
+});
