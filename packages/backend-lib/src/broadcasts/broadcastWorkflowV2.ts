4c0f058ffe4d90bdfad3613732227a72c204a65c
Max Gurewitz <max@dittofeed.com>
2025-03-28 17:00:07 -0700

    wip

--- packages/backend-lib/src/broadcasts/broadcastWorkflowV2.ts
+++ packages/backend-lib/src/broadcasts/broadcastWorkflowV2.ts
@@ -53,6 +53,46 @@ export async function broadcastWorkflowV2({
     return;
   }
 
+  const { rateLimit } = broadcast.config;
+
+  if (rateLimit !== undefined && rateLimit <= 0) {
+    logger.error("rate limit is 0, invalid config", {
+      broadcastId,
+      rateLimit,
+      workspaceId,
+    });
+    return;
+  }
+
+  const sendRateLimitedMessages = async function sendRateLimitedMessages({
+    timezones,
+    batchSize,
+    usersPerSecond,
+  }: {
+    timezones: string[];
+    batchSize: number;
+    usersPerSecond: number;
+  }) {
+    let cursor: string | null = null;
+    do {
+      const startTime = Date.now();
+      const { nextCursor } = await sendMessages({
+        workspaceId,
+        timezones,
+        limit: batchSize,
+        cursor: cursor ?? undefined,
+      });
+      const endTime = Date.now();
+
+      // FIXME
+      const sleepTime = 0;
+      if (sleepTime > 0) {
+        await sleep(sleepTime);
+      }
+      cursor = nextCursor ?? null;
+    } while (cursor != null);
+  };
+
   const { scheduledAt, config } = broadcast;
   if (scheduledAt) {
     if (config.useIndividualTimezone) {
