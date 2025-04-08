07f1f769608fb40a5c83af59f6507d4240939e0b
Max Gurewitz <max@dittofeed.com>
2025-04-08 15:15:34 -0700

    wip

--- packages/dashboard/src/pages/broadcasts/v2/preview.tsx
+++ packages/dashboard/src/pages/broadcasts/v2/preview.tsx
@@ -1,6 +1,10 @@
 import Typography from "@mui/material/Typography";
+import { db } from "backend-lib/src/db";
+import * as schema from "backend-lib/src/db/schema";
+import { and, eq } from "drizzle-orm";
 import { GetServerSideProps } from "next";
 import React from "react";
+import { validate as validateUuid } from "uuid";
 
 import DashboardContent from "../../../components/dashboardContent";
 import { addInitialStateToProps } from "../../../lib/addInitialStateToProps";
@@ -9,7 +13,29 @@ import { PropsWithInitialState } from "../../../lib/types";
 import BroadcastLayout from "./broadcastLayout";
 
 export const getServerSideProps: GetServerSideProps<PropsWithInitialState> =
-  requestContext(async (_ctx, dfContext) => {
+  requestContext(async (ctx, dfContext) => {
+    const broadcastId = ctx.params?.broadcastId;
+    if (typeof broadcastId !== "string") {
+      return {
+        notFound: true,
+      };
+    }
+    if (!validateUuid(broadcastId)) {
+      return {
+        notFound: true,
+      };
+    }
+    const broadcast = await db().query.broadcast.findFirst({
+      where: and(
+        eq(schema.broadcast.id, broadcastId),
+        eq(schema.workspace.id, dfContext.workspace.id),
+      ),
+    });
+    if (!broadcast) {
+      return {
+        notFound: true,
+      };
+    }
     return {
       props: addInitialStateToProps({
         dfContext,
