50dad5808e5b208673abd44c1f18bd2bfa661714
Max Gurewitz <max@dittofeed.com>
2025-04-08 15:01:21 -0700

    wip

--- packages/dashboard/src/pages/broadcasts/v2/content.tsx
+++ packages/dashboard/src/pages/broadcasts/v2/content.tsx
@@ -1,11 +1,25 @@
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
+import { GetServerSideProps } from "next";
 import Link from "next/link";
 import React from "react";
 
 import DashboardContent from "../../../components/dashboardContent";
+import { addInitialStateToProps } from "../../../lib/addInitialStateToProps";
+import { requestContext } from "../../../lib/requestContext";
+import { PropsWithInitialState } from "../../../lib/types";
 import BroadcastLayout from "./broadcastLayout";
+
+export const getServerSideProps: GetServerSideProps<PropsWithInitialState> =
+  requestContext(async (_ctx, dfContext) => {
+    return {
+      props: addInitialStateToProps({
+        dfContext,
+        props: {},
+      }),
+    };
+  });
 
 export default function BroadcastContentPage() {
   return (
--- packages/dashboard/src/pages/broadcasts/v2/preview.tsx
+++ packages/dashboard/src/pages/broadcasts/v2/preview.tsx
@@ -1,8 +1,22 @@
 import Typography from "@mui/material/Typography";
+import { GetServerSideProps } from "next";
 import React from "react";
 
 import DashboardContent from "../../../components/dashboardContent";
+import { addInitialStateToProps } from "../../../lib/addInitialStateToProps";
+import { requestContext } from "../../../lib/requestContext";
+import { PropsWithInitialState } from "../../../lib/types";
 import BroadcastLayout from "./broadcastLayout";
+
+export const getServerSideProps: GetServerSideProps<PropsWithInitialState> =
+  requestContext(async (_ctx, dfContext) => {
+    return {
+      props: addInitialStateToProps({
+        dfContext,
+        props: {},
+      }),
+    };
+  });
 
 export default function BroadcastPreviewPage() {
   return (
