204f25423b92556b14d34520f7751dbda40b4777
Max Gurewitz <max@dittofeed.com>
2025-04-09 11:38:26 -0700

    send app version with health check endpoint (#1401)

--- packages/api/src/controllers/indexController.ts
+++ packages/api/src/controllers/indexController.ts
@@ -1,3 +1,4 @@
+import config from "backend-lib/src/config";
 import { FastifyInstance, FastifyReply, FastifyRequest } from "fastify";
 
 // eslint-disable-next-line @typescript-eslint/require-await
@@ -9,7 +10,12 @@ export default async function indexController(fastify: FastifyInstance) {
         description: "Application health check endpoint.",
       },
     },
-    async (_request: FastifyRequest, reply: FastifyReply) =>
-      reply.status(200).send(),
+    async (_request: FastifyRequest, reply: FastifyReply) => {
+      const { appVersion } = config();
+
+      return reply.status(200).send({
+        version: appVersion,
+      });
+    },
   );
 }
