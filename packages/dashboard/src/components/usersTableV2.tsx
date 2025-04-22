dc4dc69756a60c2764477593f0dedc939fac7989
Max Gurewitz <max@dittofeed.com>
2025-04-22 15:35:07 -0700

    wip

--- packages/dashboard/src/components/usersTableV2.tsx
+++ packages/dashboard/src/components/usersTableV2.tsx
@@ -1095,6 +1095,7 @@ export default function UsersTableV2({
         alignItems: "stretch",
       }}
     >
+      {controls}
       <TableContainer component={Paper}>
         <Table stickyHeader>
           <TableHead>
