39ad3e58d03606f249d8ea602363bc1b0894f5d2
Max Gurewitz <max@dittofeed.com>
2025-04-29 16:28:35 -0700

    wip

---
+++ packages/dashboard/src/lib/useRecomputeBroadcastSegmentMutation.ts
@@ -0,0 +1,54 @@
+import { useMutation, UseMutationOptions } from "@tanstack/react-query";
+import axios from "axios";
+import { schemaValidate } from "isomorphic-lib/src/resultHandling/schemaValidation";
+import {
+  CompletionStatus,
+  EmptyResponse,
+  RecomputeBroadcastSegmentRequest,
+} from "isomorphic-lib/src/types";
+
+import { useAppStorePick } from "./appStore";
+
+export const RECOMPUTE_BROADCAST_SEGMENT_MUTATION_KEY = [
+  "recomputeBroadcastSegment",
+];
+
+export function useRecomputeBroadcastSegmentMutation(
+  options?: UseMutationOptions<
+    EmptyResponse,
+    Error,
+    Omit<RecomputeBroadcastSegmentRequest, "workspaceId">
+  >,
+) {
+  const { apiBase, workspace } = useAppStorePick(["apiBase", "workspace"]);
+
+  const mutationFn = async (
+    params: Omit<RecomputeBroadcastSegmentRequest, "workspaceId">,
+  ): Promise<EmptyResponse> => {
+    if (workspace.type !== CompletionStatus.Successful) {
+      throw new Error("Workspace not available");
+    }
+
+    const { id: workspaceId } = workspace.value;
+    const response = await axios.put(
+      `${apiBase}/api/broadcasts/recompute-segment`,
+      {
+        ...params,
+        workspaceId,
+      },
+    );
+
+    schemaValidate(response.data, EmptyResponse);
+    return response.data;
+  };
+
+  return useMutation<
+    EmptyResponse,
+    Error,
+    Omit<RecomputeBroadcastSegmentRequest, "workspaceId">
+  >({
+    mutationFn,
+    mutationKey: RECOMPUTE_BROADCAST_SEGMENT_MUTATION_KEY,
+    ...options,
+  });
+}
