e369443110ae8cc9c951267c9f9f19fe1aa46cc9
Max Gurewitz <max@dittofeed.com>
2025-05-12 15:09:18 -0700

    wip

--- packages/backend-lib/src/computedProperties/computePropertiesQueueWorkflow.test.ts
+++ packages/backend-lib/src/computedProperties/computePropertiesQueueWorkflow.test.ts
@@ -19,5 +19,73 @@ describe("computePropertiesQueueWorkflow", () => {
         expect(items[1]).toBe(item1);
       });
     });
+
+    describe("when you have two items with different maxPeriods", () => {
+      it("should return the item with the earlier maxPeriod first", () => {
+        const item1 = {
+          id: randomUUID(),
+          maxPeriod: 2000, // Later
+        };
+        const item2 = {
+          id: randomUUID(),
+          maxPeriod: 1000, // Earlier
+        };
+        const items = [item1, item2];
+        items.sort(compareWorkspaceItems);
+        expect(items[0]).toBe(item2);
+        expect(items[1]).toBe(item1);
+      });
+    });
+
+    describe("when you have two items with same priority and maxPeriod (or undefined) but different insertedAt times", () => {
+      it("should return the item with the earlier insertedAt first", () => {
+        const item1 = {
+          id: randomUUID(),
+          insertedAt: 200, // Later
+        };
+        const item2 = {
+          id: randomUUID(),
+          insertedAt: 100, // Earlier
+        };
+        const items = [item1, item2];
+        items.sort(compareWorkspaceItems);
+        expect(items[0]).toBe(item2);
+        expect(items[1]).toBe(item1);
+      });
+
+      it("should return the item with the earlier insertedAt first when maxPeriods are the same", () => {
+        const item1 = {
+          id: randomUUID(),
+          maxPeriod: 1000,
+          insertedAt: 200, // Later
+        };
+        const item2 = {
+          id: randomUUID(),
+          maxPeriod: 1000,
+          insertedAt: 100, // Earlier
+        };
+        const items = [item1, item2];
+        items.sort(compareWorkspaceItems);
+        expect(items[0]).toBe(item2);
+        expect(items[1]).toBe(item1);
+      });
+
+      it("should return the item with the earlier insertedAt first when priorities are the same", () => {
+        const item1 = {
+          id: randomUUID(),
+          priority: 1,
+          insertedAt: 200, // Later
+        };
+        const item2 = {
+          id: randomUUID(),
+          priority: 1,
+          insertedAt: 100, // Earlier
+        };
+        const items = [item1, item2];
+        items.sort(compareWorkspaceItems);
+        expect(items[0]).toBe(item2);
+        expect(items[1]).toBe(item1);
+      });
+    });
   });
 });
