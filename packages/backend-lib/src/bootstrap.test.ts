9fd38fa753a13f6050727342b74555b01989fd30
Max Gurewitz <max@dittofeed.com>
2025-05-14 14:56:51 -0700

    make domain check more defensive (#1451)

--- packages/backend-lib/src/bootstrap.test.ts
+++ packages/backend-lib/src/bootstrap.test.ts
@@ -1,5 +1,7 @@
 import { randomUUID } from "node:crypto";
 
+import { unwrap } from "isomorphic-lib/src/resultHandling/resultUtils";
+
 import { bootstrapPostgres } from "./bootstrap";
 import { CreateWorkspaceErrorType } from "./types";
 
@@ -15,6 +17,28 @@ describe("bootstrap", () => {
       }
       expect(workspaceResult.error.type).toBe(
         CreateWorkspaceErrorType.InvalidDomain,
+      );
+    });
+
+    it("should reject invalid domain with .com", async () => {
+      const workspaceResult = await bootstrapPostgres({
+        workspaceName: randomUUID(),
+        workspaceDomain: "gmail.com",
+      });
+      if (workspaceResult.isOk()) {
+        throw new Error("expected to fail with validation error");
+      }
+      expect(workspaceResult.error.type).toBe(
+        CreateWorkspaceErrorType.InvalidDomain,
+      );
+    });
+
+    it("it should not reject similar domains", async () => {
+      unwrap(
+        await bootstrapPostgres({
+          workspaceName: randomUUID(),
+          workspaceDomain: "dittomail.com",
+        }),
       );
     });
   });
