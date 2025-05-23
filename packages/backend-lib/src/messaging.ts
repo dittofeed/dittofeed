871c90b8a7fafe7c5850d670a42d4cedf6a2c1c9
Max Gurewitz <max@dittofeed.com>
2025-05-22 21:50:10 -0700

    wip

--- packages/backend-lib/src/messaging.ts
+++ packages/backend-lib/src/messaging.ts
@@ -1269,6 +1269,8 @@ export async function sendEmail({
           bcc: unsplitBcc,
           name: emailName,
           attachments: attachmentsSent,
+          workspaceOccupantId,
+          workspaceOccupantType,
           provider: {
             type: EmailProviderType.Gmail,
             messageId: gmailResult.value.messageId,
