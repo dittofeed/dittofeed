a8ad21e1194a60d254247c3b805622f93874620e
Max Gurewitz <max@dittofeed.com>
2025-05-30 14:36:26 -0700

    wip

--- packages/dashboard/src/pages/oauth2/callback/[provider].page.tsx
+++ packages/dashboard/src/pages/oauth2/callback/[provider].page.tsx
@@ -1,6 +1,7 @@
 import logger from "backend-lib/src/logger";
 import { OAUTH_COOKIE_NAME } from "isomorphic-lib/src/constants";
 import { GetServerSideProps } from "next";
+import { useEffect } from "react";
 
 import {
   decodeAndValidateOauthState,
@@ -8,78 +9,89 @@ import {
 } from "../../../lib/oauth";
 import { requestContext } from "../../../lib/requestContext";
 
-export const getServerSideProps: GetServerSideProps = requestContext(
-  async (ctx, dfContext) => {
-    const { code, provider, state } = ctx.query;
-    if (
-      typeof code !== "string" ||
-      typeof provider !== "string" ||
-      (state && typeof state !== "string")
-    ) {
-      return {
-        redirect: {
-          permanent: false,
-          destination: "/",
-        },
-      };
-    }
-    const validatedState = decodeAndValidateOauthState({
-      stateParam: state,
-      storedCsrfToken: ctx.req.cookies[OAUTH_COOKIE_NAME],
-    });
-    // allow hubspot to be called without a state param for backwards compatibility
-    if (!validatedState && provider !== "hubspot") {
-      logger().error(
-        {
-          provider,
-          state,
-        },
-        "invalid state param",
-      );
-      return {
-        redirect: {
-          permanent: false,
-          destination: "/",
-        },
-      };
-    }
-    const callbackResult = await handleOauthCallback({
-      workspaceId: dfContext.workspace.id,
-      provider,
-      code,
-      returnTo: validatedState?.returnTo,
-      occupantId: dfContext.member.id,
-      occupantType: "WorkspaceMember",
-      baseRedirectUri: "/dashboard/oauth2/callback",
-    });
+export const getServerSideProps: GetServerSideProps<{
+  isPopup?: boolean;
+}> = requestContext(async (ctx, dfContext) => {
+  const { code, provider, state } = ctx.query;
+  if (
+    typeof code !== "string" ||
+    typeof provider !== "string" ||
+    (state && typeof state !== "string")
+  ) {
+    return {
+      redirect: {
+        permanent: false,
+        destination: "/",
+      },
+    };
+  }
+  const validatedState = decodeAndValidateOauthState({
+    stateParam: state,
+    storedCsrfToken: ctx.req.cookies[OAUTH_COOKIE_NAME],
+  });
+  // allow hubspot to be called without a state param for backwards compatibility
+  if (!validatedState && provider !== "hubspot") {
+    logger().error(
+      {
+        provider,
+        state,
+      },
+      "invalid state param",
+    );
+    return {
+      redirect: {
+        permanent: false,
+        destination: "/",
+      },
+    };
+  }
+  const callbackResult = await handleOauthCallback({
+    workspaceId: dfContext.workspace.id,
+    provider,
+    code,
+    returnTo: validatedState?.returnTo,
+    occupantId: dfContext.member.id,
+    occupantType: "WorkspaceMember",
+    baseRedirectUri: "/dashboard/oauth2/callback",
+  });
 
-    if (callbackResult.isErr()) {
-      logger().error(
-        {
-          err: callbackResult.error,
-          workspaceId: dfContext.workspace.id,
-        },
-        "failed to handle oauth callback",
-      );
-      return {
-        redirect: {
-          permanent: false,
-          destination: callbackResult.error.redirectUrl,
-        },
-      };
-    }
-    if (callbackResult.value.actionType === "redirect") {
-      return {
-        redirect: {
-          permanent: false,
-          destination: callbackResult.value.redirectUrl,
-        },
-      };
-    }
-  },
-);
+  if (callbackResult.isErr()) {
+    logger().error(
+      {
+        err: callbackResult.error,
+        workspaceId: dfContext.workspace.id,
+      },
+      "failed to handle oauth callback",
+    );
+    return {
+      redirect: {
+        permanent: false,
+        destination: callbackResult.error.redirectUrl,
+      },
+    };
+  }
+  if (callbackResult.value.actionType === "redirect") {
+    return {
+      redirect: {
+        permanent: false,
+        destination: callbackResult.value.redirectUrl,
+      },
+    };
+  }
+  return {
+    props: {
+      isPopup: true,
+      serverInitialState: {},
+    },
+  };
+});
 
 // Should only be reached if the callback is a popup
-export default function CallbackPage() {
+export default function CallbackPage({ isPopup }: { isPopup?: boolean }) {
+  useEffect(() => {
+    if (isPopup) {
+      window.close();
+    }
+  }, [isPopup]);
   return null;
 }
