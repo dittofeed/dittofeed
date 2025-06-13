372e0a8c8c1eb908c6c66390f399900e033abcbe
Max Gurewitz <max@dittofeed.com>
2025-06-13 00:48:23 -0700

    wip

--- packages/dashboard/src/pages/journeys/[id].page.tsx
+++ packages/dashboard/src/pages/journeys/[id].page.tsx
@@ -1,26 +1,25 @@
-import { useRouter } from "next/router";
+import { GetServerSideProps } from "next";
+import qs from "qs";
+import { validate } from "uuid";
 
-import JourneysBuilder from "../../components/journeys/journeysBuilder";
-import JourneyLayout from "../../components/journeys/layout";
-import {
-  JourneyGetServerSideProps,
-  journeyGetServerSideProps,
-} from "./getServerSideProps";
-
-export const getServerSideProps: JourneyGetServerSideProps = (ctx) =>
-  journeyGetServerSideProps(ctx);
-
-function Journey() {
-  const path = useRouter();
-  const id = typeof path.query.id === "string" ? path.query.id : undefined;
-  if (!id) {
-    return null;
+// Redirect to the new journey editor page passing the id as a query param
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const id = ctx.params?.id;
+  if (typeof id !== "string" || !validate(id)) {
+    return {
+      notFound: true,
+    };
   }
+  const queryParams = { ...ctx.query, id };
+  const url = `/journeys/v2?${qs.stringify(queryParams)}`;
+  return {
+    redirect: {
+      destination: url,
+      permanent: false,
+    },
+  };
+};
 
-  return (
-    <JourneyLayout journeyId={id}>
-      <JourneysBuilder journeyId={id} />
-    </JourneyLayout>
-  );
+export default function Journey() {
+  return null;
 }
-export default Journey;
