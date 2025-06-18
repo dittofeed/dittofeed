4e24a51e80df4f2cf086454df2a981a224f2300a
Max Gurewitz <max@dittofeed.com>
2025-06-17 22:25:50 -0700

    wip

--- packages/dashboard/src/components/broadcasts/configuration.tsx
+++ packages/dashboard/src/components/broadcasts/configuration.tsx
@@ -437,6 +437,7 @@ export default function Configuration({
             type="number"
             inputProps={{
               min: 1,
+              max: 1000,
             }}
             disabled={disabled}
             value={broadcast.config.batchSize ?? ""}
