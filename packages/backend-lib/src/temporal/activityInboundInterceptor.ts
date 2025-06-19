a05fb3875919059cc1f7a4ea9f0633742aa42ffd
Max Gurewitz <max@dittofeed.com>
2025-06-18 20:22:30 -0700

    log error traces on activity failures (#1514)

--- packages/backend-lib/src/temporal/activityInboundInterceptor.ts
+++ packages/backend-lib/src/temporal/activityInboundInterceptor.ts
@@ -6,6 +6,8 @@ import {
   Next,
 } from "@temporalio/worker";
 
+import logger from "../logger";
+
 export interface CustomContext extends Context {
   workflowClient: WorkflowClient;
 }
@@ -25,10 +27,31 @@ export class CustomActivityInboundInterceptor
   }
 
   // eslint-disable-next-line class-methods-use-this
-  execute(
+  async execute(
     input: ActivityExecuteInput,
     next: Next<ActivityInboundCallsInterceptor, "execute">,
   ): Promise<unknown> {
-    return next(input);
+    try {
+      return await next(input);
+    } catch (err) {
+      const activityInfo = Context.current().info;
+
+      // Log the error with full stack trace
+      logger().error(
+        {
+          err,
+          activityType: activityInfo.activityType,
+          activityId: activityInfo.activityId,
+          workflowId: activityInfo.workflowExecution.workflowId,
+          workflowRunId: activityInfo.workflowExecution.runId,
+          attempt: activityInfo.attempt,
+          taskQueue: activityInfo.taskQueue,
+        },
+        "Activity failed",
+      );
+
+      // Re-throw the error so Temporal can handle it properly
+      throw err;
+    }
   }
 }
