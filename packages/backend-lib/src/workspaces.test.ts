bea76800d9e7534b023cd99f2debc40978f9fac6
Max Gurewitz <max@dittofeed.com>
2025-06-27 18:29:01 -0700

    wip

---
+++ packages/backend-lib/src/workspaces.test.ts
@@ -0,0 +1,26 @@
+describe("workspaces", () => {
+  describe("after tombstoning a workspace", () => {
+    it("should be able to create a new workspace with the same name", async () => {
+      // create workspace
+      // tombstone workspace
+      // create new workspace with same name
+      // add resource to new workspace
+    });
+
+    it("should be able to create a new workspace with the same external id", async () => {
+      // create workspace
+      // tombstone workspace
+      // create new workspace with same external id
+      // add resource to new workspace
+    });
+
+    it("should fail to activate the tombstoned workspace after creating a new workspace with the same name", async () => {
+      // create workspace
+      // tombstone workspace
+      // create new workspace with same name
+      // add resource to new workspace
+      // activate tombstoned workspace
+      // expect error
+    });
+  });
+});
