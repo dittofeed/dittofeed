8952ddc39cfe615a72c4b2fb49dee7429c68aebd
Max Gurewitz <max@dittofeed.com>
2025-07-03 18:29:13 -0700

    wip

--- packages/backend-lib/scripts/20crm-core-client.sh
+++ packages/backend-lib/scripts/20crm-core-client.sh
@@ -13,13 +13,22 @@ curl -L \
   "$twenty_url/open-api/core" \
   -o ./.tmp/twenty-core-openapi.json
 
+rm -rf ./packages/backend-lib/src/twentyCrm/coreClient
+
 openapi-generator-cli generate \
   -i ./.tmp/twenty-core-openapi.json \
   -g typescript-axios \
   -o ./packages/backend-lib/src/twentyCrm/coreClient \
   --skip-validate-spec \
-  --openapi-normalizer FILTER="operation:findManyPeople,createOnePerson,findOnePerson,deleteOnePerson,UpdateOnePerson,findPersonDuplicates" \
+  --openapi-normalizer FILTER="tag:people" \
   --type-mappings="CompanyForResponse=object,OpportunityForResponse=object,TaskTargetForResponse=object,NoteTargetForResponse=object,AttachmentForResponse=object,FavoriteForResponse=object,MessageParticipantForResponse=object,CalendarEventParticipantForResponse=object,TimelineActivityForResponse=object" \
   --additional-properties="supportsES6=true,withSeparateModelsAndApi=true,apiPackage=api,modelPackage=model"
 
+# Clean up the api directory
 find ./packages/backend-lib/src/twentyCrm/coreClient/api -type f ! -name 'people-api.ts' -delete
+
+# Clean up the model directory
+find ./packages/backend-lib/src/twentyCrm/coreClient/model -type f ! -name 'person*.ts' -delete
+
+# Clean up the docs directory
+find ./packages/backend-lib/src/twentyCrm/coreClient/docs -type f ! -name 'PeopleApi.md' ! -name 'Person*.md' -delete
