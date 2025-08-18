2ab62621e4ddecfd50c3e1afc181ddc4c77d8f8f
Max Gurewitz <max@dittofeed.com>
2025-08-17 22:49:08 -0700

    wip

--- packages/backend-lib/src/destinations/signalwire.ts
+++ packages/backend-lib/src/destinations/signalwire.ts
@@ -1,13 +1,17 @@
 import { Messaging, SignalWire } from "@signalwire/realtime-api";
 import { Static, Type } from "@sinclair/typebox";
 import { schemaValidateWithErr } from "isomorphic-lib/src/resultHandling/schemaValidation";
-import { Result } from "neverthrow";
+import { err, ok, Result } from "neverthrow";
+
+import logger from "../logger";
 
 export interface SignalWireNonRetryableError {
   errorCode: string;
   errorMessage: string;
   status: string;
 }
+
+export type SignalWireHandlingApplicationError = Error;
 
 export interface SignalWireSuccess {
   sid: string;
@@ -16,10 +20,12 @@ export interface SignalWireSuccess {
 
 export const SignalWireResult = Type.Object({
   sid: Type.String(),
-  error_code: Type.Optional(Type.String()),
-  error_message: Type.Optional(Type.String()),
+  error_code: Type.Optional(Type.Union([Type.String(), Type.Null()])),
+  error_message: Type.Optional(Type.Union([Type.String(), Type.Null()])),
   status: Type.String(),
 });
+
+export const SIGNAL_WIRE_FAILURE_STATUSES = new Set(["failed", "undelivered"]);
 
 export async function sendSms({
   project,
@@ -28,26 +34,53 @@ export async function sendSms({
   body,
   from,
 }: {
+  workspaceId: string;
   project: string;
   to: string;
   body: string;
   token: string;
   from: string;
-}): Promise<Result<SignalWireSuccess, SignalWireNonRetryableError>> {
+  // FIXME add tags
+}): Promise<
+  Result<
+    SignalWireSuccess,
+    SignalWireNonRetryableError | SignalWireHandlingApplicationError
+  >
+> {
   const client = await SignalWire({
     project,
     token,
   });
   try {
+    // FIXME add callback url with tags as query params
     const result: unknown = await client.messaging.send({
       from,
       to,
       body,
     });
+
     const verifiedResult = schemaValidateWithErr(result, SignalWireResult);
+    if (verifiedResult.isErr()) {
+      return err(verifiedResult.error);
+    }
+
+    const {
+      sid,
+      status,
+      error_code: errorCode,
+      error_message: errorMessage,
+    } = verifiedResult.value;
+    // check status and error_code if neither indicate an error, return a success result
+    if (!SIGNAL_WIRE_FAILURE_STATUSES.has(status) && !errorCode) {
+      return ok({ sid, status });
+    }
   } catch (error) {
-    // TODO throw retryable error
-    // TODO return not retryable error
-    throw new Error("Not implemented");
+    logger().error(
+      {
+        err: error,
+      },
+      "transient signalwire error",
+    );
+    throw error;
   }
 }
