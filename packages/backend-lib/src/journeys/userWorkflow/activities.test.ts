6f28d9816627295ddeabdf5efed1937d311f7d7b
Max Gurewitz <max@dittofeed.com>
2025-08-23 22:45:25 -0700

    wip

--- packages/backend-lib/src/journeys/userWorkflow/activities.test.ts
+++ packages/backend-lib/src/journeys/userWorkflow/activities.test.ts
@@ -2,6 +2,8 @@ import { randomUUID } from "crypto";
 
 import { db } from "../../db";
 import * as schema from "../../db/schema";
+import { UserPropertyDefinitionType } from "../../types";
+import { upsertUserProperty } from "../../userProperties";
 
 describe("user workflows activity test", () => {
   let workspaceId: string;
@@ -13,6 +15,33 @@ describe("user workflows activity test", () => {
         id: workspaceId,
         name: `user-workflow-activity-test-${workspaceId}`,
       });
+    await Promise.all([
+      upsertUserProperty(
+        {
+          workspaceId,
+          name: "id",
+          definition: {
+            type: UserPropertyDefinitionType.Id,
+          },
+        },
+        {
+          skipProtectedCheck: true,
+        },
+      ),
+      upsertUserProperty(
+        {
+          workspaceId,
+          name: "email",
+          definition: {
+            type: UserPropertyDefinitionType.Trait,
+            path: "email",
+          },
+        },
+        {
+          skipProtectedCheck: true,
+        },
+      ),
+    ]);
   });
   describe("sendMessageFactory", () => {
     describe("with events in context", () => {
