a7cf07cdc28266bc661405266d7520f1cff2ae6f
Max Gurewitz <max@dittofeed.com>
2026-01-07 17:43:57 -0800

    lint fix

--- packages/dashboard/src/components/dashboardContent.tsx
+++ packages/dashboard/src/components/dashboardContent.tsx
@@ -3,7 +3,9 @@ import dynamic from "next/dynamic";
 import DashboardHead from "./dashboardHead";
 import MainLayout from "./mainLayout";
 
-const CommandPalette = dynamic(() => import("./commandPalette"), { ssr: false });
+const CommandPalette = dynamic(() => import("./commandPalette"), {
+  ssr: false,
+});
 
 export default function DashboardContent({
   children,
