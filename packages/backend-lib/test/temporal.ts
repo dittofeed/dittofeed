4e7ee25e4c95fa7407263f35a2a12cf15e616ca4
Max Gurewitz <max@dittofeed.com>
2026-01-09 10:58:32 -0800

    create worker

--- packages/backend-lib/test/temporal.ts
+++ packages/backend-lib/test/temporal.ts
@@ -11,13 +11,13 @@ import * as activities from "../src/temporal/activities";
 import { CustomActivityInboundInterceptor } from "../src/temporal/activityInboundInterceptor";
 import workerLogger from "../src/workerLogger";
 
-export async function createEnvAndWorker({
+export async function createWorker({
+  testEnv,
   activityOverrides,
 }: {
+  testEnv: TestWorkflowEnvironment;
   activityOverrides?: Parameters<typeof Worker.create>[0]["activities"];
-} = {}) {
-  const testEnv = await TestWorkflowEnvironment.createTimeSkipping();
-
+}) {
   const worker = await Worker.create({
     connection: testEnv.nativeConnection,
     workflowsPath: require.resolve(
@@ -38,5 +38,18 @@ export async function createEnvAndWorker({
     sinks: defaultSinks(workerLogger),
     taskQueue: "default",
   });
+  return worker;
+}
+export async function createEnvAndWorker({
+  activityOverrides,
+}: {
+  activityOverrides?: Parameters<typeof Worker.create>[0]["activities"];
+} = {}) {
+  const testEnv = await TestWorkflowEnvironment.createTimeSkipping();
+  const worker = await createWorker({
+    testEnv,
+    activityOverrides,
+  });
+
   return { testEnv, worker };
 }
